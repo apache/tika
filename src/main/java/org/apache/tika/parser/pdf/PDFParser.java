5cbfe94b115934b04cea4954af97a4dbc1285393
Jukka Zitting <jukka@apache.org>
2007-11-13 01:04:30 +0000

    TIKA-100 - Structured PDF parsing     - Customized the PdfTextStripper class to produce XHTML SAX events       (there's a somewhat similar PdfText2HTML class in PDFBox, but       that class produces a character stream instead of SAX events)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tika/trunk@594376 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/tika/parser/pdf/PDFParser.java
+++ src/main/java/org/apache/tika/parser/pdf/PDFParser.java
@@ -18,17 +18,14 @@ package org.apache.tika.parser.pdf;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.util.Calendar;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.Parser;
-import org.apache.tika.sax.XHTMLContentHandler;
 
 import org.pdfbox.pdmodel.PDDocument;
 import org.pdfbox.pdmodel.PDDocumentInformation;
-import org.pdfbox.util.PDFTextStripper;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
@@ -40,64 +37,55 @@ public class PDFParser implements Parser {
     public void parse(
             InputStream stream, ContentHandler handler, Metadata metadata)
             throws IOException, SAXException, TikaException {
+        PDDocument pdfDocument = PDDocument.load(stream);
         try {
-            PDDocument pdfDocument = PDDocument.load(stream);
-            try {
-                if (pdfDocument.isEncrypted()) {
+            if (pdfDocument.isEncrypted()) {
+                try {
                     pdfDocument.decrypt("");
+                } catch (Exception e) {
+                    // Ignore
                 }
-
-                PDDocumentInformation info =
-                    pdfDocument.getDocumentInformation();
-                if (info.getTitle() != null) {
-                    metadata.set(Metadata.TITLE, info.getTitle());
-                }
-                if (info.getAuthor() != null) {
-                    metadata.set(Metadata.AUTHOR, info.getAuthor());
-                }
-                if (info.getCreator() != null) {
-                    metadata.set(Metadata.CREATOR, info.getCreator());
-                }
-                if (info.getKeywords() != null) {
-                    metadata.set(Metadata.KEYWORDS, info.getKeywords());
-                }
-                if (info.getProducer() != null) {
-                    // TODO: Need a Metadata key for producer
-                    metadata.set("producer", info.getProducer());
-                }
-                if (info.getSubject() != null) {
-                    metadata.set(Metadata.SUBJECT, info.getSubject());
-                }
-                if (info.getTrapped() != null) {
-                    // TODO: Need a Metadata key for producer
-                    metadata.set("trapped", info.getTrapped());
-                }
-                Calendar created = info.getCreationDate();
-                if (created != null) {
-                    metadata.set("created", created.getTime().toString());
-                }
-                Calendar modified = info.getModificationDate();
-                if (modified != null) {
-                    metadata.set(
-                            Metadata.LAST_MODIFIED,
-                            modified.getTime().toString());
-                }
-
-                StringWriter writer = new StringWriter();
-                new PDFTextStripper().writeText(pdfDocument, writer);
-
-                XHTMLContentHandler xhtml =
-                    new XHTMLContentHandler(handler, metadata);
-                xhtml.startDocument();
-                xhtml.element("p", writer.getBuffer().toString());
-                xhtml.endDocument();
-            } finally {
-                pdfDocument.close();
             }
+            metadata.add(Metadata.CONTENT_TYPE, "application/pdf");
+            extractMetadata(pdfDocument, metadata);
+            PDF2XHTML.process(pdfDocument, handler, metadata);
+        } finally {
+            pdfDocument.close();
+        }
+    }
+
+    private void extractMetadata(PDDocument document, Metadata metadata)
+            throws TikaException {
+        PDDocumentInformation info = document.getDocumentInformation();
+        addMetadata(metadata, Metadata.TITLE, info.getTitle());
+        addMetadata(metadata, Metadata.AUTHOR, info.getAuthor());
+        addMetadata(metadata, Metadata.CREATOR, info.getCreator());
+        addMetadata(metadata, Metadata.KEYWORDS, info.getKeywords());
+        addMetadata(metadata, "producer", info.getProducer());
+        addMetadata(metadata, Metadata.SUBJECT, info.getSubject());
+        addMetadata(metadata, "trapped", info.getTrapped());
+        try {
+            addMetadata(metadata, "created", info.getCreationDate());
         } catch (IOException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new TikaException("Error parsing a PDF document", e);
+            // Invalid date format, just ignore
+        }
+        try {
+            Calendar modified = info.getModificationDate(); 
+            addMetadata(metadata, Metadata.LAST_MODIFIED, modified);
+        } catch (IOException e) {
+            // Invalid date format, just ignore
+        }
+    }
+
+    private void addMetadata(Metadata metadata, String name, String value) {
+        if (value != null) {
+            metadata.add(name, value);
+        }
+    }
+
+    private void addMetadata(Metadata metadata, String name, Calendar value) {
+        if (value != null) {
+            metadata.set(name, value.getTime().toString());
         }
     }
 
