d1ab05c8e4a9f192c0bcec5538af557659393e66
Jukka Zitting <jukka@apache.org>
2008-09-04 19:34:08 +0000

    TIKA-149: Parser for zip files
    Moved the ZipParser class to o.a.t.parser.pkg and extracted an abstract PackageParser base class to better support other package formats.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tika/trunk@692207 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/tika/parser/pkg/PackageParser.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.pkg;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.input.CloseShieldInputStream;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AbstractParser;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * Abstract base class for parsers that deal with package formats.
+ * Subclasses can call the
+ * {@link #parseEntry(InputStream, XHTMLContentHandler, Metadata)}
+ * method to parse the given package entry using the configured
+ * entry parser. The entries will be written to the XHTML event stream
+ * as &lt;div class="package-entry"&gt; elements that contain the
+ * (optional) entry name as a &lt;h1&gt; element and the full
+ * structured body content of the parsed entry.
+ */
+public abstract class PackageParser extends AbstractParser {
+
+    /**
+     * The parser instance used to parse package entries.
+     */
+    private Parser parser;
+
+    /**
+     * Returns the parser instance used to parse package entries.
+     *
+     * @return entry parser
+     */
+    public Parser getParser() {
+        Parser parser = this.parser;
+        if (parser == null) {
+            parser = new AutoDetectParser();
+        }
+        return parser;
+    }
+
+    /**
+     * Sets the parser instance used to parse package entries.
+     *
+     * @param parser entry parser
+     */
+    public void setParser(Parser parser) {
+        this.parser = parser;
+    }
+
+    /**
+     * Parses the given entry entry using the underlying parser instance.
+     * It is not an error if the entry can not be parsed, in that case
+     * just the entry name (if given) is emitted.
+     *
+     * @param stream entry stream
+     * @param xhtml XHTML event handler
+     * @param metadata entry metadata
+     * @throws IOException if an IO error occurs
+     * @throws SAXException if a SAX error occurs
+     */
+    protected void parseEntry(
+            InputStream stream, XHTMLContentHandler xhtml, Metadata metadata)
+            throws IOException, SAXException {
+        xhtml.startElement("div", "class", "package-entry");
+
+        String name = metadata.get(Metadata.RESOURCE_NAME_KEY);
+        if (name != null) {
+            xhtml.element("h1", name);
+            xhtml.characters("\n");
+        }
+
+        try {
+            getParser().parse(
+                    new CloseShieldInputStream(stream),
+                    new BodyContentHandler(xhtml),
+                    metadata);
+            xhtml.characters("\n");
+        } catch (TikaException e) {
+            // Could not parse the entry, just skip the content
+        }
+
+        xhtml.endElement("div");
+    }
+
+}
--- src/main/java/org/apache/tika/parser/pkg/ZipParser.java
+++ src/main/java/org/apache/tika/parser/pkg/ZipParser.java
@@ -14,30 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.tika.parser.zip;
-
-import org.apache.commons.io.input.CloseShieldInputStream;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AbstractParser;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.Parser;
-import org.apache.tika.sax.BodyContentHandler;
-import org.apache.tika.sax.XHTMLContentHandler;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.SAXException;
+package org.apache.tika.parser.pkg;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.apache.commons.io.input.CloseShieldInputStream;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
 /**
  * Zip File Parser.
  */
-public class ZipParser extends AbstractParser {
-
-    private Parser parser;
+public class ZipParser extends PackageParser {
 
     /**
      * Parses the given stream as a Zip file.
@@ -57,7 +51,9 @@ public class ZipParser extends AbstractParser {
         try {
             ZipEntry entry = zip.getNextEntry();
             while (entry != null) {
-                parseEntry(xhtml, entry, zip);
+                Metadata entrydata = new Metadata();
+                entrydata.set(Metadata.RESOURCE_NAME_KEY, entry.getName());
+                parseEntry(zip, xhtml, entrydata);
                 entry = zip.getNextEntry();
             }
         } finally {
@@ -67,48 +63,4 @@ public class ZipParser extends AbstractParser {
         xhtml.endDocument();
     }
 
-    /**
-     * Parses the given Zip entry using the underlying parser instance.
-     * It is not an error if the entry can not be parsed, in that case
-     * just the entry name is emitted.
-     *
-     * @param xhtml XHTML event handler
-     * @param entry zip entry
-     * @param stream zip stream
-     * @throws IOException if an IO error occurs
-     * @throws SAXException if a SAX error occurs
-     */
-    private void parseEntry(
-            XHTMLContentHandler xhtml, ZipEntry entry, InputStream stream)
-            throws IOException, SAXException {
-        xhtml.startElement("div", "class", "file");
-        xhtml.element("h1", entry.getName());
-        xhtml.characters("\n");
-
-        try {
-            Metadata metadata = new Metadata();
-            metadata.set(Metadata.RESOURCE_NAME_KEY, entry.getName());
-            getParser().parse(
-                    new CloseShieldInputStream(stream),
-                    new BodyContentHandler(xhtml),
-                    metadata);
-        } catch (TikaException e) {
-            // Could not parse the entry, just skip the content
-        }
-
-        xhtml.characters("\n");
-        xhtml.endElement("div");
-    }
-
-    public Parser getParser() {
-        if (parser == null)
-        {
-            return new AutoDetectParser();
-        }
-        return parser;
-    }
-
-    public void setParser(Parser parser) {
-        this.parser = parser;
-    }
 }
