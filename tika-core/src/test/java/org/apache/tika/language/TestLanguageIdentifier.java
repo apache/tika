12e06899f9d1a231804bac350676612a767f644b
Jukka Zitting <jukka@apache.org>
2009-07-31 07:43:34 +0000

    TIKA-209: Language detection is weak.
    Re-indent to match Java coding conventions.
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@799534 13f79535-47bb-0310-9956-ffa450edef68

--- tika-core/src/test/java/org/apache/tika/language/TestLanguageIdentifier.java
+++ tika-core/src/test/java/org/apache/tika/language/TestLanguageIdentifier.java
@@ -16,21 +16,19 @@
  */
 package org.apache.tika.language;
 
-// JDK imports
-import java.io.InputStream;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
-import org.apache.tika.language.NGramProfile.NGramEntry;
-
-// JUnit imports
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
+import org.apache.tika.language.NGramProfile.NGramEntry;
 
 /**
  * JUnit based test of class {@link LanguageIdentifier}.
@@ -39,8 +37,7 @@ import junit.textui.TestRunner;
  * @author Jerome Charron - http://frutch.free.fr/
  */
 public class TestLanguageIdentifier extends TestCase {
-    
-    
+
     public TestLanguageIdentifier(String testName) {
         super(testName);
     }
@@ -48,144 +45,141 @@ public class TestLanguageIdentifier extends TestCase {
     public static Test suite() {
         return new TestSuite(TestLanguageIdentifier.class);
     }
-    
+
     public static void main(String[] args) {
         TestRunner.run(suite());
     }
 
-  String tokencontent1 = "testaddtoken";
-  String tokencontent2 = "anotherteststring";
+    String tokencontent1 = "testaddtoken";
+    String tokencontent2 = "anotherteststring";
 
-  int[] counts1 = { 3, 2, 2, 1, 1, 1, 1, 1 };
+    int[] counts1 = { 3, 2, 2, 1, 1, 1, 1, 1 };
 
-  String[] chars1 = { "t", "d", "e", "a", "k", "n", "o", "s" };
+    String[] chars1 = { "t", "d", "e", "a", "k", "n", "o", "s" };
 
-  /**
-   * Test analyze method
-   */
-  public void testAnalyze() {
-    String tokencontent = "testmeagain";
+    /**
+     * Test analyze method
+     */
+    public void testAnalyze() {
+        String tokencontent = "testmeagain";
 
-    NGramProfile p = new NGramProfile("test", 1, 1);
-    p.analyze(new StringBuilder(tokencontent));
+        NGramProfile p = new NGramProfile("test", 1, 1);
+        p.analyze(new StringBuilder(tokencontent));
 
-    //test that profile size is ok, eg 9 different NGramEntries "tesmagin"
-    assertEquals(8, p.getSorted().size());
-  }
-
-  /**
-   * Test addNGrams method with StringBuffer argument
-   *
-   */
-  public void testAddNGramsStringBuffer() {
-    String tokencontent = "testmeagain";
-
-    NGramProfile p = new NGramProfile("test", 1, 1);
-    p.add(new StringBuffer(tokencontent));
-
-    //test that profile size is ok, eg 8 different NGramEntries "tesmagin"
-    assertEquals(8, p.getSorted().size());
-
-  }
-
-  /**
-   * test getSorted method
-   */
-  public void testGetSorted() {
-    int[] count = { 4, 3, 1 };
-    String[] ngram = { "a", "b", "c" };
-
-    String teststring = "AAaaBbbC";
-
-    NGramProfile p = new NGramProfile("test", 1, 1);
-    p.analyze(new StringBuilder(teststring));
-
-    //test size of profile
-    assertEquals(3, p.getSorted().size());
-
-    testCounts(p.getSorted(), count);
-    testContents(p.getSorted(), ngram);
-
-  }
-
-  public void testGetSimilarity() {
-    NGramProfile a = new NGramProfile("a", 1, 1);
-    NGramProfile b = new NGramProfile("b", 1, 1);
-    
-    a.analyze(new StringBuilder(tokencontent1));
-    b.analyze(new StringBuilder(tokencontent2));
-
-    //because of rounding errors might slightly return different results
-    assertEquals(a.getSimilarity(b), b.getSimilarity(a), 0.0000002);
-
-  }
-
-  public void testExactMatch() {
-    NGramProfile a = new NGramProfile("a", 1, 1);
-    
-    a.analyze(new StringBuilder(tokencontent1));
-
-    assertEquals(a.getSimilarity(a), 0, 0);
-
-  }
-
-  
-  public void testIO() {
-    //Create profile and set some contents
-    NGramProfile a = new NGramProfile("a", 1, 1);
-    a.analyze(new StringBuilder(this.tokencontent1));
-
-    NGramProfile b = new NGramProfile("a_from_inputstream", 1, 1);
-
-    //save profile
-    ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-    try {
-      a.save(os);
-      os.close();
-    } catch (Exception e) {
-      fail();
+        //test that profile size is ok, eg 9 different NGramEntries "tesmagin"
+        assertEquals(8, p.getSorted().size());
     }
 
-    //load profile
-    InputStream is = new ByteArrayInputStream(os.toByteArray());
-    try {
-      b.load(is);
-      is.close();
-    } catch (Exception e) {
-      fail();
+    /**
+     * Test addNGrams method with StringBuffer argument
+     *
+     */
+    public void testAddNGramsStringBuffer() {
+        String tokencontent = "testmeagain";
+
+        NGramProfile p = new NGramProfile("test", 1, 1);
+        p.add(new StringBuffer(tokencontent));
+
+        //test that profile size is ok, eg 8 different NGramEntries "tesmagin"
+        assertEquals(8, p.getSorted().size());
+
     }
 
-    //check it
-    testCounts(b.getSorted(), counts1);
-    testContents(b.getSorted(), chars1);
-  }
+    /**
+     * test getSorted method
+     */
+    public void testGetSorted() {
+        int[] count = { 4, 3, 1 };
+        String[] ngram = { "a", "b", "c" };
 
-  private void testContents(List<NGramEntry> entries, String contents[]) {
-    int c = 0;
+        String teststring = "AAaaBbbC";
 
-    for (NGramEntry nge : entries) {
-      assertEquals(contents[c], nge.getSeq().toString());
-      c++;
+        NGramProfile p = new NGramProfile("test", 1, 1);
+        p.analyze(new StringBuilder(teststring));
+
+        //test size of profile
+        assertEquals(3, p.getSorted().size());
+
+        testCounts(p.getSorted(), count);
+        testContents(p.getSorted(), ngram);
     }
-  }
 
-  private void testCounts(List<NGramEntry> entries, int counts[]) {
-    int c = 0;
+    public void testGetSimilarity() {
+        NGramProfile a = new NGramProfile("a", 1, 1);
+        NGramProfile b = new NGramProfile("b", 1, 1);
 
-    for (NGramEntry nge : entries) {
-      System.out.println(nge);
-      assertEquals(counts[c], nge.getCount());
-      c++;
+        a.analyze(new StringBuilder(tokencontent1));
+        b.analyze(new StringBuilder(tokencontent2));
+
+        //because of rounding errors might slightly return different results
+        assertEquals(a.getSimilarity(b), b.getSimilarity(a), 0.0000002);
     }
-  }
+
+    public void testExactMatch() {
+        NGramProfile a = new NGramProfile("a", 1, 1);
+
+        a.analyze(new StringBuilder(tokencontent1));
+
+        assertEquals(a.getSimilarity(a), 0, 0);
+    }
+
+
+    public void testIO() {
+        //Create profile and set some contents
+        NGramProfile a = new NGramProfile("a", 1, 1);
+        a.analyze(new StringBuilder(this.tokencontent1));
+
+        NGramProfile b = new NGramProfile("a_from_inputstream", 1, 1);
+
+        //save profile
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        try {
+            a.save(os);
+            os.close();
+        } catch (Exception e) {
+            fail();
+        }
+
+        //load profile
+        InputStream is = new ByteArrayInputStream(os.toByteArray());
+        try {
+            b.load(is);
+            is.close();
+        } catch (Exception e) {
+            fail();
+        }
+
+        //check it
+        testCounts(b.getSorted(), counts1);
+        testContents(b.getSorted(), chars1);
+    }
+
+    private void testContents(List<NGramEntry> entries, String contents[]) {
+        int c = 0;
+
+        for (NGramEntry nge : entries) {
+            assertEquals(contents[c], nge.getSeq().toString());
+            c++;
+        }
+    }
+
+    private void testCounts(List<NGramEntry> entries, int counts[]) {
+        int c = 0;
+
+        for (NGramEntry nge : entries) {
+            System.out.println(nge);
+            assertEquals(counts[c], nge.getCount());
+            c++;
+        }
+    }
 
     public void testIdentify() {
         try {
             long total = 0;
             LanguageIdentifier idfr = new LanguageIdentifier();
             BufferedReader in = new BufferedReader(new InputStreamReader(
-                        this.getClass().getResourceAsStream("test-referencial.txt")));
+                    this.getClass().getResourceAsStream("test-referencial.txt")));
             String line = null;
             while((line = in.readLine()) != null) {
                 String[] tokens = line.split(";");
@@ -198,7 +192,7 @@ public class TestLanguageIdentifier extends TestCase {
                     // Then, each line of the file...
                     BufferedReader testFile = new BufferedReader(
                             new InputStreamReader(
-                                this.getClass().getResourceAsStream(tokens[0]), "UTF-8"));
+                                    this.getClass().getResourceAsStream(tokens[0]), "UTF-8"));
                     String testLine = null;
                     while((testLine = testFile.readLine()) != null) {
                         testLine = testLine.trim();
