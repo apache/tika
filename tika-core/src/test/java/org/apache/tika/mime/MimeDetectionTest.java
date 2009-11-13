39469baa4c0a991b171dd71b36f65c205058e462
Chris Mattmann <mattmann@apache.org>
2009-11-13 22:35:27 +0000

    - fix for TIKA-309: Mime type application/rdf+xml not correctly detected
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@836035 13f79535-47bb-0310-9956-ffa450edef68

--- tika-core/src/test/java/org/apache/tika/mime/MimeDetectionTest.java
+++ tika-core/src/test/java/org/apache/tika/mime/MimeDetectionTest.java
@@ -18,6 +18,7 @@ package org.apache.tika.mime;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 import junit.framework.TestCase;
 
@@ -47,6 +48,8 @@ public class MimeDetectionTest extends TestCase {
         testFile("application/xml", "test-utf16be.xml");
         testFile("application/xml", "test-long-comment.xml");
         testFile("application/xslt+xml", "stylesheet.xsl");
+        testUrl("application/rdf+xml", new URL("http://www.ai.sri.com/daml/services/owl-s/1.2/Process.owl"));
+        testUrl("application/rdf+xml", new URL("http://www.w3.org/2002/07/owl#"));
     }
     
     public void testAutosetSupertype() throws MimeTypeException {
@@ -57,25 +60,34 @@ public class MimeDetectionTest extends TestCase {
     	type = types.forName("text/something");
     	assertEquals("text/plain", type.getSuperType().getName());
     }
+    
+    private void testUrl(String expected, URL url) throws IOException{
+        InputStream in = url.openStream();
+        testStream(expected, url.toString(), in);        
+    }
 
     private void testFile(String expected, String filename) throws IOException {
         InputStream in = getClass().getResourceAsStream(filename);
-        assertNotNull("Test file not found: " + filename, in);
+        testStream(expected, filename, in);
+    }
+    
+    private void testStream(String expected, String urlOrFileName, InputStream in) throws IOException{
+        assertNotNull("Test stream: ["+urlOrFileName+"] is null!", in);
         if (!in.markSupported()) {
             in = new java.io.BufferedInputStream(in);
         }
         try {
             Metadata metadata = new Metadata();
             String mime = this.mimeTypes.detect(in, metadata).toString();
-            assertEquals(filename + " is not properly detected.", expected, mime);
+            assertEquals(urlOrFileName + " is not properly detected.", expected, mime);
 
             //Add resource name and test again
-            metadata.set(Metadata.RESOURCE_NAME_KEY, filename);
+            metadata.set(Metadata.RESOURCE_NAME_KEY, urlOrFileName);
             mime = this.mimeTypes.detect(in, metadata).toString();
-            assertEquals(filename + " is not properly detected.", expected, mime);
+            assertEquals(urlOrFileName + " is not properly detected after adding resource name.", expected, mime);
         } finally {
             in.close();
-        }
+        }        
     }
 
 }
