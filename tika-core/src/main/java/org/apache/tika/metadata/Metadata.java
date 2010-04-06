14a229e5814b0148ba0b8d08f0cc71708a861c88
Chris Mattmann <mattmann@apache.org>
2010-04-06 07:00:07 +0000

    - basic support for netCDF parsing, as specified in TIKA-400 netCDF Tika Parser. Can extend more later, but enough support right now to commit. Includes basic unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@931037 13f79535-47bb-0310-9956-ffa450edef68

--- tika-core/src/main/java/org/apache/tika/metadata/Metadata.java
+++ tika-core/src/main/java/org/apache/tika/metadata/Metadata.java
@@ -25,7 +25,7 @@ import java.util.Properties;
  * A multi-valued metadata container.
  */
 public class Metadata implements CreativeCommons, DublinCore, HttpHeaders,
-        MSOffice, TikaMetadataKeys, TikaMimeKeys {
+        MSOffice, ClimateForcast, TikaMetadataKeys, TikaMimeKeys {
 
     /**
      * A map of all metadata attributes.
