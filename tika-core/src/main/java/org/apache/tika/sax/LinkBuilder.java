62beaae70537f826e0041b01fe1c98ae47a6b4d6
Jukka Zitting <jukka@apache.org>
2010-10-27 16:32:50 +0000

    TIKA-503: Add a ContentHandler for collecting links from parser output
    Add support collecting also image links. More to come...
    
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1028028 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tika-core/src/main/java/org/apache/tika/sax/LinkBuilder.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.sax;
+
+class LinkBuilder {
+
+    private final String type;
+
+    private String uri = "";
+
+    private String title = "";
+
+    private final StringBuilder text = new StringBuilder();
+
+    public LinkBuilder(String type) {
+        this.type = type;
+    }
+
+    public void setURI(String uri) {
+        if (uri != null) {
+            this.uri = uri;
+        } else {
+            this.uri = "";
+        }
+    }
+
+    public void setTitle(String title) {
+        if (title != null) {
+            this.title = title;
+        } else {
+            this.title = "";
+        }
+    }
+
+    public void characters(char[] ch, int offset, int length) {
+        text.append(ch, offset, length);
+    }
+
+    public Link getLink() {
+        return new Link(type, uri, title, text.toString());
+    }
+
+}
