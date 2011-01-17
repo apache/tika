e5b345e9ac316bb2619669a265ce7f3bee2e26e9
Jukka Zitting <jukka@apache.org>
2011-01-17 11:17:51 +0000

    TIKA-416: Out-of-process text extraction
    Make ParseContext more lightweight for easier serialization.
    Some ForkServer javadocs.
    
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1059877 13f79535-47bb-0310-9956-ffa450edef68

--- tika-core/src/main/java/org/apache/tika/parser/ParseContext.java
+++ tika-core/src/main/java/org/apache/tika/parser/ParseContext.java
@@ -38,23 +38,48 @@ import org.xml.sax.SAXNotSupportedException;
  */
 public class ParseContext implements Serializable {
 
-    /**
-     * Serial version UID.
-     */
+    /** Serial version UID. */
     private static final long serialVersionUID = -5921436862145826534L;
 
-    private final Map<Class<?>, Object> context =
-        new HashMap<Class<?>, Object>();
+    /** Map of objects in this context */
+    private final Map<String, Object> context = new HashMap<String, Object>();
  
+    /**
+     * Adds the given value to the context as an implementation of the given
+     * interface.
+     *
+     * @param key the interface implemented by the given value
+     * @param value the value to be added, or <code>null</code> to remove
+     */
     public <T> void set(Class<T> key, T value) {
-        context.put(key, value);
+        if (value != null) {
+            context.put(key.getName(), value);
+        } else {
+            context.remove(key.getName());
+        }
     }
 
+    /**
+     * Returns the object in this context that implements the given interface.
+     *
+     * @param key the interface implemented by the requested object
+     * @return the object that implements the given interface,
+     *         or <code>null</code> if not found
+     */
     @SuppressWarnings("unchecked")
     public <T> T get(Class<T> key) {
-        return (T) context.get(key);
+        return (T) context.get(key.getName());
     }
 
+    /**
+     * Returns the object in this context that implements the given interface,
+     * or the given default value if such an object is not found.
+     *
+     * @param key the interface implemented by the requested object
+     * @param defaultValue value to return if the requested object is not found
+     * @return the object that implements the given interface,
+     *         or the given default value if not found
+     */
     public <T> T get(Class<T> key, T defaultValue) {
         T value = get(key);
         if (value != null) {
