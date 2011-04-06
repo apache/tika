2821b706cc12838fce1730d8299107b3840b10be
Nick Burch <nick@apache.org>
2011-04-06 16:19:17 +0000

    TIKA-634 - Initial work on supporting more flexible ExternalParser loading (via XML, part done), and external parser metadata extraction
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1089516 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tika-core/src/main/java/org/apache/tika/parser/external/ExternalParsersConfigReader.java
@@ -0,0 +1,158 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.external;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.mime.MimeTypeException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Builds up ExternalParser instances based on XML file(s)
+ *  which define what to run, for what, and how to process
+ *  any output metadata.
+ * Typically used to configure up a series of external programs 
+ *  (like catdoc or pdf2txt) to extract text content from documents.
+ *  
+ * <pre>
+ *  TODO XML DTD Here
+ * </pre>
+ */
+public final class ExternalParsersConfigReader implements ExternalParsersConfigReaderMetKeys {
+   
+   public static List<ExternalParser> read(InputStream stream) throws TikaException, IOException {
+      try {
+          DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+          DocumentBuilder builder = factory.newDocumentBuilder();
+          Document document = builder.parse(new InputSource(stream));
+          return read(document);
+      } catch (ParserConfigurationException e) {
+          throw new TikaException("Unable to create an XML parser", e);
+      } catch (SAXException e) {
+          throw new TikaException("Invalid parser configuration", e);
+      }
+   }
+   
+   public static List<ExternalParser> read(Document document) throws TikaException, IOException {
+      return read(document.getDocumentElement());
+   }
+   
+   public static List<ExternalParser> read(Element element) throws TikaException, IOException {
+      List<ExternalParser> parsers = new ArrayList<ExternalParser>();
+      
+      if (element != null && element.getTagName().equals(EXTERNAL_PARSERS_TAG)) {
+         NodeList nodes = element.getChildNodes();
+         for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+               Element child = (Element) node;
+               if (child.getTagName().equals(PARSER_TAG)) {
+                  ExternalParser p = readParser(child);
+                  if(p != null) {
+                     parsers.add( p );
+                  }
+               }
+            }
+         }
+      } else {
+         throw new MimeTypeException(
+               "Not a <" + EXTERNAL_PARSERS_TAG + "/> configuration document: "
+               + element.getTagName());
+      }
+      
+      return parsers;
+   }
+   
+   /**
+    * Builds and Returns an ExternalParser, or null if a check
+    *  command was given that didn't match.
+    */
+   private static ExternalParser readParser(Element parserDef) throws TikaException {
+      ExternalParser parser = new ExternalParser();
+
+      NodeList children = parserDef.getChildNodes();
+      for(int i=0; i<children.getLength(); i++) {
+         Node node = children.item(i);
+         if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element child = (Element) node;
+            if (child.getTagName().equals(CHECK_TAG)) {
+               // TODO
+            }
+            else if (child.getTagName().equals(COMMAND_TAG)) {
+               parser.setCommand(
+                     child.getFirstChild().getNodeValue()
+               );
+            }
+            else if (child.getTagName().equals(MIMETYPES_TAG)) {
+               parser.setSupportedTypes(
+                     readMimeTypes(child)
+               );
+            }
+            else if (child.getTagName().equals(METADATA_TAG)) {
+               parser.setMetadataExtractionPatterns(
+                     readMetadataPatterns(child)
+               );
+            }
+         }
+      }
+      
+      return parser;
+   }
+   
+   private static Set<MediaType> readMimeTypes(Element mimeTypes) {
+      Set<MediaType> types = new HashSet<MediaType>();
+      return types;
+   }
+   
+   private static Map<Pattern,String> readMetadataPatterns(Element metadataDef) {
+      Map<Pattern, String> metadata = new HashMap<Pattern, String>();
+      
+      NodeList children = metadataDef.getChildNodes();
+      for(int i=0; i<children.getLength(); i++) {
+         Node node = children.item(i);
+         if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element child = (Element) node;
+            if (child.getTagName().equals(METADATA_MATCH_TAG)) {
+               String metadataKey = child.getAttribute(METADATA_KEY_ATTR);
+               Pattern pattern = Pattern.compile( child.getFirstChild().getNodeValue() );
+               metadata.put(pattern, metadataKey);
+            }
+         }
+      }
+      
+      return metadata;
+   }
+}
---
+++ tika-core/src/main/java/org/apache/tika/parser/external/ExternalParsersFactory.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.external;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.apache.tika.config.ServiceLoader;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.exception.TikaException;
+
+/**
+ * Creates instances of ExternalParser based on XML 
+ *  configuration files.
+ *  
+ * @see ExternalParsersConfigReader
+ */
+public class ExternalParsersFactory {
+   
+   public static List<ExternalParser> create() throws IOException, TikaException {
+      return create(new ServiceLoader());
+   }
+   
+   public static List<ExternalParser> create(ServiceLoader loader) 
+           throws IOException, TikaException {
+      return create("tika-external-parsers.xml", loader);
+   }
+   
+   public static List<ExternalParser> create(String filename, ServiceLoader loader) 
+           throws IOException, TikaException {
+      String filepath = ExternalParsersFactory.class.getPackage().getName().replace('.', '/') +
+                     "/" + filename;
+      Enumeration<URL> files = loader.findServiceResources(filepath);
+      ArrayList<URL> list = Collections.list(files);
+      URL[] urls = list.toArray(new URL[list.size()]);
+      return create(urls);
+   }
+   
+   public static List<ExternalParser> create(URL... urls) throws IOException, TikaException {
+      List<ExternalParser> parsers = new ArrayList<ExternalParser>();
+      for(URL url : urls) {
+         InputStream stream = url.openStream();
+         try {
+            parsers.addAll(
+                  ExternalParsersConfigReader.read(stream)
+            );
+         } finally {
+            stream.close();
+         }
+      }
+      return parsers;
+   }
+   
+   public static void attachExternalParsers(TikaConfig config) throws IOException, TikaException {
+      attachExternalParsers( create(), config );
+   }
+   
+   public static void attachExternalParsers(List<ExternalParser> parsers, TikaConfig config) {
+      // TODO
+   }
+}
