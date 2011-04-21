b44909d2f75f6a1a0580cfb0317e0d4d70401ec0
Nick Burch <nick@apache.org>
2011-04-21 16:07:32 +0000

    Remove un-used import
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1095763 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/main/java/org/apache/tika/parser/microsoft/OfficeParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/microsoft/OfficeParser.java
@@ -39,7 +39,6 @@ import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.microsoft.ooxml.OOXMLParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.apache.tika.sax.EmbeddedContentHandler;
