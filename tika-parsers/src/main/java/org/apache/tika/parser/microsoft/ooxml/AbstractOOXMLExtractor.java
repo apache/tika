ea21418703d689ad0fa926ca49a941076b5ac43a
Nick Burch <nick@apache.org>
2011-09-19 16:34:26 +0000

    TIKA-705 Temporary workaround for the relative links issue, pending upgrade to POI 3.8 beta 5
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1172690 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/main/java/org/apache/tika/parser/microsoft/ooxml/AbstractOOXMLExtractor.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/microsoft/ooxml/AbstractOOXMLExtractor.java
@@ -19,6 +19,7 @@ package org.apache.tika.parser.microsoft.ooxml;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.apache.poi.POIXMLDocument;
@@ -120,9 +121,21 @@ public abstract class AbstractOOXMLExtractor implements OOXMLExtractor {
             for (PackagePart source : getMainDocumentParts()) {
                 for (PackageRelationship rel : source.getRelationships()) {
                     if (rel.getTargetMode() == TargetMode.INTERNAL) {
+                        // TODO Simply this when on POI 3.8 beta 5
                         URI uri = rel.getTargetURI();
+                        if(uri.getFragment() != null) {
+                           // TODO Workaround for TIKA-705 needed until 3.8 beta 5
+                           try {
+                              String u = uri.toString();
+                              uri = new URI(u.substring(0, u.indexOf('#')));
+                           } catch(URISyntaxException e) {
+                              throw new TikaException("Broken OOXML file", e);
+                           }
+                        }
                         PackagePart target = rel.getPackage().getPart(
                                 PackagingURIHelper.createPartName(uri));
+                        // TODO Simpler version in POI 3.8 beta 5
+                        // PackagePart target = source.getRelatedPart(rel);
 
                         String type = rel.getRelationshipType();
                         if (RELATION_OLE_OBJECT.equals(type)
