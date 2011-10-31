4cb7d7b3dd4adb985266ddb4b2f009e62a636b74
Jukka Zitting <jukka@apache.org>
2011-10-31 15:00:55 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1195497 13f79535-47bb-0310-9956-ffa450edef68

--- tika-core/src/main/java/org/apache/tika/config/TikaConfig.java
+++ tika-core/src/main/java/org/apache/tika/config/TikaConfig.java
@@ -24,7 +24,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.imageio.spi.ServiceRegistry;
