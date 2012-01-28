fa10b69d9a0d19894d65d15dfb92d0087af13e56
Nick Burch <nick@apache.org>
2012-01-28 20:53:46 +0000

    TIKA-852 Initial MP4 Parser, powered by MP4Parser from Google Code
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1237162 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tika-parsers/src/main/java/org/apache/tika/parser/mp4/MP4Parser.java
@@ -0,0 +1,297 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.mp4;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.IOUtils;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.Property;
+import org.apache.tika.metadata.XMPDM;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AbstractParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import com.coremedia.iso.IsoBufferWrapper;
+import com.coremedia.iso.IsoBufferWrapperImpl;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.boxes.AbstractContainerBox;
+import com.coremedia.iso.boxes.Box;
+import com.coremedia.iso.boxes.FileTypeBox;
+import com.coremedia.iso.boxes.MetaBox;
+import com.coremedia.iso.boxes.MovieBox;
+import com.coremedia.iso.boxes.MovieHeaderBox;
+import com.coremedia.iso.boxes.TrackBox;
+import com.coremedia.iso.boxes.TrackHeaderBox;
+import com.coremedia.iso.boxes.UserDataBox;
+import com.coremedia.iso.boxes.apple.AbstractAppleMetaDataBox;
+import com.coremedia.iso.boxes.apple.AppleAlbumBox;
+import com.coremedia.iso.boxes.apple.AppleArtistBox;
+import com.coremedia.iso.boxes.apple.AppleCommentBox;
+import com.coremedia.iso.boxes.apple.AppleCustomGenreBox;
+import com.coremedia.iso.boxes.apple.AppleEncoderBox;
+import com.coremedia.iso.boxes.apple.AppleItemListBox;
+import com.coremedia.iso.boxes.apple.AppleRecordingYearBox;
+import com.coremedia.iso.boxes.apple.AppleStandardGenreBox;
+import com.coremedia.iso.boxes.apple.AppleTrackAuthorBox;
+import com.coremedia.iso.boxes.apple.AppleTrackNumberBox;
+import com.coremedia.iso.boxes.apple.AppleTrackTitleBox;
+
+/**
+ * Parser for the MP4 media container format, as well as the older
+ *  QuickTime format that MP4 is based on.
+ * 
+ * This uses the MP4Parser project from http://code.google.com/p/mp4parser/
+ *  to do the underlying parsing
+ */
+public class MP4Parser extends AbstractParser {
+    /** Serial version UID */
+    private static final long serialVersionUID = 84011216792285L;
+    
+    // Ensure this stays in Sync with the entries in tika-mimetypes.xml
+    private static final Map<MediaType,List<String>> typesMap = new HashMap<MediaType, List<String>>();
+    static {
+       // All types should be 4 bytes long, space padded as needed
+       typesMap.put(MediaType.audio("mp4"), Arrays.asList(
+             "M4A ", "M4B ", "F4A ", "F4B "));
+       typesMap.put(MediaType.video("3gpp"), Arrays.asList(
+             "3ge6", "3ge7", "3gg6", "3gp1", "3gp2", "3gp3", "3gp4", "3gp5", "3gp6", "3gs7"));
+       typesMap.put(MediaType.video("3gpp2"), Arrays.asList(
+             "3g2a", "3g2b", "3g2c"));
+       typesMap.put(MediaType.video("mp4"), Arrays.asList(
+             "mp41", "mp42"));
+       typesMap.put(MediaType.video("x-m4v"), Arrays.asList(
+             "M4V ", "M4VH", "M4VP"));
+       
+       typesMap.put(MediaType.video("quicktime"), Collections.<String>emptyList());
+       typesMap.put(MediaType.application("mp4"), Collections.<String>emptyList());
+    }
+
+    private static final Set<MediaType> SUPPORTED_TYPES =
+       Collections.unmodifiableSet(typesMap.keySet());
+
+    public Set<MediaType> getSupportedTypes(ParseContext context) {
+        return SUPPORTED_TYPES;
+    }
+
+
+    public void parse(
+            InputStream stream, ContentHandler handler,
+            Metadata metadata, ParseContext context)
+            throws IOException, SAXException, TikaException {
+        IsoBufferWrapper isoBufferWrapper = null;
+        TikaInputStream tstream = TikaInputStream.cast(stream);
+
+        // Have MP4Parser process the stream / file
+        if (tstream != null && tstream.hasFile()) {
+           isoBufferWrapper = new IsoBufferWrapperImpl(tstream.getFile());
+        } else {
+           isoBufferWrapper = new IsoBufferWrapperImpl(
+                 IOUtils.toByteArray(stream));
+        }
+        IsoFile isoFile = new IsoFile(isoBufferWrapper);
+        isoFile.parse();
+        
+        
+        // Grab the file type box
+        FileTypeBox fileType = getOrNull(isoFile, FileTypeBox.class);
+        if (fileType != null) {
+           // Identify the type
+           MediaType type = MediaType.application("mp4");
+           for (MediaType t : typesMap.keySet()) {
+              if (typesMap.get(t).contains(fileType.getMajorBrand())) {
+                 type = t;
+                 break;
+              }
+           }
+           metadata.set(Metadata.CONTENT_TYPE, type.toString());
+           
+           if (type.getType().equals("audio")) {
+              metadata.set(XMPDM.AUDIO_COMPRESSOR, fileType.getMajorBrand().trim());
+           }
+        } else {
+           // Some older QuickTime files lack the FileType
+           metadata.set(Metadata.CONTENT_TYPE, "video/quicktime");
+        }
+        
+        
+        // Get the main MOOV box
+        MovieBox moov = getOrNull(isoFile, MovieBox.class);
+        if (moov == null) {
+           // Bail out
+           return;
+        }
+
+        
+        XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
+        xhtml.startDocument();
+        
+        
+        // Pull out some information from the header box
+        MovieHeaderBox mHeader = getOrNull(moov, MovieHeaderBox.class);
+        if (mHeader != null) {
+           // Get the creation and modification dates
+           metadata.set(
+                 Metadata.CREATION_DATE, 
+                 MP4TimeToDate(mHeader.getCreationTime())
+           );
+           metadata.set(
+                 Property.externalDate(Metadata.MODIFIED), // TODO Should be a real property
+                 MP4TimeToDate(mHeader.getModificationTime())
+           );
+           
+           // Get the duration
+           double durationSeconds = ((double)mHeader.getDuration()) / mHeader.getTimescale();
+           // TODO Use this
+           
+           // The timescale is normally the sampling rate
+           metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int)mHeader.getTimescale());
+        }
+        
+        
+        // Get some more information from the track header
+        // TODO Decide how to handle multiple tracks
+        List<TrackBox> tb = moov.getBoxes(TrackBox.class);
+        if (tb.size() > 0) {
+           TrackHeaderBox header = getOrNull(tb.get(0), TrackHeaderBox.class);
+           if (header != null) {
+              // Get the creation and modification dates
+              metadata.set(
+                    Metadata.CREATION_DATE, 
+                    MP4TimeToDate(header.getCreationTime())
+              );
+              metadata.set(
+                    Property.externalDate(Metadata.MODIFIED), // TODO Should be a real property
+                    MP4TimeToDate(header.getModificationTime())
+              );
+              
+              // Get the video with and height
+              metadata.set(Metadata.IMAGE_WIDTH,  (int)header.getWidth());
+              metadata.set(Metadata.IMAGE_LENGTH, (int)header.getHeight());
+           }
+        }
+        
+        // Get metadata from the User Data Box
+        UserDataBox userData = getOrNull(moov, UserDataBox.class);
+        if (userData != null) {
+           MetaBox meta = getOrNull(userData, MetaBox.class);
+
+           // Check for iTunes Metadata
+           // See http://atomicparsley.sourceforge.net/mpeg-4files.html and
+           //  http://code.google.com/p/mp4v2/wiki/iTunesMetadata for more on these
+           AppleItemListBox apple = getOrNull(meta, AppleItemListBox.class);
+           if (apple != null) {
+              // Title
+              AppleTrackTitleBox title = getOrNull(apple, AppleTrackTitleBox.class);
+              addMetadata(Metadata.TITLE, metadata, title);
+
+              // Artist
+              AppleArtistBox artist = getOrNull(apple, AppleArtistBox.class);
+              addMetadata(Metadata.AUTHOR, metadata, artist);
+              addMetadata(XMPDM.ARTIST, metadata, artist);
+              
+              // Album
+              AppleAlbumBox album = getOrNull(apple, AppleAlbumBox.class);
+              addMetadata(XMPDM.ALBUM, metadata, album);
+              
+              // Composer
+              AppleTrackAuthorBox composer = getOrNull(apple, AppleTrackAuthorBox.class);
+              addMetadata(XMPDM.COMPOSER, metadata, composer);
+              
+              // Genre
+              AppleStandardGenreBox sGenre = getOrNull(apple, AppleStandardGenreBox.class);
+              AppleCustomGenreBox   cGenre = getOrNull(apple, AppleCustomGenreBox.class);
+              addMetadata(XMPDM.GENRE, metadata, sGenre);
+              addMetadata(XMPDM.GENRE, metadata, cGenre);
+              
+              // Year
+              AppleRecordingYearBox year = getOrNull(apple, AppleRecordingYearBox.class);
+              addMetadata(XMPDM.RELEASE_DATE, metadata, year);
+              
+              // Track number 
+              AppleTrackNumberBox trackNum = getOrNull(apple, AppleTrackNumberBox.class);
+              if (trackNum != null) {
+                 metadata.set(XMPDM.TRACK_NUMBER, trackNum.getTrackNumber());
+                 //metadata.set(XMPDM.NUMBER_OF_TRACKS, trackNum.getNumberOfTracks()); // TODO
+              }
+              
+              // Comment
+              AppleCommentBox comment = getOrNull(apple, AppleCommentBox.class);
+              addMetadata(XMPDM.LOG_COMMENT, metadata, comment);
+              
+              // Encoder
+              AppleEncoderBox encoder = getOrNull(apple, AppleEncoderBox.class);
+              // addMetadata(XMPDM.???, metadata, encoder); // TODO
+              
+              
+              // As text
+              for (Box box : apple.getBoxes()) {
+                 if (box instanceof AbstractAppleMetaDataBox) {
+                    xhtml.element("p", ((AbstractAppleMetaDataBox)box).getValue());
+                 }
+              }
+           }
+           
+           // TODO Check for other kinds too
+        }
+
+        // All done
+        xhtml.endDocument();
+    }
+    
+    private static void addMetadata(String key, Metadata m, AbstractAppleMetaDataBox metadata) {
+       if (metadata != null) {
+          m.add(key, metadata.getValue());
+       }
+    }
+    private static void addMetadata(Property prop, Metadata m, AbstractAppleMetaDataBox metadata) {
+       if (metadata != null) {
+          m.set(prop, metadata.getValue());
+       }
+    }
+    
+    /**
+     * MP4 Dates are stored as 32-bit integer, which represent the seconds 
+     * since midnight, January 1, 1904, and are generally in UTC 
+     */
+    private static Date MP4TimeToDate(long mp4Time) {
+       long unix = mp4Time - EPOC_AS_MP4_TIME;
+       return new Date(unix*1000);
+    }
+    private static final long EPOC_AS_MP4_TIME = 2082844800l;
+    
+    private static <T extends Box> T getOrNull(AbstractContainerBox box, Class<T> clazz) {
+       List<T> boxes = box.getBoxes(clazz);
+       if (boxes.size() == 0) {
+          return null;
+       }
+       return boxes.get(0);
+    }
+}
---
+++ tika-parsers/src/test/java/org/apache/tika/parser/mp4/MP4ParserTest.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.mp4;
+
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.XMPDM;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+
+/**
+ * Test case for parsing mp4 files.
+ * 
+ * TODO Work out why this test passes in Eclipse, but fails from Maven
+ */
+public abstract class MP4ParserTest extends TestCase {
+    /**
+     * Test that we can extract information from
+     *  a M4A MP4 Audio file
+     */
+    public void testMP4ParsingAudio() throws Exception {
+        Parser parser = new AutoDetectParser(); // Should auto-detect!
+        ContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+
+        InputStream stream = MP4ParserTest.class.getResourceAsStream(
+                "/test-documents/testMP4.m4a");
+        try {
+            parser.parse(stream, handler, metadata, new ParseContext());
+        } finally {
+            stream.close();
+        }
+
+        // Check core properties
+        assertEquals("audio/mp4", metadata.get(Metadata.CONTENT_TYPE));
+        assertEquals("Test Title", metadata.get(Metadata.TITLE));
+        assertEquals("Test Artist", metadata.get(Metadata.AUTHOR));
+        assertEquals("2012-01-28T18:39:18Z", metadata.get(Metadata.CREATION_DATE));
+        assertEquals("2012-01-28T18:40:25Z", metadata.get(Metadata.MODIFIED));
+
+        // Check the textual contents
+        String content = handler.toString();
+        assertTrue(content.contains("Test Title"));
+        assertTrue(content.contains("Test Artist"));
+        assertTrue(content.contains("Test Album"));
+        assertTrue(content.contains("2008"));
+        assertTrue(content.contains("Test Comment"));
+        assertTrue(content.contains("Test Genre"));
+        
+        // Check XMPDM-typed audio properties
+        assertEquals("Test Album", metadata.get(XMPDM.ALBUM));
+        assertEquals("Test Artist", metadata.get(XMPDM.ARTIST));
+        assertEquals("Test Composer", metadata.get(XMPDM.COMPOSER));
+        assertEquals("2008", metadata.get(XMPDM.RELEASE_DATE));
+        assertEquals("Test Genre", metadata.get(XMPDM.GENRE));
+        assertEquals("Test Comments", metadata.get(XMPDM.LOG_COMMENT.getName()));
+        assertEquals("1", metadata.get(XMPDM.TRACK_NUMBER));
+        
+        assertEquals("44100", metadata.get(XMPDM.AUDIO_SAMPLE_RATE));
+        //assertEquals("Stereo", metadata.get(XMPDM.AUDIO_CHANNEL_TYPE)); // TODO Extract
+        assertEquals("M4A", metadata.get(XMPDM.AUDIO_COMPRESSOR));
+        
+        
+        // Check again by file, rather than stream
+        TikaInputStream tstream = TikaInputStream.get(
+              MP4ParserTest.class.getResourceAsStream("/test-documents/testMP4.m4a"));
+        tstream.getFile();
+        try {
+           parser.parse(tstream, handler, metadata, new ParseContext());
+        } finally {
+           tstream.close();
+        }
+    }
+    
+    // TODO Test a MP4 Video file
+    // TODO Test an old QuickTime Video File
+}
