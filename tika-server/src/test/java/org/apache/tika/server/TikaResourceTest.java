dd0d3304bc7fcfad16b20f97241a1559cc409b68
Chris Mattmann <mattmann@apache.org>
2012-03-28 04:05:10 +0000

    - TIKA-593: improvement: use CXF client for test harnesses, remove all extraneous pom.xml dependencies and remove dep on commons-httpclient
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1306147 13f79535-47bb-0310-9956-ffa450edef68

--- tika-server/src/test/java/org/apache/tika/server/TikaResourceTest.java
+++ tika-server/src/test/java/org/apache/tika/server/TikaResourceTest.java
@@ -17,24 +17,25 @@
 
 package org.apache.tika.server;
 
-import java.io.IOException;
-import org.apache.commons.httpclient.HttpException;
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
 import org.apache.cxf.binding.BindingFactoryManager;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSBindingFactory;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 import org.junit.Test;
 
 public class TikaResourceTest extends CXFTestBase {
-	private static final String TIKA_PATH = "tika";
+	private static final String TIKA_PATH = "/tika";
 	public static final String TEST_DOC = "test.doc";
 	public static final String TEST_XLSX = "16637.xlsx";
 	private static final int UNPROCESSEABLE = 422;
-	private static final String service = "http://localhost:"
-			+ TikaServerCli.DEFAULT_PORT + "/";
 	private static final String endPoint = "http://localhost:"
-			+ TikaServerCli.DEFAULT_PORT + "/" + TIKA_PATH;
+			+ TikaServerCli.DEFAULT_PORT;
+	private static final String WADL_MEDIA_TYPE = "application/vnd.sun.wadl+xml";
 
 	private Server server;
 
@@ -49,7 +50,7 @@ public class TikaResourceTest extends CXFTestBase {
 		sf.setResourceClasses(TikaResource.class);
 		sf.setResourceProvider(TikaResource.class,
 				new SingletonResourceProvider(new TikaResource()));
-		sf.setAddress(service);
+		sf.setAddress(endPoint + "/");
 		BindingFactoryManager manager = sf.getBus().getExtension(
 				BindingFactoryManager.class);
 		JAXRSBindingFactory factory = new JAXRSBindingFactory();
@@ -58,7 +59,6 @@ public class TikaResourceTest extends CXFTestBase {
 				factory);
 		server = sf.create();
 	}
-	
 
 	/*
 	 * (non-Javadoc)
@@ -70,34 +70,43 @@ public class TikaResourceTest extends CXFTestBase {
 		server.stop();
 		server.destroy();
 	}
-	
 
 	@Test
 	public void testHelloWorld() throws Exception {
-		getAndCompare(endPoint, TikaResource.GREETING, "text/plain",
-				"text/plain", 200);
+		Response response = WebClient.create(endPoint + TIKA_PATH)
+				.type("text/plain").accept("text/plain").get();
+		assertEquals(TikaResource.GREETING,
+				getStringFromInputStream((InputStream) response.getEntity()));
 	}
 
 	@Test
 	public void testSimpleWord() throws Exception {
-		String responseMsg = putAndGetString(endPoint,
-				ClassLoader
-						.getSystemResourceAsStream(TikaResourceTest.TEST_DOC));
-
+		Response response = WebClient.create(endPoint + TIKA_PATH)
+				.type("application/msword")
+				.accept("text/plain")
+				.put(ClassLoader.getSystemResourceAsStream(TEST_DOC));
+		String responseMsg = getStringFromInputStream((InputStream) response
+				.getEntity());
 		assertTrue(responseMsg.contains("test"));
 	}
 
 	@Test
-	public void testApplicationWadl() throws HttpException, IOException {
-		String serviceWadl = endPoint + "/application.wadl";
-		String resp = getAndReturnResp(serviceWadl);
+	public void testApplicationWadl() throws Exception {
+		Response response = WebClient
+				.create(endPoint + TIKA_PATH + "/application.wadl")
+				.accept("text/plain").get();
+		String resp = getStringFromInputStream((InputStream) response
+				.getEntity());
 		assertTrue(resp.length() > 0);
 	}
 
 	@Test
 	public void testPasswordXLS() throws Exception {
-		putAndCheckStatus(endPoint,
-				ClassLoader.getSystemResourceAsStream("password.xls"),
-				UNPROCESSEABLE);
+		Response response = WebClient.create(endPoint + TIKA_PATH)
+				.type("application/vnd.ms-excel")
+				.accept("text/plain")
+				.put(ClassLoader.getSystemResourceAsStream("password.xls"));
+
+		assertEquals(UNPROCESSEABLE, response.getStatus());
 	}
 }
