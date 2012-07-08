b8beefd21e5b33dd824baca096080668e7cff27a
Jukka Zitting <jukka@apache.org>
2012-07-08 12:04:17 +0000

    TIKA-471: Avoid Charset name bottleneck when multiple threads are using HtmlParser
    Move common charset cache from UniversalEncodingDetector to CharsetUtils.
    Improve the performance of common code paths.
    
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1358724 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/main/java/org/apache/tika/parser/txt/UniversalEncodingListener.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/txt/UniversalEncodingListener.java
@@ -17,8 +17,6 @@
 package org.apache.tika.parser.txt;
 
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
@@ -34,64 +32,6 @@ import org.mozilla.universalchardet.UniversalDetector;
 class UniversalEncodingListener implements CharsetListener {
 
     private static final String CHARSET_ISO_8859_1 = "ISO-8859-1";
-
-    private static final String CHARSET_ISO_8859_15 = "ISO-8859-15";
-
-    private static Map<String, Charset> makeCC(String... names) {
-        Map<String, Charset> charsets = new HashMap<String, Charset>();
-        for (String name : names) {
-            try {
-                charsets.put(name, Charset.forName(name));
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-        return charsets;
-    }
-
-    private static final Map<String, Charset> CONSTANT_CHARSETS = makeCC(
-            Constants.CHARSET_BIG5,
-            Constants.CHARSET_EUC_JP,
-            Constants.CHARSET_EUC_KR,
-            Constants.CHARSET_EUC_TW,
-            Constants.CHARSET_GB18030,
-            Constants.CHARSET_HZ_GB_2312, // not supported?
-            Constants.CHARSET_IBM855,
-            Constants.CHARSET_IBM866,
-            Constants.CHARSET_ISO_2022_CN,
-            Constants.CHARSET_ISO_2022_JP,
-            Constants.CHARSET_ISO_2022_KR,
-            CHARSET_ISO_8859_1,
-            Constants.CHARSET_ISO_8859_5,
-            Constants.CHARSET_ISO_8859_7,
-            Constants.CHARSET_ISO_8859_8,
-            CHARSET_ISO_8859_15,
-            Constants.CHARSET_KOI8_R,
-            Constants.CHARSET_MACCYRILLIC,
-            Constants.CHARSET_SHIFT_JIS,
-            Constants.CHARSET_UTF_16BE,
-            Constants.CHARSET_UTF_16LE,
-            Constants.CHARSET_UTF_32BE, // not supported?
-            Constants.CHARSET_UTF_32LE, // not supported?
-            Constants.CHARSET_UTF_8,
-            Constants.CHARSET_WINDOWS_1251,
-            Constants.CHARSET_WINDOWS_1252,
-            Constants.CHARSET_WINDOWS_1253,
-            Constants.CHARSET_WINDOWS_1255,
-            Constants.CHARSET_X_ISO_10646_UCS_4_2143, // not supported?
-            Constants.CHARSET_X_ISO_10646_UCS_4_3412); // not supported?
-
-    private static Charset getCharset(String name) {
-        Charset charset = CONSTANT_CHARSETS.get(name);
-        if (charset == null) {
-            try {
-                charset = CharsetUtils.forName(name);
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-        return charset;
-    }
 
     private final UniversalDetector detector = new UniversalDetector(this);
 
@@ -121,7 +61,11 @@ class UniversalEncodingListener implements CharsetListener {
                 name = CHARSET_ISO_8859_1;
             }
         }
-        this.charset = getCharset(name);
+        try {
+            this.charset = CharsetUtils.forName(name);
+        } catch (Exception e) {
+            // ignore
+        }
     }
 
     public boolean isDone() {
