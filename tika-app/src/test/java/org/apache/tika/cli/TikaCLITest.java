7c284277f9e7fcec38a79c2f8f2cce3b224c1340
Michael McCandless <mikemccand@apache.org>
2012-12-02 18:29:59 +0000

    create temp files under tika-app/target for this test
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1416226 13f79535-47bb-0310-9956-ffa450edef68

--- tika-app/src/test/java/org/apache/tika/cli/TikaCLITest.java
+++ tika-app/src/test/java/org/apache/tika/cli/TikaCLITest.java
@@ -218,14 +218,14 @@ public class TikaCLITest extends TestCase{
 
     // TIKA-1031
     public void testZipWithSubdirs() throws Exception {
-        String[] params = {"-z", resourcePrefix + "testWithSubdirs.zip"};
+        String[] params = {"-z", "--extract-dir=target", resourcePrefix + "testWithSubdirs.zip"};
         new File("subdir/foo.txt").delete();
         new File("subdir").delete();
         TikaCLI.main(params);
         String content = outContent.toString();
         assertTrue(content.contains("Extracting 'subdir/foo.txt'"));
         // clean up. TODO: These should be in target.
-        new File("subdir/foo.txt").delete();
-        new File("subdir").delete();
+        new File("target/subdir/foo.txt").delete();
+        new File("target/subdir").delete();
     }
 }
