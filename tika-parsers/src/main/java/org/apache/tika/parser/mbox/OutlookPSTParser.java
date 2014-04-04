a0601736bac15172d80d6807b2d61cd209e87cc0
Hong-Thai Nguyen <thaichat04@apache.org>
2014-04-04 09:21:10 +0000

    TIKA-623 - Extract each mail as attachment
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1584574 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/main/java/org/apache/tika/parser/mbox/OutlookPSTParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/mbox/OutlookPSTParser.java
@@ -19,6 +19,7 @@ package org.apache.tika.parser.mbox;
 import static java.lang.String.valueOf;
 import static java.util.Collections.singleton;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +31,7 @@ import org.apache.tika.extractor.ParsingEmbeddedDocumentExtractor;
 import org.apache.tika.io.TemporaryResources;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
@@ -50,129 +52,146 @@ import com.pff.PSTMessage;
  */
 public class OutlookPSTParser extends AbstractParser {
 
+  private static final long serialVersionUID = 620998217748364063L;
 
-    private static final long serialVersionUID = 620998217748364063L;
+  private static final MediaType MS_OUTLOOK_PST_MIMETYPE = MediaType.application("vnd.ms-outlook-pst");
+  private static final Set<MediaType> SUPPORTED_TYPES = singleton(MS_OUTLOOK_PST_MIMETYPE);
 
-    private static final MediaType MS_OUTLOOK_PST_MIMETYPE = MediaType.application("vnd.ms-outlook-pst");
-    private static final Set<MediaType> SUPPORTED_TYPES = singleton(MS_OUTLOOK_PST_MIMETYPE);
+  public Set<MediaType> getSupportedTypes(ParseContext context) {
+    return SUPPORTED_TYPES;
+  }
 
-    public Set<MediaType> getSupportedTypes(ParseContext context) {
-        return SUPPORTED_TYPES;
+  public void parse(InputStream stream, ContentHandler handler, Metadata metadata, ParseContext context)
+      throws IOException, SAXException, TikaException {
+
+    // Use the delegate parser to parse the contained document
+    EmbeddedDocumentExtractor embeddedExtractor = context.get(EmbeddedDocumentExtractor.class,
+        new ParsingEmbeddedDocumentExtractor(context));
+
+    metadata.set(Metadata.CONTENT_TYPE, MS_OUTLOOK_PST_MIMETYPE.toString());
+
+    XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
+    xhtml.startDocument();
+
+    TikaInputStream in = TikaInputStream.get(stream);
+    try {
+      PSTFile pstFile = new PSTFile(in.getFile().getPath());
+      metadata.set(Metadata.CONTENT_LENGTH, valueOf(pstFile.getFileHandle().length()));
+      boolean isValid = pstFile.getFileHandle().getFD().valid();
+      metadata.set("isValid", valueOf(isValid));
+      if (isValid) {
+        parseFolder(xhtml, pstFile.getRootFolder(), embeddedExtractor);
+      }
+    } catch (Exception e) {
+      throw new TikaException(e.getMessage(), e);
     }
 
-    public void parse(InputStream stream, ContentHandler handler,
-            Metadata metadata, ParseContext context) throws IOException,
-            SAXException, TikaException {
+    xhtml.endDocument();
+  }
 
-        // Use the delegate parser to parse the contained document
-        EmbeddedDocumentExtractor embeddedExtractor = context.get(
-                EmbeddedDocumentExtractor.class,
-                new ParsingEmbeddedDocumentExtractor(context));
+  private void parseFolder(XHTMLContentHandler handler, PSTFolder pstFolder, EmbeddedDocumentExtractor embeddedExtractor)
+      throws Exception {
+    if (pstFolder.getContentCount() > 0) {
+      PSTMessage pstMail = (PSTMessage) pstFolder.getNextChild();
+      while (pstMail != null) {
+        AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute("", "class", "class", "CDATA", "embedded");
+        attributes.addAttribute("", "id", "id", "CDATA", pstMail.getInternetMessageId());
+        handler.startElement("div", attributes);
+        handler.element("h1", pstMail.getSubject());
 
-        metadata.set(Metadata.CONTENT_TYPE, MS_OUTLOOK_PST_MIMETYPE.toString());
+        parserMailItem(handler, pstMail, embeddedExtractor);
+        parseMailAttachments(handler, pstMail, embeddedExtractor);
 
-        XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
-        xhtml.startDocument();
+        handler.endElement("div");
 
-        TikaInputStream in = TikaInputStream.get(stream);
-        try {
-            PSTFile pstFile = new PSTFile(in.getFile().getPath());
-            metadata.set(Metadata.CONTENT_LENGTH, valueOf(pstFile.getFileHandle().length()));
-            boolean isValid = pstFile.getFileHandle().getFD().valid();
-            metadata.set("isValid", valueOf(isValid));
-            if (isValid) {
-                parseFolder(xhtml, pstFile.getRootFolder(), embeddedExtractor);
-            }
-        } catch (Exception e) {
-            throw new TikaException(e.getMessage());
+        pstMail = (PSTMessage) pstFolder.getNextChild();
+      }
+    }
+
+    if (pstFolder.hasSubfolders()) {
+      for (PSTFolder pstSubFolder : pstFolder.getSubFolders()) {
+        handler.startElement("div", createAttribute("class", "email-folder"));
+        handler.element("h1", pstSubFolder.getDisplayName());
+        parseFolder(handler, pstSubFolder, embeddedExtractor);
+        handler.endElement("div");
+      }
+    }
+  }
+
+  private void parserMailItem(XHTMLContentHandler handler, PSTMessage pstMail, EmbeddedDocumentExtractor embeddedExtractor) throws SAXException, IOException {
+    Metadata mailMetadata = new Metadata();
+    mailMetadata.set(Metadata.RESOURCE_NAME_KEY, pstMail.getInternetMessageId());
+    mailMetadata.set(Metadata.EMBEDDED_RELATIONSHIP_ID, pstMail.getInternetMessageId());
+    mailMetadata.set(TikaCoreProperties.IDENTIFIER, pstMail.getInternetMessageId());
+    mailMetadata.set(TikaCoreProperties.TITLE, pstMail.getSubject());
+    mailMetadata.set(Metadata.MESSAGE_FROM, pstMail.getSenderName());
+    mailMetadata.set(TikaCoreProperties.CREATOR, pstMail.getSenderName());
+    mailMetadata.set(TikaCoreProperties.CREATED, pstMail.getCreationTime());
+    mailMetadata.set(TikaCoreProperties.MODIFIED, pstMail.getLastModificationTime());
+    mailMetadata.set(TikaCoreProperties.COMMENTS, pstMail.getComment());
+    mailMetadata.set("descriptorNodeId", valueOf(pstMail.getDescriptorNodeId()));
+    mailMetadata.set("senderEmailAddress", pstMail.getSenderEmailAddress());
+    mailMetadata.set("recipients", pstMail.getRecipientsString());
+    mailMetadata.set("displayTo", pstMail.getDisplayTo());
+    mailMetadata.set("displayCC", pstMail.getDisplayCC());
+    mailMetadata.set("displayBCC", pstMail.getDisplayBCC());
+    mailMetadata.set("importance", valueOf(pstMail.getImportance()));
+    mailMetadata.set("priority", valueOf(pstMail.getPriority()));
+    mailMetadata.set("flagged", valueOf(pstMail.isFlagged()));
+
+    byte[] mailContent = pstMail.getBody().getBytes();
+    embeddedExtractor.parseEmbedded(new ByteArrayInputStream(mailContent), handler, mailMetadata, true);
+  }
+
+
+  private static AttributesImpl createAttribute(String attName, String attValue) {
+    AttributesImpl attributes = new AttributesImpl();
+    attributes.addAttribute("", attName, attName, "CDATA", attValue);
+    return attributes;
+  }
+
+  private void parseMailAttachments(XHTMLContentHandler xhtml, PSTMessage email, EmbeddedDocumentExtractor embeddedExtractor)
+      throws TikaException {
+    int numberOfAttachments = email.getNumberOfAttachments();
+    for (int i = 0; i < numberOfAttachments; i++) {
+      File tempFile = null;
+      try {
+        PSTAttachment attach = email.getAttachment(i);
+
+        // Get the filename; both long and short filenames can be used for attachments
+        String filename = attach.getLongFilename();
+        if (filename.isEmpty()) {
+          filename = attach.getFilename();
         }
 
-        xhtml.endDocument();
-    }
+        xhtml.element("p", filename);
 
-    private static void parseFolder(XHTMLContentHandler xhtml, PSTFolder pstFolder, EmbeddedDocumentExtractor embeddedExtractor) throws Exception {
-        if (pstFolder.getContentCount() > 0) {
-            PSTMessage pstMail = (PSTMessage) pstFolder.getNextChild();
-            while (pstMail != null) {
-                xhtml.startElement("div", createAttribute("class", "email-entry"));
-                xhtml.element("h1", pstMail.getInternetMessageId());
-                createMetadata(xhtml, "subject", pstMail.getSubject());
-                createMetadata(xhtml, "internetMessageId", valueOf(pstMail.getInternetMessageId()));
-                createMetadata(xhtml, "descriptorNodeId", valueOf(pstMail.getDescriptorNodeId()));
-                createMetadata(xhtml, "lastModificationTime", valueOf(pstMail.getLastModificationTime().getTime()));
-                createMetadata(xhtml, "senderName", pstMail.getSenderName());
-                createMetadata(xhtml, "senderEmailAddress", pstMail.getSenderEmailAddress());
-                createMetadata(xhtml, "recipients", pstMail.getRecipientsString());
-
-                xhtml.element("p", pstMail.getBody());
-                parseAttachments(xhtml, pstMail, embeddedExtractor);
-
-                xhtml.endElement("div");
-                pstMail = (PSTMessage) pstFolder.getNextChild();
-            }
+        Metadata attachMeta = new Metadata();
+        attachMeta.set(Metadata.RESOURCE_NAME_KEY, filename);
+        attachMeta.set(Metadata.EMBEDDED_RELATIONSHIP_ID, filename);
+        AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute("", "class", "class", "CDATA", "embedded");
+        attributes.addAttribute("", "id", "id", "CDATA", filename);
+        xhtml.startElement("div", attributes);
+        if (embeddedExtractor.shouldParseEmbedded(attachMeta)) {
+          TemporaryResources tmp = new TemporaryResources();
+          try {
+            TikaInputStream tis = TikaInputStream.get(attach.getFileInputStream(), tmp);
+            embeddedExtractor.parseEmbedded(tis, xhtml, attachMeta, true);
+          } finally {
+            tmp.dispose();
+          }
         }
+        xhtml.endElement("div");
 
-        if (pstFolder.hasSubfolders()) {
-            for (PSTFolder pstSubFolder : pstFolder.getSubFolders()) {
-                xhtml.startElement("div", createAttribute("class", "email-folder"));
-                xhtml.element("h1", pstSubFolder.getDisplayName());
-                parseFolder(xhtml, pstSubFolder, embeddedExtractor);
-                xhtml.endElement("div");
-            }
-        }
+      } catch (Exception e) {
+        throw new TikaException("Unable to unpack document stream", e);
+      } finally {
+        if (tempFile != null)
+          tempFile.delete();
+      }
     }
-
-    private static void createMetadata(XHTMLContentHandler xhtml, String metaName, String metaValue) throws SAXException {
-        xhtml.startElement("meta", createAttribute(metaName, metaValue));
-        xhtml.endElement("meta");
-    }
-
-    private static AttributesImpl createAttribute(String attName, String attValue) {
-        AttributesImpl attributes  = new AttributesImpl();
-        attributes.addAttribute("", attName, attName, "CDATA", attValue);
-        return attributes;
-    }
-
-    private static void parseAttachments(XHTMLContentHandler xhtml, PSTMessage email, EmbeddedDocumentExtractor embeddedExtractor) throws TikaException {
-        int numberOfAttachments = email.getNumberOfAttachments();
-        for (int i = 0; i < numberOfAttachments; i++) {
-            File tempFile = null;
-            try {
-                PSTAttachment attach = email.getAttachment(i);
-
-                // Get the filename; both long and short filenames can be used for attachments
-                String filename = attach.getLongFilename();
-                if (filename.isEmpty()) {
-                    filename = attach.getFilename();
-                }
-
-                xhtml.element("p", filename);
-
-                Metadata attachMeta = new Metadata();
-                attachMeta.set(Metadata.RESOURCE_NAME_KEY, filename);
-                attachMeta.set(Metadata.EMBEDDED_RELATIONSHIP_ID, filename);
-                AttributesImpl attributes = new AttributesImpl();
-                attributes.addAttribute("", "class", "class", "CDATA", "embedded");
-                attributes.addAttribute("", "id", "id", "CDATA", filename);
-                xhtml.startElement("div", attributes);
-                if (embeddedExtractor.shouldParseEmbedded(attachMeta)) {
-                    TemporaryResources tmp = new TemporaryResources();
-                    try {
-                        TikaInputStream tis = TikaInputStream.get(attach.getFileInputStream(), tmp);
-                        embeddedExtractor.parseEmbedded(tis, xhtml, attachMeta, true);
-                    } finally {
-                        tmp.dispose();
-                    }
-                }
-                xhtml.endElement("div");
-
-            } catch (Exception e) {
-                throw new TikaException("Unable to unpack document stream", e);
-            } finally {
-                if (tempFile != null)
-                    tempFile.delete();
-            }
-        }
-    }
+  }
 
 }
