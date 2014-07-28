80d85537c740c5daf483596b07fbf4140b30b74e
Tyler Palsulich <tpalsulich@apache.org>
2014-07-28 19:31:42 +0000

    Remove unused imports and redundant throws declarations in tika-server.
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1614143 13f79535-47bb-0310-9956-ffa450edef68

--- tika-server/src/main/java/org/apache/tika/server/TikaServerCli.java
+++ tika-server/src/main/java/org/apache/tika/server/TikaServerCli.java
@@ -17,10 +17,8 @@
 
 package org.apache.tika.server;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
