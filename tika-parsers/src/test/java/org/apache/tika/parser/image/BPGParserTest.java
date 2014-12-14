4b0161789c655f6f3d71ef1fbc81bf39a56cac37
Nick Burch <nick@apache.org>
2014-12-14 12:53:50 +0000

    TIKA-1495 Start on a BPG parser, so far just covering width and height (rest to follow later)
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1645454 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tika-parsers/src/test/java/org/apache/tika/parser/image/BPGParserTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.image;
+
+import java.io.InputStream;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.junit.Test;
+import org.xml.sax.helpers.DefaultHandler;
+
+import static junit.framework.Assert.assertEquals;
+
+public class BPGParserTest {
+    private final Parser parser = new BPGParser();
+
+    /**
+     * Tests a very basic file, without much metadata
+     */
+    @Test
+    public void testBPG() throws Exception {
+        Metadata metadata = new Metadata();
+        metadata.set(Metadata.CONTENT_TYPE, "image/x-bpg");
+        InputStream stream =
+            getClass().getResourceAsStream("/test-documents/testBPG.bpg");
+        parser.parse(stream, new DefaultHandler(), metadata, new ParseContext());
+
+        assertEquals("100", metadata.get(Metadata.IMAGE_WIDTH));
+        assertEquals("75", metadata.get(Metadata.IMAGE_LENGTH));
+        assertEquals("10", metadata.get(Metadata.BITS_PER_SAMPLE));
+    }
+}
