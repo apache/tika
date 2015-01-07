9f8fc77764e781f88afce7c13d36c9967caf4ae4
Nick Burch <nick@apache.org>
2015-01-07 15:31:26 +0000

    Disabled exif related bpg tests for TIKA-1495
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1650084 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/test/java/org/apache/tika/parser/image/BPGParserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/image/BPGParserTest.java
@@ -17,15 +17,20 @@
 package org.apache.tika.parser.image;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.Photoshop;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.junit.Test;
 import org.xml.sax.helpers.DefaultHandler;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BPGParserTest {
     private final Parser parser = new BPGParser();
@@ -63,7 +68,30 @@ public class BPGParserTest {
         assertEquals("10", metadata.get(Metadata.BITS_PER_SAMPLE));
         assertEquals("YCbCr Colour", metadata.get(Photoshop.COLOR_MODE));
         
-        // TODO Check comment data
+        // TODO Get the exif comment data to be properly extracted, see TIKA-1495
+        if (false) {
+            assertEquals("Tosteberga \u00C4ngar", metadata.get(TikaCoreProperties.TITLE));
+            assertEquals("Bird site in north eastern Sk\u00E5ne, Sweden.\n(new line)", metadata.get(TikaCoreProperties.DESCRIPTION));
+            List<String> keywords = Arrays.asList(metadata.getValues(Metadata.KEYWORDS));
+            assertTrue(keywords.contains("coast"));
+            assertTrue(keywords.contains("bird watching"));
+            assertEquals(keywords, Arrays.asList(metadata.getValues(TikaCoreProperties.KEYWORDS)));
+        }
+        
+        // TODO Get the exif data to be properly extracted, see TIKA-1495
+        if (false) {
+            assertEquals("1.0E-6", metadata.get(Metadata.EXPOSURE_TIME)); // 1/1000000
+            assertEquals("2.8", metadata.get(Metadata.F_NUMBER));
+            assertEquals("4.6", metadata.get(Metadata.FOCAL_LENGTH));
+            assertEquals("114", metadata.get(Metadata.ISO_SPEED_RATINGS));
+            assertEquals(null, metadata.get(Metadata.EQUIPMENT_MAKE));
+            assertEquals(null, metadata.get(Metadata.EQUIPMENT_MODEL));
+            assertEquals(null, metadata.get(Metadata.SOFTWARE));
+            assertEquals("1", metadata.get(Metadata.ORIENTATION));
+            assertEquals("300.0", metadata.get(Metadata.RESOLUTION_HORIZONTAL));
+            assertEquals("300.0", metadata.get(Metadata.RESOLUTION_VERTICAL));
+            assertEquals("Inch", metadata.get(Metadata.RESOLUTION_UNIT));          
+        }
     }
 
     /**
@@ -82,6 +110,25 @@ public class BPGParserTest {
         assertEquals("10", metadata.get(Metadata.BITS_PER_SAMPLE));
         assertEquals("YCbCr Colour", metadata.get(Photoshop.COLOR_MODE));
         
-        // TODO Check geographic data
+        // TODO Get the geographic data to be properly extracted, see TIKA-1495
+        if (false) {
+            assertEquals("12.54321", metadata.get(Metadata.LATITUDE));
+            assertEquals("-54.1234", metadata.get(Metadata.LONGITUDE));
+        }
+        
+        // TODO Get the exif data to be properly extracted, see TIKA-1495
+        if (false) {
+            assertEquals("6.25E-4", metadata.get(Metadata.EXPOSURE_TIME)); // 1/1600
+            assertEquals("5.6", metadata.get(Metadata.F_NUMBER));
+            assertEquals("false", metadata.get(Metadata.FLASH_FIRED));
+            assertEquals("194.0", metadata.get(Metadata.FOCAL_LENGTH));
+            assertEquals("400", metadata.get(Metadata.ISO_SPEED_RATINGS));
+            assertEquals("Canon", metadata.get(Metadata.EQUIPMENT_MAKE));
+            assertEquals("Canon EOS 40D", metadata.get(Metadata.EQUIPMENT_MODEL));
+            assertEquals("Adobe Photoshop CS3 Macintosh", metadata.get(Metadata.SOFTWARE));
+            assertEquals("240.0", metadata.get(Metadata.RESOLUTION_HORIZONTAL));
+            assertEquals("240.0", metadata.get(Metadata.RESOLUTION_VERTICAL));
+            assertEquals("Inch", metadata.get(Metadata.RESOLUTION_UNIT));
+        }
     }
 }
