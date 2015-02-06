8d9ddf9d7f2b5809b81e262fad3181a4dadb2e57
Tim Allison <tallison@apache.org>
2015-02-06 02:26:22 +0000

    TIKA-1542 substitute Apache friendly TTF test file for our current copyrighted file
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1657739 13f79535-47bb-0310-9956-ffa450edef68

--- tika-parsers/src/test/java/org/apache/tika/parser/font/FontParsersTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/font/FontParsersTest.java
@@ -16,28 +16,25 @@
  */
 package org.apache.tika.parser.font;
 
-import static org.junit.Assert.assertEquals;
 import static org.apache.tika.TikaTest.assertContains;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_FAMILY_NAME;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_FULL_NAME;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_NAME;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_SUB_FAMILY_NAME;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_VERSION;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_WEIGHT;
+import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_PS_NAME;
+import static org.junit.Assert.assertEquals;
 
-import java.util.TimeZone;
-
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
-import org.xml.sax.ContentHandler;
-import org.apache.tika.io.TikaInputStream;
 import org.junit.Test;
-
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_NAME;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_FULL_NAME;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_FAMILY_NAME;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_WEIGHT;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_VERSION;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_FONT_SUB_FAMILY_NAME;
-import static org.apache.tika.parser.font.AdobeFontMetricParser.MET_PS_NAME;
+import org.xml.sax.ContentHandler;
 
 /**
  * Test case for parsing various different font files.
@@ -86,38 +83,29 @@ public class FontParsersTest {
         ParseContext context = new ParseContext();
         TikaInputStream stream = TikaInputStream.get(
                 FontParsersTest.class.getResource(
-                        "/test-documents/testTrueType.ttf"));
-
-        //Pending PDFBOX-2122's integration (PDFBox 1.8.6)
-        //we must set the default timezone to something
-        //standard for this test.
-        //TODO: once we upgrade to PDFBox 1.8.6, remove
-        //this timezone code.
-        TimeZone defaultTimeZone = TimeZone.getDefault();
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-
+                        "/test-documents/testTrueType2.ttf"));
+        
         try {
             parser.parse(stream, handler, metadata, context);
         } finally {
-            //make sure to reset default timezone
-            TimeZone.setDefault(defaultTimeZone);
             stream.close();
         }
 
         assertEquals("application/x-font-ttf", metadata.get(Metadata.CONTENT_TYPE));
-        assertEquals("NewBaskervilleEF-Roman", metadata.get(TikaCoreProperties.TITLE));
+        assertEquals("Aclonica", metadata.get(TikaCoreProperties.TITLE));
 
-        assertEquals("1904-01-01T00:00:00Z",   metadata.get(Metadata.CREATION_DATE));
-        assertEquals("1904-01-01T00:00:00Z",   metadata.get(TikaCoreProperties.CREATED));
-        assertEquals("1904-01-01T00:00:00Z",   metadata.get(TikaCoreProperties.MODIFIED));
+        assertEquals("2011-04-21T07:59:00Z",   metadata.get(Metadata.CREATION_DATE));
+        assertEquals("2011-04-21T07:59:00Z",   metadata.get(TikaCoreProperties.CREATED));
+        assertEquals("2011-04-22T19:56:14Z",   metadata.get(TikaCoreProperties.MODIFIED));
         
-        assertEquals("NewBaskervilleEF-Roman", metadata.get(MET_FONT_NAME));
-        assertEquals("NewBaskerville",         metadata.get(MET_FONT_FAMILY_NAME));
+        assertEquals("Aclonica", metadata.get(MET_FONT_NAME));
+        assertEquals("Aclonica",         metadata.get(MET_FONT_FAMILY_NAME));
         assertEquals("Regular",                metadata.get(MET_FONT_SUB_FAMILY_NAME));
-        assertEquals("NewBaskervilleEF-Roman", metadata.get(MET_PS_NAME));
+        assertEquals("Aclonica-Regular", metadata.get(MET_PS_NAME));
         
         assertEquals("Copyright",           metadata.get("Copyright").substring(0, 9));
-        assertEquals("ITC New Baskerville", metadata.get("Trademark").substring(0, 19));
+        assertContains("http://www.apache.org/licenses/LICENSE-2.0.html", metadata.get("Copyright"));
+        assertEquals("Aclonica is a trade", metadata.get("Trademark").substring(0, 19));
         
         // Not extracted
         assertEquals(null, metadata.get(MET_FONT_FULL_NAME));
