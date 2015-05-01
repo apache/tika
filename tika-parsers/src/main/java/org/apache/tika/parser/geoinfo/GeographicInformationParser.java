66ba03ee85946d7babf9815b9734f0ee83b4767f
unknown <gautham.g44@gmail.com>
2015-04-30 22:34:38 -0700

    fix for TIKA-443 contributed by gautham.g44@gmail.com

--- tika-parsers/src/main/java/org/apache/tika/parser/geoinfo/GeographicInformationParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/geoinfo/GeographicInformationParser.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.tika.parser.geoinfo;
 
 import org.apache.sis.internal.util.CheckedArrayList;
@@ -235,7 +252,7 @@ public class GeographicInformationParser extends AbstractParser{
             }
             CheckedHashSet<Locale> localeCheckedHashSet= (CheckedHashSet<Locale>) defaultDataIdentification.getLanguages();
             for(Locale l:localeCheckedHashSet){
-                metadata.add("IdentificationInfoLanguage-->",l.getDisplayLanguage());
+                metadata.add("IdentificationInfoLanguage-->",l.getDisplayLanguage(Locale.ENGLISH));
             }
             CodeListSet<TopicCategory> categoryList= (CodeListSet<TopicCategory>) defaultDataIdentification.getTopicCategories();
             for(TopicCategory t:categoryList){
