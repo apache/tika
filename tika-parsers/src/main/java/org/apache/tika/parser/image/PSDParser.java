b3656b0c0f2ce2b7508086d64462ad82c9505556
Kevin <kevin@meta.sc>
2016-01-09 21:02:28 -0500

    Extract layer names and some text fields

--- tika-parsers/src/main/java/org/apache/tika/parser/image/PSDParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/image/PSDParser.java
@@ -26,10 +26,7 @@ import java.util.Set;
 import org.apache.poi.util.IOUtils;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.EndianUtils;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.metadata.Photoshop;
-import org.apache.tika.metadata.TIFF;
-import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.metadata.*;
 import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
@@ -128,13 +125,275 @@ public class PSDParser extends AbstractParser {
             }
         }
 
-        // Next is the Layer and Mask Info
+        // Using http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577409_pgfId-1031423
+        // for information about the Layer and Mask Section
+
+        // This size is not needed so we skip it
+        // long layersAndMaskSectionSize = EndianUtils.readIntBE(stream);
+        stream.skip(4);
+
+        // Layer info section contains layer names
+        long layerInfoSectionSize = EndianUtils.readIntBE(stream);
+        // Rounds up to nearest even number of bytes
+        layerInfoSectionSize = (layerInfoSectionSize + 1) & ~0x01;
+        read = 0;
+
+        // From PSD spec:
+        // If [layer count] is a negative number, its absolute
+        // value is the number of layers and the first alpha channel contains
+        // the transparency data for the merged result.
+        long layerCount = Math.abs(EndianUtils.readShortBE(stream));
+        read += 2;
+
+        // Holds names of layers
+        String[] names = new String[(int)layerCount];
+        String[] textFields = new String[(int)layerCount];
+        for (int i = 0; i < layerCount; i++) {
+            textFields[i] = "";
+        }
+        int layers = 0;
+        long readMarker;
+        int tempNumber;
+
+        while (read < layerInfoSectionSize && layers < layerCount) {
+            // 16 bytes - object coordinates
+            read += stream.skip(16);
+
+            // 2 byes - number of channels in layer (retrieve)
+            long channelCount = EndianUtils.readShortBE(stream);
+            read += 2;
+
+            // 6 * number of channels - channel info
+            read += stream.skip(6*channelCount);
+
+            // ----------- skip 12 bytes for this block
+            // 4 bytes - blend mode signature
+            // 4 bytes - blend mode key
+            // 1 byte - opacity
+            // 1 byte - clipping
+            // 1 byte - flags
+            // 1 byte - filler
+            // -----------
+            read += stream.skip(12);
+
+            // 4 bytes - length of extra data field (retrieve)
+            long extraDataFieldLength = EndianUtils.readIntBE(stream);
+            read += 4;
+            readMarker = read;
+
+            // 4 bytes - length of mask/adjustment layer (retrieve and skip)
+            long maskAdjustmentLength = EndianUtils.readIntBE(stream);
+            // 4 bytes - length of layer blending ranges (retrieve and skip)
+            long layerBlendingLength = EndianUtils.readIntBE(stream);
+            read += 8;
+            read += stream.skip(maskAdjustmentLength+layerBlendingLength);
+
+            // Get the size of the name string
+            int stringSize = stream.read();
+//            System.out.printf("\n\nstringSize: %d\n", stringSize);
+            read += 1;
+            // Read name string bytes into array
+            byte[] nameBytes = new byte[stringSize];
+            read += stream.read(nameBytes);
+//            System.out.printf("nameBytes: %s\n", new String(nameBytes));
+            // Convert the bytes into string
+            String layerName = new String(nameBytes, "UTF-8");
+//            System.out.printf("layerName: %s\n", layerName);
+
+            // Consume the remainder of the name section
+            int nameLength = layerName.length() + 1;
+            int skipSize;
+            if (nameLength%4 != 0) {
+                skipSize = 4 - nameLength%4;
+//                System.out.printf("skipSize: %d", skipSize);
+                read += stream.skip(skipSize);
+            }
+
+//            System.out.printf("extraDataFieldLength: %d, read: %d, readMarker: %d\n", extraDataFieldLength, read, readMarker);
+
+            // The additional layer information begins with a 4 byte signature
+            byte[] extraBytesSignature = new byte[4];
+            read += stream.read(extraBytesSignature);
+            String additionalLayerInfoSignature = new String(extraBytesSignature, "UTF-8");
+//            System.out.println("additionalLayerInfoSignature: "+additionalLayerInfoSignature);
+            if (additionalLayerInfoSignature.equals("8BIM")) {
+                // Get the key for this info -- 4 bytes
+                byte[] infoKeyBytes = new byte[4];
+                read += stream.read(infoKeyBytes);
+                String infoKey = new String(infoKeyBytes, "UTF-8");
+//                System.out.println("infoKey: "+infoKey);
+                if (infoKey.equals("TySh")) {
+                    // 4 bytes give size of additional layer info
+                    int additionalLayerInfoSize = EndianUtils.readIntBE(stream);
+                    additionalLayerInfoSize = (additionalLayerInfoSize + 1) & ~0x01;
+                    read += 4;
+//                    System.out.printf("additionalLayerInfoSize: %d\n", additionalLayerInfoSize);
+
+
+                    // WORKS, kind of **************************
+                    byte[] additionalDataBytes = new byte[additionalLayerInfoSize];
+                    read += stream.read(additionalDataBytes);
+
+                    // Get the index where the four bytes "TEXT" begin.
+                    String additionalData = new String(additionalDataBytes, "UTF-8");
+                    tempNumber = additionalData.indexOf("TEXT");
+
+//                    byte[] textKey = Arrays.copyOfRange(additionalDataBytes, tempNumber, tempNumber+4);
+//                    System.out.printf("textKey: %s\n", new String(textKey));
+
+                    // Isolate the bytes which tell us the size of the unicode string
+                    byte[] textSize = Arrays.copyOfRange(additionalDataBytes, tempNumber+4, tempNumber+8);
+//                    System.out.printf("textSize: %d\n", EndianUtils.getIntBE(textSize));
+
+                    // Get the number of bytes the text spans
+                    long textSpan = EndianUtils.getIntBE(textSize)*2;
+
+                    if (textSpan > 0 && textSpan <= Integer.MAX_VALUE ) {
+
+                        byte[] text = Arrays.copyOfRange(additionalDataBytes, tempNumber+8, tempNumber+8+(int)textSpan);
+                        //                    System.out.printf("theRest: %s\n", new String(text));
+
+                        textFields[layers] = new String(text, "UNICODE");
+
+                    }
+                    // *******************************************
+
+                    // // // //
+                    //
+                    // Below is the proper code which does not work and
+                    // it sits here hoping to be figured out some day
+                    //
+                    // // // //
+
+//                    byte[] theRest = Arrays.copyOfRange(additionalDataBytes, tempNumber+8, additionalDataBytes.length-1);
+//                    System.out.printf("theRest: %s\n", new String(theRest));
+//
+//                    // Enter-- Additional layer information "Data" section
+//
+//                    // 2 bytes version
+//                    // 6 * 8 bytes transform stuff
+//                    // 2 bytes text version ( == 50 for ps 6.0)
+//                    // 4 bytes descriptor version ( == 16 for ps 6.0)
+//                    read += stream.skip(56);
+//
+//                    // ****** Text Data ************
+//
+//
+//                    // "Unicode string"
+//                    // 4 bytes for number of chars, then 2 bytes per char ?'name from classID'?
+//                    tempNumber = EndianUtils.readIntBE(stream);
+////                    System.out.printf("lengthOfUnicodeClassID: %d\n", tempNumber);
+//                    read += 4;
+//                    byte[] tmpBytes = new byte[tempNumber*2];
+//                    read += stream.read(tmpBytes);
+////                    System.out.printf("nameFromClassID: %s\n", new String(tmpBytes));
+////                    read += stream.skip(tempNumber*2);
+//
+//                    // 4 bytes for length of...
+//                    // ^^ if zero, 4 byte classID
+//                    // ^^ not zero, consume length
+//                    tempNumber = EndianUtils.readIntBE(stream);
+////                    System.out.printf("lengthOfClassID: %d\n", tempNumber);
+//                    read += 4;
+//                    if (tempNumber == 0) {
+//                        read += stream.skip(4);
+//                    } else {
+//                        read += stream.skip(tempNumber);
+//                    }
+//
+//                    // 4 bytes for number of items in descriptor
+//                    int numberOfItems = EndianUtils.readIntBE(stream);
+//                    read += 4;
+////                    System.out.printf("numberOfItems: %d\n", numberOfItems);
+//                    int items = 0;
+//                    assert(numberOfItems<50);
+//                    while (items < numberOfItems) {
+//
+//                        // 4 bytes length of item -- if not zero, is not key & consume
+//                        // No length is given for key unless key is supported,
+//                        // must skip all if first is not "TEXT"
+//                        // TODO: Support all the keys even though only one is something we want
+//                        tempNumber = EndianUtils.readIntBE(stream);
+////                        System.out.printf("lengthOfItem: %d\n", tempNumber);
+//                        read += 4;
+//                        if (tempNumber != 0) {
+//                            read += stream.skip(tempNumber);
+//                            items++;
+//                            continue;
+//                        }
+//
+//                        // 4 bytes for item key
+//                        byte[] itemKeyBytes = new byte[8];
+//                        read += stream.read(itemKeyBytes);
+//                        String itemKey = new String(itemKeyBytes, "UTF-8");
+////                        System.out.println("itemKey: "+itemKey);
+//                        // match key to "TEXT"
+                          // Note: In practice it appears that the key is "Txt TEXT" not "TEXT"-- this is undocumented
+//                        if (itemKey.equals("Txt TEXT")) {
+//                            // 4 bytes for number of characters in the string
+//                            tempNumber = EndianUtils.readIntBE(stream);
+//                            read += 4;
+//                            // unicode string, 2 bytes per char
+//                            byte[] textBoxBytes = new byte[tempNumber*2];
+//                            read += stream.read(textBoxBytes);
+//                            textFields[layers] = new String(textBoxBytes, "UNICODE");
+////                            System.out.printf("text field: %s\n", textFields[layers]);
+//                        } else {
+//                            // TODO: See the previous todo, the one about supporting keys
+////                            break;
+//                            break;
+//                        }
+//                        items++;
+//                    }
+//
+//
+//                    // ******************************
+//
+//                    // 2 bytes warp version
+//                    // 4 bytes descriptor version
+//                    // variable for descriptor -- 4 bytes to get size, then consume size
+//                    // 4 * 8 bytes for something
+
+                }
+            }
+
+
+            // Consume unread data so the next iteration wont be fucked
+//            System.out.printf("extraDataFieldLength: %d, read: %d, readMarker: %d", extraDataFieldLength, read, readMarker);
+            if (extraDataFieldLength - (read-readMarker) > 0) {
+                byte[] extraBytes = new byte[(int) (extraDataFieldLength - (read - readMarker))];
+                read += stream.read(extraBytes);
+            }
+//            System.out.printf("extraBytes:\nlength: %d\ncontent: %s\n", extraBytes.length, new String(extraBytes));
+
+            // Consume the rest of the section
+//            read += stream.skip(extraDataFieldLength - (read-readMarker));
+
+            names[layers] = layerName;
+            layers++;
+
+        }
+//        System.out.printf("read: %d, total: %d\n", read, layerInfoSectionSize);
+//        System.out.println("Done with PSD Layers");
+        String csvNames = "";
+        for (String name : names) {
+            csvNames += (name + ",");
+        }
+        metadata.set(PSD.LAYER_NAMES, csvNames);
         // Finally we have Image Data
         // We can't do anything with these parts
 
-        // We don't have any helpful text, sorry...
+        // Add the text layer bodies as the body of text
         XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
         xhtml.startDocument();
+        xhtml.startElement("p");
+        for (String text : textFields) {
+            if (text.length() > 0) {
+                xhtml.characters(text.toCharArray(), 0, text.toCharArray().length);
+                xhtml.newline();
+            }
+        }
+        xhtml.endElement("p");
         xhtml.endDocument();
     }
 
