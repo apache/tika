d621ce8d4553e300742c0f64b0e1d95143783332
Ken Krugler <ken@transpac.com>
2016-02-06 16:42:54 -0800

    Roll in new lang detect support in new module
    This is one of several steps. Next is to get rid of the old support for
    language detection (in tika-core), and fix up all of those dependencies.

---
+++ tika-langdetect/src/main/java/org/apache/tika/langdetect/LanguageResult.java
@@ -0,0 +1,75 @@
+package org.apache.tika.langdetect;
+
+import java.util.Locale;
+
+public class LanguageResult {
+
+	private String language;
+	
+	private LanguageConfidence confidence;
+	
+	// rawScore should be a number from 0.0 to 1.0, with higher values implying
+	// greater confidence.
+	private float rawScore;
+	
+	/**
+	 * 
+	 * @param language ISO 639-1 language code (plus optional "-<country code>")
+	 * @param rawScore confidence of detector in the result.
+	 */
+	public LanguageResult(String language, LanguageConfidence confidence, float rawScore) {
+		this.language = language;
+		this.confidence = confidence;
+		this.rawScore = rawScore;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public float getRawScore() {
+		return rawScore;
+	}
+	
+	public LanguageConfidence getConfidence() {
+		return confidence;
+	}
+	
+	public boolean isReasonablyCertain() {
+		return confidence == LanguageConfidence.HIGH;
+	}
+	
+	/**
+	 * Return true if the target language matches the detected language. We consider
+	 * it a match if, for the precision requested or detected, it matches. This means:
+	 * 
+	 * target	|	detected	| match?
+	 * zh		|	en			| false
+	 * zh		|	zh			| true
+	 * zh		|	zh-CN		| true
+	 * zh-CN	|	zh			| true
+	 * zh-CN	|	zh-TW		| false
+	 * zh-CN	|	zh-cn		| true (case-insensitive)
+	 * 
+	 * @param language
+	 * @return
+	 */
+	public boolean isLanguage(String language) {
+		String[] targetLanguage = language.split("\\-");
+		String[] resultLanguage = this.language.split("\\-");
+		
+		int minLength = Math.min(targetLanguage.length, resultLanguage.length);
+		for (int i = 0; i < minLength; i++) {
+			if (!targetLanguage[i].equalsIgnoreCase(resultLanguage[i])) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(Locale.US, "%s: %s (%f)", language, confidence, rawScore);
+	}
+}
---
+++ tika-langdetect/src/test/java/org/apache/tika/langdetect/LanguageDetectorTest.java
@@ -0,0 +1,76 @@
+package org.apache.tika.langdetect;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tika.io.IOUtils;
+
+public abstract class LanguageDetectorTest {
+
+    protected String[] getTestLanguages() throws IOException {
+    	List<String> result = new ArrayList<>();
+    	
+    	List<String> lines = IOUtils.readLines(LanguageDetector.class.getResourceAsStream("language-codes.txt"));
+    	for (String line : lines) {
+    		line = line.trim();
+    		if (line.isEmpty() || line.startsWith("#")) {
+    			continue;
+    		}
+    		
+    		String[] parsed = line.split("\t");
+    		String language = parsed[0];
+    		if (hasTestLanguage(language)) {
+    			result.add(language);
+    		}
+    	}
+    	
+    	return result.toArray(new String[result.size()]);
+    }
+    
+
+    protected boolean hasTestLanguage(String language) {
+        InputStream stream = LanguageDetectorTest.class.getResourceAsStream("/language-tests/" + language + ".test");
+        if (stream != null) {
+        	IOUtils.closeQuietly(stream);
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+    
+    protected void writeTo(String language, Writer writer) throws IOException {
+    	writeTo(language, writer, Integer.MAX_VALUE);
+    }
+
+    protected void writeTo(String language, Writer writer, int limit) throws IOException {
+        InputStream stream = LanguageDetectorTest.class.getResourceAsStream("/language-tests/" + language + ".test");
+        
+        try {
+        	copyAtMost(new InputStreamReader(stream, UTF_8), writer, limit);
+        } finally {
+            stream.close();
+        }
+    }
+
+    protected int copyAtMost(Reader input, Writer output, int limit) throws IOException {
+        char[] buffer = new char[4096];
+        int count = 0;
+        int n = 0;
+        
+        while ((-1 != (n = input.read(buffer))) && (count < limit)) {
+        	int bytesToCopy = Math.min(limit - count, n);
+            output.write(buffer, 0, bytesToCopy);
+            count += bytesToCopy;
+        }
+        
+        return count;
+    }
+
+}
