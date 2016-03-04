e5d348ddb9727e67666d39eb1b10e6feb633022c
nandan-pc <nandan.pc@gmail.com>
2016-03-03 21:21:26 -0800

    Fix for TIKA-1886 provided by nandan-pc

--- tika-parsers/src/test/java/org/apache/tika/mime/TestMimeTypes.java
+++ tika-parsers/src/test/java/org/apache/tika/mime/TestMimeTypes.java
@@ -171,7 +171,12 @@ public class TestMimeTypes {
                 // no need for the name-based detection to refine it
                 "application/x-tika-msworks-spreadsheet");
     }
-    
+
+    @Test
+    public void testHFATypes() throws Exception {
+        assertTypeByData("application/x-erdas-hfa","39235201F864F0E1732D126BD35E6E4E2A634C6CF0B63594C6209AB86DB98C50");
+    }
+
     @Test
     public void testStarOfficeDetection() throws Exception {
         assertTypeDetection("testVORCalcTemplate.vor",
