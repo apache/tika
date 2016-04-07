61d8ec7534e91bdc994456e1883a5d07abf2037c
tballison <tallison@mitre.org>
2016-04-07 15:32:15 -0400

    TIKA-1933

--- tika-core/src/main/java/org/apache/tika/fork/ForkClient.java
+++ tika-core/src/main/java/org/apache/tika/fork/ForkClient.java
@@ -16,6 +16,8 @@
  */
 package org.apache.tika.fork;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -32,8 +34,6 @@ import java.util.zip.ZipEntry;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.IOUtils;
 import org.xml.sax.ContentHandler;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 class ForkClient {
 
@@ -174,6 +174,12 @@ class ForkClient {
         }
         if (process != null) {
             process.destroy();
+            try {
+                //TIKA-1933
+                process.waitFor();
+            } catch (InterruptedException e) {
+
+            }
         }
         if (jar != null) {
             jar.delete();
