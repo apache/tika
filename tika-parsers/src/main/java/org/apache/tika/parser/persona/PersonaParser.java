2d5359c0944c263ca17627faa4e578860d9097ac
Shiven Saiwal <shivensaiwal@guest-wireless-upc-1605-10-120-080-164.usc.edu>
2016-04-22 16:58:03 -0700

    Persona Parser Created

--- tika-parsers/src/main/java/org/apache/tika/parser/persona/PersonaParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/persona/PersonaParser.java
@@ -1,3 +1,16 @@
+/*
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
 package org.apache.tika.parser.persona;
 
 import java.io.BufferedReader;
@@ -46,25 +59,34 @@ public class PersonaParser extends AbstractParser {
 			throws IOException, SAXException, TikaException {
 		// TODO Auto-generated method stub
 		
+		//Reading the property file containing the authentication information.
+		Properties prop = new Properties();
+		InputStream input = this.getClass().getResourceAsStream("personaparser.properties");
+		try
+		{
+			prop.load(input);	
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Config properties not found");
+		}
+		
+		String user = prop.getProperty("user");
+		String password = prop.getProperty("password");
+		
+		//Reading a text file having the input to the parser.
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 		ArrayList<String> data = new ArrayList<String>();
 		String line = reader.readLine();
 		while(line != null){
            
             data.add(line);
-            line = reader.readLine();
-            
-            
+            line = reader.readLine();    
         }           
 		
-	
-		String host = data.get(0);
-		Properties prop = new Properties();
-		InputStream input = PersonaParser.class.getClassLoader().getResourceAsStream("org/apache/tika/parser/persona/personaparser.properties");
-		prop.load(input);
 		
-		String user = prop.getProperty("user");
-		String password = prop.getProperty("password");
+		String host = data.get(0);
 		String start = data.get(1);
 		String rows = data.get(2);
 		StringBuilder builder = new StringBuilder();
