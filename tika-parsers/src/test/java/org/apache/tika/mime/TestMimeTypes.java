acf031a06faac4061eced224dfabacba6e6fed8f
tballison <tallison@mitre.org>
2016-06-15 10:07:06 -0400

    TIKA-2009 -- add mime magic for djvu

--- tika-parsers/src/test/java/org/apache/tika/mime/TestMimeTypes.java
+++ tika-parsers/src/test/java/org/apache/tika/mime/TestMimeTypes.java
@@ -1048,6 +1048,12 @@ public class TestMimeTypes {
         assertType("application/x-ms-owner", "testMSOwnerFile");
     }
 
+    @Test
+    public void testDJVU() throws Exception {
+        assertType("image/vnd.djvu", "testDJVU.djvu");
+        assertTypeByData("image/vnd.djvu", "testDJVU.djvu");
+    }
+
     private void assertText(byte[] prefix) throws IOException {
         assertMagic("text/plain", prefix);
     }
