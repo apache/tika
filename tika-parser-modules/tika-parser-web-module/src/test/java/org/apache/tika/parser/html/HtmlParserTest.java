09bd22fb4bd67979070e1f57d43f6a4bff755ff0
tballison <tallison@mitre.org>
2016-08-12 12:12:37 -0400

    TIKA-1938 via Joseph Naegele

--- tika-parser-modules/tika-parser-web-module/src/test/java/org/apache/tika/parser/html/HtmlParserTest.java
+++ tika-parser-modules/tika-parser-web-module/src/test/java/org/apache/tika/parser/html/HtmlParserTest.java
@@ -37,6 +37,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -1127,6 +1128,45 @@ public class HtmlParserTest extends TikaTest {
         assertEquals("application/xhtml+xml; charset=ISO-8859-1", metadata.get(Metadata.CONTENT_TYPE));
 
     }
+
+    @Test
+    public void testScriptSrc() throws Exception {
+        String url = "http://domain.com/logic.js";
+        String scriptInBody =
+                "<html><body><script src=\"" + url + "\"></script></body></html>";
+        String scriptInHead =
+                "<html><head><script src=\"" + url + "\"></script></head></html>";
+
+        assertScriptLink(scriptInBody, url);
+        assertScriptLink(scriptInHead, url);
+    }
+
+    private void assertScriptLink(String html, String url) throws Exception {
+        // IdentityHtmlMapper is needed to extract <script> tags
+        ParseContext context = new ParseContext();
+        context.set(HtmlMapper.class, IdentityHtmlMapper.INSTANCE);
+        Metadata metadata = new Metadata();
+        metadata.set(Metadata.CONTENT_TYPE, "text/html");
+
+        final List<String> links = new ArrayList<String>();
+        new HtmlParser().parse(
+                new ByteArrayInputStream(html.getBytes(UTF_8)),
+                new DefaultHandler() {
+                    @Override
+                    public void startElement(
+                            String u, String l, String name, Attributes atts) {
+                        if (name.equals("script") && atts.getValue("", "src") != null) {
+                            links.add(atts.getValue("", "src"));
+                        }
+                    }
+                },
+                metadata,
+                context);
+
+        assertEquals(1, links.size());
+        assertEquals(url, links.get(0));
+    }
+
 
     @Test
     public void testSkippingCommentsInEncodingDetection() throws Exception {
