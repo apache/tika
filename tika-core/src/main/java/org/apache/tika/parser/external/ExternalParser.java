011f33807e7e794c2c06154774ec167b818930f2
Konstantin Gribov <grossws@gmail.com>
2016-10-31 20:31:17 +0300

    TIKA-2056 Fixup (forgotten import)

--- tika-core/src/main/java/org/apache/tika/parser/external/ExternalParser.java
+++ tika-core/src/main/java/org/apache/tika/parser/external/ExternalParser.java
@@ -24,6 +24,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
