2df68c84b043f3158c0bdfa63d1a0c8d44d7e18a
tballison <tallison@mitre.org>
2016-11-01 14:07:51 -0400

    improve test for TIKA-2098

--- tika-parsers/src/test/java/org/apache/tika/parser/pdf/PDFParserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/pdf/PDFParserTest.java
@@ -1266,8 +1266,8 @@ public class PDFParserTest extends TikaTest {
     public void testMaxLength() throws Exception {
         InputStream is = getResourceAsStream("/test-documents/testPDF.pdf");
         String content = new Tika().parseToString(is, new Metadata(), 100);
-
-        assertTrue(content.length() <= 100);
+        assertTrue(content.length() == 100);
+        assertContains("Tika - Content", content);
     }
 
     private void assertException(String path, Parser parser, ParseContext context, Class expected) {
