9bf4405c56c946b7bf0a0fe5cc196e3ad2d2985a
kraman14 <kumar.raman@yahoo.com>
2017-01-12 17:33:34 -0800

    changed .travis.yml, deploy.sh

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_13d7419eac8d_key -iv $encrypted_13d7419eac8d_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
 fi
