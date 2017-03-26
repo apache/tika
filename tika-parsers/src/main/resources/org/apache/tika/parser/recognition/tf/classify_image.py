cb8f8f5e7ea2b4e13853e6dfc2165127521d9c64
Kranthi Kiran GV <kranthi.gv@gmail.com>
2017-03-27 02:21:36 +0530

    fix the image

--- tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/classify_image.py
+++ tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/classify_image.py
@@ -230,7 +230,7 @@ def maybe_download_and_extract():
 
 def main(_):
   maybe_download_and_extract()
-  image = (FLAGS.image_file if FLAGS.image_file)
+  image = FLAGS.image_file
   run_inference_on_image(image)
 
 
