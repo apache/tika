a4815bf8f0ae2cb3aa2e0b01c4d10d706e24852e
Raunaq Abhyankar <raunaq.abhyankar@gmail.com>
2017-03-28 20:33:00 +0530

    Updated Object Recognition Parser from Inception v3 to Inception v4. Changed classify_image.py and added Inception v4 dependencies in ./tensorflow

--- tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/classify_image.py
+++ tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/classify_image.py
@@ -39,107 +39,32 @@ import os.path
 import re
 import sys
 import tarfile
+from PIL import Image
 
 import numpy as np
 from six.moves import urllib
 import tensorflow as tf
+slim = tf.contrib.slim
+
+from inception_v4 import *
+
+#For converting class_id into human readable string.
+import imagenet
 
 FLAGS = tf.app.flags.FLAGS
 
-# classify_image_graph_def.pb:
-#   Binary representation of the GraphDef protocol buffer.
-# imagenet_synset_to_human_label_map.txt:
-#   Map from synset ID to a human readable string.
-# imagenet_2012_challenge_label_map_proto.pbtxt:
-#   Text representation of a protocol buffer mapping a label to synset ID.
+# inception_v4.ckpt:
+#   Checkpoint for Inception v4 model dated 09/09/2016
+
 tf.app.flags.DEFINE_string(
-    'model_dir', '/tmp/imagenet',
-    """Path to classify_image_graph_def.pb, """
-    """imagenet_synset_to_human_label_map.txt, and """
-    """imagenet_2012_challenge_label_map_proto.pbtxt.""")
+    'model_dir', './tf-objectrec-model',
+    """Path to checkpoint file of Inception v4.""")
 tf.app.flags.DEFINE_string('image_file', '',
                            """Absolute path to image file.""")
-tf.app.flags.DEFINE_integer('num_top_predictions', 5,
-                            """Display this many predictions.""")
 
 # pylint: disable=line-too-long
-DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+DATA_URL = 'http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz'
 # pylint: enable=line-too-long
-
-
-class NodeLookup(object):
-  """Converts integer node ID's to human readable labels."""
-
-  def __init__(self,
-               label_lookup_path=None,
-               uid_lookup_path=None):
-    if not label_lookup_path:
-      label_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
-    if not uid_lookup_path:
-      uid_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
-    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
-
-  def load(self, label_lookup_path, uid_lookup_path):
-    """Loads a human readable English name for each softmax node.
-
-    Args:
-      label_lookup_path: string UID to integer node ID.
-      uid_lookup_path: string UID to human-readable string.
-
-    Returns:
-      dict from integer node ID to human-readable string.
-    """
-    if not tf.gfile.Exists(uid_lookup_path):
-      tf.logging.fatal('File does not exist %s', uid_lookup_path)
-    if not tf.gfile.Exists(label_lookup_path):
-      tf.logging.fatal('File does not exist %s', label_lookup_path)
-
-    # Loads mapping from string UID to human-readable string
-    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
-    uid_to_human = {}
-    p = re.compile(r'[n\d]*[ \S,]*')
-    for line in proto_as_ascii_lines:
-      parsed_items = p.findall(line)
-      uid = parsed_items[0]
-      human_string = parsed_items[2]
-      uid_to_human[uid] = human_string
-
-    # Loads mapping from string UID to integer node ID.
-    node_id_to_uid = {}
-    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
-    for line in proto_as_ascii:
-      if line.startswith('  target_class:'):
-        target_class = int(line.split(': ')[1])
-      if line.startswith('  target_class_string:'):
-        target_class_string = line.split(': ')[1]
-        node_id_to_uid[target_class] = target_class_string[1:-2]
-
-    # Loads the final mapping of integer node ID to human-readable string
-    node_id_to_name = {}
-    for key, val in node_id_to_uid.items():
-      if val not in uid_to_human:
-        tf.logging.fatal('Failed to locate: %s', val)
-      name = uid_to_human[val]
-      node_id_to_name[key] = name
-
-    return node_id_to_name
-
-  def id_to_string(self, node_id):
-    if node_id not in self.node_lookup:
-      return ''
-    return self.node_lookup[node_id]
-
-
-def create_graph():
-  """Creates a graph from saved GraphDef file and returns a saver."""
-  # Creates graph from saved graph_def.pb.
-  with tf.gfile.FastGFile(os.path.join(
-      FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
 
 
 def run_inference_on_image(image):
@@ -153,33 +78,47 @@ def run_inference_on_image(image):
   """
   if not tf.gfile.Exists(image):
     tf.logging.fatal('File does not exist %s', image)
-  image_data = tf.gfile.FastGFile(image, 'rb').read()
+  
+  checkpoint_file = os.path.join(FLAGS.model_dir, 'inception_v4.ckpt')
+  
+  #Load the model
+  sess = tf.Session()
+  arg_scope = inception_v4_arg_scope()
+  
+  #For Inception_v4, the default image height and width is 299. Number of channels is 3
+  image_size = inception_v4.default_image_size
+  input_tensor = tf.placeholder(tf.float32, [None, image_size, image_size, 3])
 
-  # Creates graph from saved GraphDef.
-  create_graph()
+  #Scale the input tensor
+  scaled_input_tensor = tf.scalar_mul((1.0/255), input_tensor)
+  scaled_input_tensor = tf.subtract(scaled_input_tensor, 0.5)
+  scaled_input_tensor = tf.multiply(scaled_input_tensor, 2.0)
 
-  with tf.Session() as sess:
-    # Some useful tensors:
-    # 'softmax:0': A tensor containing the normalized prediction across
-    #   1000 labels.
-    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
-    #   float description of the image.
-    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
-    #   encoding of the image.
-    # Runs the softmax tensor by feeding the image_data as input to the graph.
-    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-    predictions = sess.run(softmax_tensor,
-                           {'DecodeJpeg/contents:0': image_data})
-    predictions = np.squeeze(predictions)
 
-    # Creates node ID --> English string lookup.
-    node_lookup = NodeLookup()
-
-    top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
-    for node_id in top_k:
-      human_string = node_lookup.id_to_string(node_id)
-      score = predictions[node_id]
-      print('%s (score = %.5f)' % (human_string, score))
+  with slim.arg_scope(arg_scope):
+    logits, end_points = inception_v4(scaled_input_tensor, is_training=False)
+  saver = tf.train.Saver()
+  saver.restore(sess, checkpoint_file)
+  
+  #Resize and preprocess the image.
+  im = Image.open(image).resize((image_size,image_size))
+  im = np.array(im)
+  im = im.reshape(-1,299,299,3)
+  
+  #Feed the preprocessed image  
+  predict_values, logit_values = sess.run([end_points['Predictions'], logits], feed_dict={input_tensor: im})
+  
+  #Sort the classes based on probabilities. Top 5 are selected and printed.
+  probabilities = predict_values[0, 0:]
+  sorted_inds = [i[0] for i in sorted(enumerate(-probabilities),
+                                            key=lambda x:x[1])]
+  
+  names = imagenet.create_readable_names_for_imagenet_labels()
+  for i in range(5):
+    index = sorted_inds[i]
+    human_string = names[index]
+    score = probabilities[index]
+    print('%s (score = %.5f)' % (human_string, score))
 
 
 def maybe_download_and_extract():
@@ -204,7 +143,7 @@ def maybe_download_and_extract():
 def main(_):
   maybe_download_and_extract()
   image = (FLAGS.image_file if FLAGS.image_file else
-           os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
+           os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))  
   run_inference_on_image(image)
 
 
