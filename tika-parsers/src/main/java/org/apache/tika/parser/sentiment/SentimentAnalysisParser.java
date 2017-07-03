bfedea8fd871d20db93af6b493d82a4b10ea4171
tballison <tallison@mitre.org>
2017-07-03 14:31:10 -0400

    TIKA-2368 -- move to different package to avoid split package warning

--- tika-parsers/src/main/java/org/apache/tika/parser/sentiment/SentimentAnalysisParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/sentiment/SentimentAnalysisParser.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.tika.parser.sentiment.analysis;
+package org.apache.tika.parser.sentiment;
 
 import java.io.File;
 import java.io.IOException;
--- tika-parsers/src/test/java/org/apache/tika/parser/sentiment/SentimentAnalysisParserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/sentiment/SentimentAnalysisParserTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.tika.parser.sentiment.analysis;
+package org.apache.tika.parser.sentiment;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
