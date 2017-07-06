cb7b84a48aa7f500c08733b06fc78e6c1f0bba14
tballison <tallison@mitre.org>
2017-07-05 20:24:52 -0400

    TIKA-2089 - bug fix, check for nulls

--- tika-parsers/src/main/java/org/apache/tika/parser/microsoft/HSLFExtractor.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/microsoft/HSLFExtractor.java
@@ -212,8 +212,17 @@ public class HSLFExtractor extends AbstractPOIFSExtractor {
 
         //get macro persist id
         DocInfoListContainer list = (DocInfoListContainer)ppt.getDocumentRecord().findFirstOfType(RecordTypes.List.typeID);
+        if (list == null) {
+            return;
+        }
         VBAInfoContainer vbaInfo = (VBAInfoContainer)list.findFirstOfType(RecordTypes.VBAInfo.typeID);
+        if (vbaInfo == null) {
+            return;
+        }
         VBAInfoAtom vbaAtom = (VBAInfoAtom)vbaInfo.findFirstOfType(RecordTypes.VBAInfoAtom.typeID);
+        if (vbaAtom == null) {
+            return;
+        }
         long persistId = vbaAtom.getPersistIdRef();
         for (HSLFObjectData objData : ppt.getEmbeddedObjects()) {
             if (objData.getExOleObjStg().getPersistId() == persistId) {
