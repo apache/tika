8a875cdbc9cb405a239f3c89192591c46fb078f0
ThejanW <thejanwijesinghe.14@cse.mrt.ac.lk>
2017-09-16 15:37:57 +0530

    TIKA 2400 : Changes to video_util.py # Fix formatting issues # Remove unused imports

--- tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/video_util.py
+++ tika-parsers/src/main/resources/org/apache/tika/parser/recognition/tf/video_util.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -16,8 +16,6 @@
 # limitations under the License.
 
 import cv2
-import os
-import sys
 import ntpath
 import numpy as np
 
@@ -25,16 +23,18 @@ print "cv2.__version__", cv2.__version__
 
 CV_FRAME_COUNT = None
 
-if hasattr(cv2,"cv"):
+if hasattr(cv2, "cv"):
     CV_FRAME_COUNT = cv2.cv.CV_CAP_PROP_FRAME_COUNT
 else:
     CV_FRAME_COUNT = cv2.CAP_PROP_FRAME_COUNT
 
+
 def _get_image_from_array(image_array):
-    #JPG to support tensorflow
-    byte_arr = cv2.imencode(".jpg", image_array )[1]
+    # JPG to support tensorflow
+    byte_arr = cv2.imencode(".jpg", image_array)[1]
     return "".join(map(chr, byte_arr))
-    
+
+
 def _path_leaf(path):
     """
     Returns file name from path. Path should not end with slash(/)
@@ -42,72 +42,73 @@ def _path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
+
 def get_center_frame(video_path):
     """
     Traverse till half of video and saves center snapshot
     @param video_path: Path to video file on system
     """
     cap = cv2.VideoCapture(video_path)
-    
+
     length = int(cap.get(CV_FRAME_COUNT))
-    
-    success,image = cap.read()
+
+    success, image = cap.read()
     count = 0
-    
-    while(success and count < length/2):
-        success,image = cap.read()
-        count += 1 
-    
+
+    while success and count < length / 2:
+        success, image = cap.read()
+        count += 1
+
     return _get_image_from_array(image)
-    
+
+
 def get_frames_interval(video_path, frame_interval):
     """
-    Selects one frames after every frame_interval 
+    Selects one frames after every frame_interval
     @param video_path: Path to video file on system
     @param frame_interval: Interval after which frame should be picked. If frame_interval=10 then every 10th frame will be extracted
     """
     cap = cv2.VideoCapture(video_path)
-    
+
     length = int(cap.get(CV_FRAME_COUNT))
-    
-    success,image = cap.read()
+
+    success, image = cap.read()
     count = 0
-    
+
     image_arr = []
-    while(success and count < length):
-        success,image = cap.read()
+    while success and count < length:
+        success, image = cap.read()
         if count % frame_interval == 0:
-            image = _get_image_from_array(image )
+            image = _get_image_from_array(image)
             image_arr.append(image)
-    
+
         count += 1
-         
+
     return image_arr
-    
+
+
 def get_n_frames(video_path, num_frame):
     """
-    Get N frames equidistant to each other in a video   
+    Get N frames equidistant to each other in a video
     @param video_path: Path to video file on system
     @param num_frame: Number of frames to be extracted from video. If num_frame=10 then 10 frames equally distant from each other will be extracted
     """
     cap = cv2.VideoCapture(video_path)
-    
+
     length = int(cap.get(CV_FRAME_COUNT))
-    
-    op_frame_idx = set(np.linspace(0, length - 2, num_frame, dtype=int)) 
-    
+
+    op_frame_idx = set(np.linspace(0, length - 2, num_frame, dtype=int))
+
     success, image = cap.read()
     count = 0
-    
+
     image_arr = []
-    while(success and count < length):
+    while success and count < length:
         success, image = cap.read()
         if success and count in op_frame_idx:
-            
-            image = _get_image_from_array(image )
+            image = _get_image_from_array(image)
             image_arr.append(image)
 
-        count += 1 
-            
-    return image_arr
+        count += 1
 
+    return image_arr
