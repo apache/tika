be434b1e2c999f2b42089874894bfb2dd6c3f762
tballison <tallison@mitre.org>
2017-11-13 16:03:54 -0500

    remove unused dependency

--- tika-parsers/src/test/java/org/apache/tika/config/TikaEncodingDetectorTest.java
+++ tika-parsers/src/test/java/org/apache/tika/config/TikaEncodingDetectorTest.java
@@ -25,7 +25,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import jj2000.j2k.codestream.HeaderInfo;
 import org.apache.tika.Tika;
 import org.apache.tika.detect.CompositeEncodingDetector;
 import org.apache.tika.detect.EncodingDetector;
