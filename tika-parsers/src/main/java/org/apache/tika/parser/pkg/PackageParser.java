c996d01c515103cd84ebd73736427ae79b50aa5b
Nassif <nassif.lfcn@dpf.gov.br>
2018-03-07 23:31:18 -0300

    TIKA-2568: detection of full encrypted 7z files

--- tika-parsers/src/main/java/org/apache/tika/parser/pkg/PackageParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/pkg/PackageParser.java
@@ -252,10 +252,14 @@ public class PackageParser extends AbstractParser {
                 }
                 
                 SevenZFile sevenz;
-                if (password == null) {
-                    sevenz = new SevenZFile(tstream.getFile());
-                } else {
-                    sevenz = new SevenZFile(tstream.getFile(), password.getBytes("UnicodeLittleUnmarked"));
+                try{
+                    if (password == null) {
+                        sevenz = new SevenZFile(tstream.getFile());
+                    } else {
+                        sevenz = new SevenZFile(tstream.getFile(), password.getBytes("UnicodeLittleUnmarked"));
+                    }
+                }catch(PasswordRequiredException e){
+                    throw new EncryptedDocumentException(e);
                 }
                 
                 // Pending a fix for COMPRESS-269 / TIKA-1525, this bit is a little nasty
