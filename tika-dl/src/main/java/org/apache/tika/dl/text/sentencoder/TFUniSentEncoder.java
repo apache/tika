ac8503f9de3b62c49a4bd80dc9acc2841a5d6c8d
ThejanW <thejanw@apache.org>
2018-09-03 03:38:36 +0530

    initial commit for TIKA-2720

--- tika-dl/src/main/java/org/apache/tika/dl/text/sentencoder/TFUniSentEncoder.java
+++ tika-dl/src/main/java/org/apache/tika/dl/text/sentencoder/TFUniSentEncoder.java
@@ -1,4 +1,19 @@
-package org.apache.tika.dl.text.sentencode;
+package org.apache.tika.dl.text.sentencoder;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -14,37 +29,61 @@ import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
+import org.tensorflow.Tensors;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * This parser is powered by <a href="https://www.tensorflow.org/">TensorFlow</a>.
+ * This implementation is pre configured to use <a href="https://arxiv.org/abs/1803.11175"> Universal Sentence Encoder </a>
+ * This parser encodes a text into high dimensional vectors that can be used for text classification, semantic similarity,
+ * clustering and other natural language tasks. The model is trained and optimized for greater-than-word length text,
+ * such as sentences, phrases or short paragraphs. It is trained on a variety of data sources and a variety of tasks
+ * with the aim of dynamically accommodating a wide variety of natural language understanding tasks.
+ * The input is variable length English text and the output is a 512 dimensional vector.
+ * <p>
+ * Although this implementation is made to work out of the box without user attention,
+ * for advances users who are interested in tweaking the settings, the following fields are configurable:
+ * <ul>
+ * <li>{@link #modelDir}</li>
+ * <li>{@link #modelURL}</li>
+ * </ul>
+ * </p>
+ *
+ * @since Tika 2.0
+ */
 
 public class TFUniSentEncoder extends AbstractParser implements Initializable {
 
     private static final Logger LOG = LoggerFactory.getLogger(TFUniSentEncoder.class);
     private static final Set<MediaType> SUPPORTED_TYPES = Collections.singleton(MediaType.application("encodings"));
-    private static final String DEF_MODEL_URL = "https://raw.githubusercontent.com/USCDataScience/SentimentAnalysisParser/master/sentiment-models/en-netflix-sentiment.bin";
+    private static final String DEF_MODEL_URL = "https://www.dropbox.com/s/brls43rgzoqtee4/uni-sent-encoder.zip?dl=1";
     private static final String BASE_MODEL_CACHE_DIR = System.getProperty("user.home") + File.separator + ".tika-dl" +
             File.separator + "models" + File.separator + "tf";
     private static final String MODEL_CACHE_DIR = BASE_MODEL_CACHE_DIR + File.separator + "universal-sentence-encoder-2";
+    private static final int BUFFER_SIZE = 4096;
 
     @Field
-    private File modelDir = new File(MODEL_CACHE_DIR);
+    private static String modelDir = MODEL_CACHE_DIR;
 
     @Field
-    private String modelURL = DEF_MODEL_URL;
+    private static String modelURL = DEF_MODEL_URL;
 
-    //    private SentimentME classifier;
+    private static SavedModelBundle savedModel;
 
-    private static synchronized File cachedDownload(File cacheDir, URI uri)
-            throws IOException {
+    private Session sess;
+
+    /**
+     * @param cacheDir directory to cache models
+     * @param uri      URL to download models from
+     * @return
+     * @throws IOException for calling unzip method
+     */
+    private static synchronized File cachedDownload(File cacheDir, URI uri) throws IOException {
 
         if ("file".equals(uri.getScheme()) || uri.getScheme() == null) {
             return new File(uri);
@@ -52,32 +91,89 @@ public class TFUniSentEncoder extends AbstractParser implements Initializable {
         if (!cacheDir.exists()) {
             cacheDir.mkdirs();
         }
-        String[] parts = uri.toASCIIString().split("/");
-        File cacheFile = new File(cacheDir, parts[parts.length - 1]);
-        File successFlag = new File(cacheFile.getAbsolutePath() + ".success");
 
-        if (cacheFile.exists() && successFlag.exists()) {
-            LOG.info("Cache exist at {}. Not downloading it", cacheFile.getAbsolutePath());
+        File zipFile = new File(cacheDir, "uni-sent-encoder.zip");
+
+        File successFlag = new File(cacheDir, ".success");
+
+        if (successFlag.exists()) {
+            LOG.info("Cache exist at {}. Not downloading it", zipFile.getAbsolutePath());
         } else {
             if (successFlag.exists()) {
                 successFlag.delete();
             }
             LOG.info("Cache doesn't exist. Going to make a copy");
             LOG.info("This might take a while! GET {}", uri);
-            FileUtils.copyURLToFile(uri.toURL(), cacheFile, 5000, 60000);
-            //restore the success flag again
-            FileUtils.write(successFlag,
-                    "CopiedAt:" + System.currentTimeMillis(),
-                    Charset.defaultCharset());
+            FileUtils.copyURLToFile(uri.toURL(), zipFile, 5000, 60000);
+            LOG.info("Compressed model file downloaded");
+            unzip(zipFile.getAbsolutePath(), modelDir);
+            LOG.info("Extracted compressed model file");
+            FileUtils.deleteQuietly(zipFile);
+            FileUtils.write(successFlag, "CopiedAt:" + System.currentTimeMillis(), Charset.defaultCharset());
+            LOG.info("Success flag restored");
         }
-        return cacheFile;
+        return cacheDir;
+    }
+
+    /**
+     * Extracts a zip file specified by the zipFilePath to a directory specified by
+     * destDirectory (will be created if does not exists)
+     *
+     * @param zipFilePath   zip file path
+     * @param destDirectory destination directory
+     * @throws IOException when file does not exist
+     */
+    private static void unzip(String zipFilePath, String destDirectory) throws IOException {
+
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     *
+     * @param zipIn    zip input stream
+     * @param filePath file path
+     * @throws IOException when file does not exist
+     */
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
     }
 
     @Override
     public void initialize(Map<String, Param> params) throws TikaConfigException {
+
         try {
-            String modelPath = cachedDownload(modelDir, URI.create(modelURL)).getAbsolutePath();
-            System.out.println(modelPath);
+            String modelPath = cachedDownload(new File(modelDir), URI.create(modelURL)).getAbsolutePath();
+            savedModel = SavedModelBundle.load(modelPath, "serve");
+            LOG.info("saved model successfully loaded into memory");
+            sess = savedModel.session();
         } catch (Exception e) {
             throw new TikaConfigException(e.getMessage(), e);
         }
@@ -108,16 +204,35 @@ public class TFUniSentEncoder extends AbstractParser implements Initializable {
      * @param context  the context for the parser
      */
     @Override
-    public void parse(InputStream stream, ContentHandler handler,
-                      Metadata metadata, ParseContext context)
+    public void parse(InputStream stream, ContentHandler handler, Metadata metadata, ParseContext context)
             throws IOException, SAXException, TikaException {
-//        if (classifier == null) {
-//            LOG.warn(getClass().getSimpleName() + " is not configured properly.");
-//            return;
-//        }
-//        String inputString = IOUtils.toString(stream, "UTF-8");
-//        String sentiment = classifier.predict(inputString);
-//        metadata.add("Sentiment", sentiment);
+
+        String inputString = IOUtils.toString(stream, "UTF-8");
+        String[] m = inputString.split("\n");
+
+        int textCount = m.length;
+        int encodingLen = 512;
+
+        byte[][] msgs = new byte[textCount][];
+
+        for (int i = 0; i < textCount; i++) {
+            msgs[i] = m[i].getBytes(UTF_8);
+        }
+
+        float[][] modelOutput = sess.runner()
+                .feed("sentences", Tensors.create(msgs))
+                .fetch("encodings:0")
+                .run()
+                .get(0)
+                .copyTo(new float[textCount][encodingLen]);
+
+        String encodings = Arrays.deepToString(modelOutput);
+        metadata.add("UniversalSentenceEncodings", encodings);
     }
 
+    public void dispose() {
+        // Dispose model
+        savedModel.close();
+        LOG.info("Model successfully closed");
+    }
 }
--- tika-dl/src/test/java/org/apache/tika/dl/text/sentencoder/TFUniSentEncoderTest.java
+++ tika-dl/src/test/java/org/apache/tika/dl/text/sentencoder/TFUniSentEncoderTest.java
@@ -1,100 +1,134 @@
-package org.apache.tika.dl.text.sentencode;
+package org.apache.tika.dl.text.sentencoder;
 
-import static org.junit.Assert.assertTrue;
-
-import org.apache.tika.Tika;
-import org.apache.tika.config.TikaConfig;
-import org.apache.tika.metadata.Metadata;
-
-import org.junit.Test;
-
-import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.ops.transforms.Transforms;
-
-import org.tensorflow.SavedModelBundle;
-import org.tensorflow.Session;
-import org.tensorflow.TensorFlow;
-import org.tensorflow.Tensors;
-
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.exception.TikaConfigException;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.junit.Test;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
+import org.xml.sax.SAXException;
 
 public class TFUniSentEncoderTest {
+
     @Test
-    public void test2() throws Exception {
+    public void sentenceSimilarityTest() throws Exception {
+        Tika tika = getTika("tf-uni-sent-encoder-config.xml");
+        if (tika == null) {
+            return;
+        }
 
-        System.out.println(TensorFlow.version());
-        try (SavedModelBundle savedModel = SavedModelBundle.load("/home/thejan/Desktop/DL-tutz/keras-tests/tweaks/text-sim/001", "serve")) {
+        String[] sentences = {
+                // About age
+                "How old are you?",
+                "What is your age?",
+                "How old did you turn?",
+                "When is your birthday?",
 
-            // create the session from the Bundle
-            Session sess = savedModel.session();
+                // About smart phones
+                "The Samsung Galaxy S10 has the potential to be the most exciting phone of 2019",
+                "Android beats iOS in smartphone loyalty, study finds",
+                "IPhone X includes a 5.8-inch edge-to-edge display which covers the entire front of the phone.",
+                "Apple became the world’s first trillion-dollar public company",
 
-            String[] m = {
-                    // Smartphones
-                    "I like my phone",
-                    "My phone is not good.",
-                    "Your cellphone looks great.",
+                // About weather
+                "With roads covered with slippery snow and ice, can challenge even the most experienced driver.",
+                "Heavy rain slammed the mid-Atlantic United States on Monday, delaying flights, forming sinkholes",
+                "News showed, violent floodwaters surging down main Streets",
+                "Recently a lot of hurricanes have hit the US",
+                "Multiple lines of scientific evidence show that the climate system is warming",
 
-                    // Weather
-                    "Will it snow tomorrow?",
-                    "It is going to rain",
-                    "Recently a lot of hurricanes have hit the US",
-                    "Global warming is real",
+                // About health
+                "An ounce of prevention is worth a pound of cure",
+                "Green tea contains bioactive compounds that improve health",
+                "Yoga has been shown to help people reduce anxiety",
+                "Is paleo better than keto?"
+        };
 
-                    // Food and health
-                    "An apple a day, keeps the doctors away",
-                    "Eating strawberries is healthy",
-                    "Is paleo better than keto?",
+        String text = String.join("\n", sentences);
+        ByteArrayInputStream stream = new ByteArrayInputStream(text.getBytes(Charset.defaultCharset()));
+        Metadata md = new Metadata();
+        tika.parse(stream, md);
+        String strEncodings = md.get("UniversalSentenceEncodings");
+        float[][] encodings = string2array(strEncodings);
+        // System.out.println(Arrays.deepToString(encodings));
 
-                    // Asking about age
-                    "How old are you?",
-                    "what is your age?",
-                    "you look young. How old are you?"
-            };
-
-            int textCount = m.length;
-
-            byte[][] msgs = new byte[textCount][];
-
-            for (int i = 0; i < textCount; i++) {
-                msgs[i] = m[i].getBytes(UTF_8);
-            }
-
-            float[][] encodings = sess.runner()
-                    .feed("sentences", Tensors.create(msgs))
-                    .fetch("encodings:0")
-                    .run()
-                    .get(0)
-                    .copyTo(new float[textCount][512]);
-
-            Map<String, Double> corrs = new HashMap<>();
-            for (int i = 0; i < encodings.length - 1; i++) {
-                for (int j = i + 1; j < encodings.length; j++) {
-                    String indexes = i + "-" + j;
-                    double cosineSim = Transforms.cosineSim(Nd4j.create(encodings[i]), Nd4j.create(encodings[j]));
-                    corrs.put(indexes, cosineSim);
-                }
-            }
-
-            Map<String, Double> sortedCorrs = corrs.entrySet().stream()
-                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                    .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-            System.out.println(sortedCorrs);
-
-            for (int i = 0; i < 12; i++) {
-                String k = (String) sortedCorrs.keySet().toArray()[i];
-                double v = sortedCorrs.get(k);
-
-                int[] two_ks = Arrays.stream(k.split("-")).mapToInt(Integer::parseInt).toArray();
-                System.out.println(m[two_ks[0]] + " -- " + m[two_ks[1]] + " : " + v);
+        Map<String, Double> correlations = new HashMap<>();
+        for (int i = 0; i < encodings.length - 1; i++) {
+            for (int j = i + 1; j < encodings.length; j++) {
+                String indexes = i + "-" + j;
+                double cosineSim = Transforms.cosineSim(Nd4j.create(encodings[i]), Nd4j.create(encodings[j]));
+                correlations.put(indexes, cosineSim);
             }
         }
+
+        Map<String, Double> sortedCorrelations = correlations.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        int i = 0;
+        double corrThreshold = 0.347;
+        while (true) {
+            String k = (String) sortedCorrelations.keySet().toArray()[i++];
+            int[] two_ks = Arrays.stream(k.split("-")).mapToInt(Integer::parseInt).toArray();
+            double v = sortedCorrelations.get(k);
+
+            if (v < corrThreshold) {
+                break;
+            } else {
+                System.out.println(sentences[two_ks[0]]);
+                System.out.println(sentences[two_ks[1]]);
+                System.out.println(v);
+                System.out.println("=====================================================================================");
+            }
+        }
+    }
+
+    private Tika getTika(String configXml) throws TikaException, SAXException, IOException {
+        try (InputStream confStream = getClass().getResourceAsStream(configXml)) {
+            assert confStream != null;
+            TikaConfig config = new TikaConfig(confStream);
+            return new Tika(config);
+        } catch (TikaConfigException e) {
+            //if can't connect to pull sentiment model...ignore test
+            if (e.getCause() != null
+                    && e.getCause() instanceof IOException) {
+                return null;
+            }
+            throw e;
+        }
+    }
+
+    private float[][] string2array(String array) {
+        String[] arrays = array
+                .replace("[[", "")
+                .replace("]]", "")
+                .replace("], [", "#")
+                .split("#");
+        List<float[]> tempResult = new ArrayList<>();
+        for (String a : arrays) {
+            String[] strFloatArray = a.split(", ");
+            float[] floatArray = new float[strFloatArray.length];
+            for (int i = 0; i < strFloatArray.length; i++) {
+                floatArray[i] = Float.parseFloat(strFloatArray[i]);
+            }
+            tempResult.add(floatArray);
+        }
+        return tempResult.toArray(new float[tempResult.size()][]);
     }
 }
