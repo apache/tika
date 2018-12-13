7696e386b117c2c988c759da2a6dd6b7095dbc73
TALLISON <tallison@apache.org>
2018-12-13 10:24:47 -0500

    TIKA-2792 -- revert mp4 parser based on large scale regression test results

--- tika-parsers/src/main/java/org/apache/tika/parser/mp4/MP4Parser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/mp4/MP4Parser.java
@@ -17,6 +17,35 @@
 package org.apache.tika.parser.mp4;
 
 
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.boxes.Box;
+import com.coremedia.iso.boxes.Container;
+import com.coremedia.iso.boxes.FileTypeBox;
+import com.coremedia.iso.boxes.MetaBox;
+import com.coremedia.iso.boxes.MovieBox;
+import com.coremedia.iso.boxes.MovieHeaderBox;
+import com.coremedia.iso.boxes.SampleDescriptionBox;
+import com.coremedia.iso.boxes.SampleTableBox;
+import com.coremedia.iso.boxes.TrackBox;
+import com.coremedia.iso.boxes.TrackHeaderBox;
+import com.coremedia.iso.boxes.UserDataBox;
+import com.coremedia.iso.boxes.apple.AppleItemListBox;
+import com.coremedia.iso.boxes.sampleentry.AudioSampleEntry;
+import com.googlecode.mp4parser.DataSource;
+import com.googlecode.mp4parser.boxes.apple.AppleAlbumBox;
+import com.googlecode.mp4parser.boxes.apple.AppleArtist2Box;
+import com.googlecode.mp4parser.boxes.apple.AppleArtistBox;
+import com.googlecode.mp4parser.boxes.apple.AppleCommentBox;
+import com.googlecode.mp4parser.boxes.apple.AppleCompilationBox;
+import com.googlecode.mp4parser.boxes.apple.AppleDiskNumberBox;
+import com.googlecode.mp4parser.boxes.apple.AppleEncoderBox;
+import com.googlecode.mp4parser.boxes.apple.AppleGenreBox;
+import com.googlecode.mp4parser.boxes.apple.AppleNameBox;
+import com.googlecode.mp4parser.boxes.apple.AppleRecordingYear2Box;
+import com.googlecode.mp4parser.boxes.apple.AppleTrackAuthorBox;
+import com.googlecode.mp4parser.boxes.apple.AppleTrackNumberBox;
+import com.googlecode.mp4parser.boxes.apple.Utf8AppleDataBox;
+import org.apache.poi.ss.formula.functions.T;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.TemporaryResources;
 import org.apache.tika.io.TikaInputStream;
@@ -29,33 +58,6 @@ import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AbstractParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.XHTMLContentHandler;
-import org.mp4parser.Box;
-import org.mp4parser.Container;
-import org.mp4parser.IsoFile;
-import org.mp4parser.boxes.apple.AppleAlbumBox;
-import org.mp4parser.boxes.apple.AppleArtist2Box;
-import org.mp4parser.boxes.apple.AppleArtistBox;
-import org.mp4parser.boxes.apple.AppleCommentBox;
-import org.mp4parser.boxes.apple.AppleCompilationBox;
-import org.mp4parser.boxes.apple.AppleDiskNumberBox;
-import org.mp4parser.boxes.apple.AppleEncoderBox;
-import org.mp4parser.boxes.apple.AppleGenreBox;
-import org.mp4parser.boxes.apple.AppleItemListBox;
-import org.mp4parser.boxes.apple.AppleNameBox;
-import org.mp4parser.boxes.apple.AppleRecordingYear2Box;
-import org.mp4parser.boxes.apple.AppleTrackAuthorBox;
-import org.mp4parser.boxes.apple.AppleTrackNumberBox;
-import org.mp4parser.boxes.apple.Utf8AppleDataBox;
-import org.mp4parser.boxes.iso14496.part12.FileTypeBox;
-import org.mp4parser.boxes.iso14496.part12.MetaBox;
-import org.mp4parser.boxes.iso14496.part12.MovieBox;
-import org.mp4parser.boxes.iso14496.part12.MovieHeaderBox;
-import org.mp4parser.boxes.iso14496.part12.SampleDescriptionBox;
-import org.mp4parser.boxes.iso14496.part12.SampleTableBox;
-import org.mp4parser.boxes.iso14496.part12.TrackBox;
-import org.mp4parser.boxes.iso14496.part12.TrackHeaderBox;
-import org.mp4parser.boxes.iso14496.part12.UserDataBox;
-import org.mp4parser.boxes.sampleentry.AudioSampleEntry;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
@@ -126,175 +128,177 @@ public class MP4Parser extends AbstractParser {
         TemporaryResources tmp = new TemporaryResources();
         TikaInputStream tstream = TikaInputStream.get(stream, tmp);
 
-        try (IsoFile isoFile = new IsoFile(tstream.getFile())) {
-            tmp.addResource(isoFile);
+        try (DataSource dataSource = new DirectFileReadDataSource(tstream.getFile())) {
+            try (IsoFile isoFile = new IsoFile(dataSource)) {
+                tmp.addResource(isoFile);
 
-            // Grab the file type box
-            FileTypeBox fileType = getOrNull(isoFile, FileTypeBox.class);
-            if (fileType != null) {
-                // Identify the type
-                MediaType type = MediaType.application("mp4");
-                for (Map.Entry<MediaType, List<String>> e : typesMap.entrySet()) {
-                    if (e.getValue().contains(fileType.getMajorBrand())) {
-                        type = e.getKey();
-                        break;
+                // Grab the file type box
+                FileTypeBox fileType = getOrNull(isoFile, FileTypeBox.class);
+                if (fileType != null) {
+                    // Identify the type
+                    MediaType type = MediaType.application("mp4");
+                    for (Map.Entry<MediaType, List<String>> e : typesMap.entrySet()) {
+                        if (e.getValue().contains(fileType.getMajorBrand())) {
+                            type = e.getKey();
+                            break;
+                        }
                     }
+                    metadata.set(Metadata.CONTENT_TYPE, type.toString());
+
+                    if (type.getType().equals("audio")) {
+                        metadata.set(XMPDM.AUDIO_COMPRESSOR, fileType.getMajorBrand().trim());
+                    }
+                } else {
+                    // Some older QuickTime files lack the FileType
+                    metadata.set(Metadata.CONTENT_TYPE, "video/quicktime");
                 }
-                metadata.set(Metadata.CONTENT_TYPE, type.toString());
 
-                if (type.getType().equals("audio")) {
-                    metadata.set(XMPDM.AUDIO_COMPRESSOR, fileType.getMajorBrand().trim());
+
+                // Get the main MOOV box
+                MovieBox moov = getOrNull(isoFile, MovieBox.class);
+                if (moov == null) {
+                    // Bail out
+                    return;
                 }
-            } else {
-                // Some older QuickTime files lack the FileType
-                metadata.set(Metadata.CONTENT_TYPE, "video/quicktime");
-            }
 
 
-            // Get the main MOOV box
-            MovieBox moov = getOrNull(isoFile, MovieBox.class);
-            if (moov == null) {
-                // Bail out
-                return;
-            }
+                XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
+                xhtml.startDocument();
 
 
-            XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
-            xhtml.startDocument();
+                // Pull out some information from the header box
+                MovieHeaderBox mHeader = getOrNull(moov, MovieHeaderBox.class);
+                if (mHeader != null) {
+                    // Get the creation and modification dates
+                    metadata.set(Metadata.CREATION_DATE, mHeader.getCreationTime());
+                    metadata.set(TikaCoreProperties.MODIFIED, mHeader.getModificationTime());
 
+                    // Get the duration
+                    double durationSeconds = ((double) mHeader.getDuration()) / mHeader.getTimescale();
+                    metadata.set(XMPDM.DURATION, DURATION_FORMAT.format(durationSeconds));
 
-            // Pull out some information from the header box
-            MovieHeaderBox mHeader = getOrNull(moov, MovieHeaderBox.class);
-            if (mHeader != null) {
-                // Get the creation and modification dates
-                metadata.set(Metadata.CREATION_DATE, mHeader.getCreationTime());
-                metadata.set(TikaCoreProperties.MODIFIED, mHeader.getModificationTime());
-
-                // Get the duration
-                double durationSeconds = ((double) mHeader.getDuration()) / mHeader.getTimescale();
-                metadata.set(XMPDM.DURATION, DURATION_FORMAT.format(durationSeconds));
-
-                // The timescale is normally the sampling rate
-                metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int) mHeader.getTimescale());
-            }
-
-
-            // Get some more information from the track header
-            // TODO Decide how to handle multiple tracks
-            List<TrackBox> tb = moov.getBoxes(TrackBox.class);
-            if (tb.size() > 0) {
-                TrackBox track = tb.get(0);
-
-                TrackHeaderBox header = track.getTrackHeaderBox();
-                // Get the creation and modification dates
-                metadata.set(TikaCoreProperties.CREATED, header.getCreationTime());
-                metadata.set(TikaCoreProperties.MODIFIED, header.getModificationTime());
-
-                // Get the video with and height
-                metadata.set(Metadata.IMAGE_WIDTH, (int) header.getWidth());
-                metadata.set(Metadata.IMAGE_LENGTH, (int) header.getHeight());
-
-                // Get the sample information
-                SampleTableBox samples = track.getSampleTableBox();
-                SampleDescriptionBox sampleDesc = samples.getSampleDescriptionBox();
-                if (sampleDesc != null) {
-                    // Look for the first Audio Sample, if present
-                    AudioSampleEntry sample = getOrNull(sampleDesc, AudioSampleEntry.class);
-                    if (sample != null) {
-                        XMPDM.ChannelTypePropertyConverter.convertAndSet(metadata, sample.getChannelCount());
-                        //metadata.set(XMPDM.AUDIO_SAMPLE_TYPE, sample.getSampleSize());    // TODO Num -> Type mapping
-                        metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int) sample.getSampleRate());
-                        //metadata.set(XMPDM.AUDIO_, sample.getSamplesPerPacket());
-                        //metadata.set(XMPDM.AUDIO_, sample.getBytesPerSample());
-                    }
+                    // The timescale is normally the sampling rate
+                    metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int) mHeader.getTimescale());
                 }
-            }
-
-            // Get metadata from the User Data Box
-            UserDataBox userData = getOrNull(moov, UserDataBox.class);
-            if (userData != null) {
-                MetaBox meta = getOrNull(userData, MetaBox.class);
-
-                // Check for iTunes Metadata
-                // See http://atomicparsley.sourceforge.net/mpeg-4files.html and
-                //  http://code.google.com/p/mp4v2/wiki/iTunesMetadata for more on these
-                AppleItemListBox apple = getOrNull(meta, AppleItemListBox.class);
-                if (apple != null) {
-                    // Title
-                    AppleNameBox title = getOrNull(apple, AppleNameBox.class);
-                    addMetadata(TikaCoreProperties.TITLE, metadata, title);
-
-                    // Artist
-                    AppleArtistBox artist = getOrNull(apple, AppleArtistBox.class);
-                    addMetadata(TikaCoreProperties.CREATOR, metadata, artist);
-                    addMetadata(XMPDM.ARTIST, metadata, artist);
-
-                    // Album Artist
-                    AppleArtist2Box artist2 = getOrNull(apple, AppleArtist2Box.class);
-                    addMetadata(XMPDM.ALBUM_ARTIST, metadata, artist2);
-
-                    // Album
-                    AppleAlbumBox album = getOrNull(apple, AppleAlbumBox.class);
-                    addMetadata(XMPDM.ALBUM, metadata, album);
-
-                    // Composer
-                    AppleTrackAuthorBox composer = getOrNull(apple, AppleTrackAuthorBox.class);
-                    addMetadata(XMPDM.COMPOSER, metadata, composer);
-
-                    // Genre
-                    AppleGenreBox genre = getOrNull(apple, AppleGenreBox.class);
-                    addMetadata(XMPDM.GENRE, metadata, genre);
-
-                    // Year
-                    AppleRecordingYear2Box year = getOrNull(apple, AppleRecordingYear2Box.class);
-                    if (year != null) {
-                        metadata.set(XMPDM.RELEASE_DATE, year.getValue());
-                    }
-
-                    // Track number
-                    AppleTrackNumberBox trackNum = getOrNull(apple, AppleTrackNumberBox.class);
-                    if (trackNum != null) {
-                        metadata.set(XMPDM.TRACK_NUMBER, trackNum.getA());
-                        //metadata.set(XMPDM.NUMBER_OF_TRACKS, trackNum.getB()); // TODO
-                    }
-
-                    // Disc number
-                    AppleDiskNumberBox discNum = getOrNull(apple, AppleDiskNumberBox.class);
-                    if (discNum != null) {
-                        metadata.set(XMPDM.DISC_NUMBER, discNum.getA());
-                    }
-
-                    // Compilation
-                    AppleCompilationBox compilation = getOrNull(apple, AppleCompilationBox.class);
-                    if (compilation != null) {
-                        metadata.set(XMPDM.COMPILATION, (int) compilation.getValue());
-                    }
-
-                    // Comment
-                    AppleCommentBox comment = getOrNull(apple, AppleCommentBox.class);
-                    addMetadata(XMPDM.LOG_COMMENT, metadata, comment);
-
-                    // Encoder
-                    AppleEncoderBox encoder = getOrNull(apple, AppleEncoderBox.class);
-                    if (encoder != null) {
-                        metadata.set(XMP.CREATOR_TOOL, encoder.getValue());
-                    }
 
 
-                    // As text
-                    for (Box box : apple.getBoxes()) {
-                        if (box instanceof Utf8AppleDataBox) {
-                            xhtml.element("p", ((Utf8AppleDataBox) box).getValue());
+                // Get some more information from the track header
+                // TODO Decide how to handle multiple tracks
+                List<TrackBox> tb = moov.getBoxes(TrackBox.class);
+                if (tb.size() > 0) {
+                    TrackBox track = tb.get(0);
+
+                    TrackHeaderBox header = track.getTrackHeaderBox();
+                    // Get the creation and modification dates
+                    metadata.set(TikaCoreProperties.CREATED, header.getCreationTime());
+                    metadata.set(TikaCoreProperties.MODIFIED, header.getModificationTime());
+
+                    // Get the video with and height
+                    metadata.set(Metadata.IMAGE_WIDTH, (int) header.getWidth());
+                    metadata.set(Metadata.IMAGE_LENGTH, (int) header.getHeight());
+
+                    // Get the sample information
+                    SampleTableBox samples = track.getSampleTableBox();
+                    SampleDescriptionBox sampleDesc = samples.getSampleDescriptionBox();
+                    if (sampleDesc != null) {
+                        // Look for the first Audio Sample, if present
+                        AudioSampleEntry sample = getOrNull(sampleDesc, AudioSampleEntry.class);
+                        if (sample != null) {
+                            XMPDM.ChannelTypePropertyConverter.convertAndSet(metadata, sample.getChannelCount());
+                            //metadata.set(XMPDM.AUDIO_SAMPLE_TYPE, sample.getSampleSize());    // TODO Num -> Type mapping
+                            metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int) sample.getSampleRate());
+                            //metadata.set(XMPDM.AUDIO_, sample.getSamplesPerPacket());
+                            //metadata.set(XMPDM.AUDIO_, sample.getBytesPerSample());
                         }
                     }
                 }
 
-                // TODO Check for other kinds too
+                // Get metadata from the User Data Box
+                UserDataBox userData = getOrNull(moov, UserDataBox.class);
+                if (userData != null) {
+                    MetaBox meta = getOrNull(userData, MetaBox.class);
+
+                    // Check for iTunes Metadata
+                    // See http://atomicparsley.sourceforge.net/mpeg-4files.html and
+                    //  http://code.google.com/p/mp4v2/wiki/iTunesMetadata for more on these
+                    AppleItemListBox apple = getOrNull(meta, AppleItemListBox.class);
+                    if (apple != null) {
+                        // Title
+                        AppleNameBox title = getOrNull(apple, AppleNameBox.class);
+                        addMetadata(TikaCoreProperties.TITLE, metadata, title);
+
+                        // Artist
+                        AppleArtistBox artist = getOrNull(apple, AppleArtistBox.class);
+                        addMetadata(TikaCoreProperties.CREATOR, metadata, artist);
+                        addMetadata(XMPDM.ARTIST, metadata, artist);
+
+                        // Album Artist
+                        AppleArtist2Box artist2 = getOrNull(apple, AppleArtist2Box.class);
+                        addMetadata(XMPDM.ALBUM_ARTIST, metadata, artist2);
+
+                        // Album
+                        AppleAlbumBox album = getOrNull(apple, AppleAlbumBox.class);
+                        addMetadata(XMPDM.ALBUM, metadata, album);
+
+                        // Composer
+                        AppleTrackAuthorBox composer = getOrNull(apple, AppleTrackAuthorBox.class);
+                        addMetadata(XMPDM.COMPOSER, metadata, composer);
+
+                        // Genre
+                        AppleGenreBox genre = getOrNull(apple, AppleGenreBox.class);
+                        addMetadata(XMPDM.GENRE, metadata, genre);
+
+                        // Year
+                        AppleRecordingYear2Box year = getOrNull(apple, AppleRecordingYear2Box.class);
+                        if (year != null) {
+                            metadata.set(XMPDM.RELEASE_DATE, year.getValue());
+                        }
+
+                        // Track number
+                        AppleTrackNumberBox trackNum = getOrNull(apple, AppleTrackNumberBox.class);
+                        if (trackNum != null) {
+                            metadata.set(XMPDM.TRACK_NUMBER, trackNum.getA());
+                            //metadata.set(XMPDM.NUMBER_OF_TRACKS, trackNum.getB()); // TODO
+                        }
+
+                        // Disc number
+                        AppleDiskNumberBox discNum = getOrNull(apple, AppleDiskNumberBox.class);
+                        if (discNum != null) {
+                            metadata.set(XMPDM.DISC_NUMBER, discNum.getA());
+                        }
+
+                        // Compilation
+                        AppleCompilationBox compilation = getOrNull(apple, AppleCompilationBox.class);
+                        if (compilation != null) {
+                            metadata.set(XMPDM.COMPILATION, (int) compilation.getValue());
+                        }
+
+                        // Comment
+                        AppleCommentBox comment = getOrNull(apple, AppleCommentBox.class);
+                        addMetadata(XMPDM.LOG_COMMENT, metadata, comment);
+
+                        // Encoder
+                        AppleEncoderBox encoder = getOrNull(apple, AppleEncoderBox.class);
+                        if (encoder != null) {
+                            metadata.set(XMP.CREATOR_TOOL, encoder.getValue());
+                        }
+
+
+                        // As text
+                        for (Box box : apple.getBoxes()) {
+                            if (box instanceof Utf8AppleDataBox) {
+                                xhtml.element("p", ((Utf8AppleDataBox) box).getValue());
+                            }
+                        }
+                    }
+
+                    // TODO Check for other kinds too
+                }
+
+
+                // All done
+                xhtml.endDocument();
             }
-
-            // All done
-            xhtml.endDocument();
-
         } finally {
             tmp.dispose();
         }
