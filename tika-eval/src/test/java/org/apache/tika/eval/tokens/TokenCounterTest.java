acdc4fcc6997a37872b7794dc24130366864e054
TALLISON <tallison@apache.org>
2019-02-01 09:11:41 -0500

    TIKA-2825

--- tika-eval/src/test/java/org/apache/tika/eval/tokens/TokenCounterTest.java
+++ tika-eval/src/test/java/org/apache/tika/eval/tokens/TokenCounterTest.java
@@ -84,7 +84,6 @@ public class TokenCounterTest {
         String s = "the http://www.cnn.com and blahdeblah@apache.org are in valuable www.sites.org 普林斯顿大学";
         tokenCounter.add(FIELD, s);
         Map<String, MutableInt> tokens = tokenCounter.getTokens(FIELD);
-        System.out.println(tokens);
         assertEquals(new MutableInt(2), tokens.get("___url___"));
         assertEquals(new MutableInt(1), tokens.get("___email___"));
     }
