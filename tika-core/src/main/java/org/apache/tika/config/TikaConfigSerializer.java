82ee73c38a3255819cd1cdef553f1de3e8b744b1
Colm O hEigeartaigh <coheigea@apache.org>
2019-06-20 17:34:12 +0100

    Disable external DTD + Stylesheets with the TransformerFactory

--- tika-core/src/main/java/org/apache/tika/config/TikaConfigSerializer.java
+++ tika-core/src/main/java/org/apache/tika/config/TikaConfigSerializer.java
@@ -27,7 +27,6 @@ import java.util.concurrent.ExecutorService;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
@@ -93,8 +92,7 @@ public class TikaConfigSerializer {
         // TODO Service Loader section
 
         // now write
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
+        Transformer transformer = XMLReaderUtils.getTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
         transformer.setOutputProperty(OutputKeys.ENCODING, charset.name());
