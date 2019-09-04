6ce1ce2ebacbf008fddd0b8171d8ee15b26bcbb9
Eric Pugh <epugh@opensourceconnections.com>
2019-09-04 08:10:01 -0400

    TIKA-2931 (#281)
    * need to lowercase the output file to match the format passed to tesseract cmd line
    
    * need to lowercase the output file to match the format passed to tesseract cmd line
    
    * pull original code from apache/tika
    
    * Send informational output to System.err, not System.out so commandline json response is clean

--- tika-app/src/main/java/org/apache/tika/cli/TikaCLI.java
+++ tika-app/src/main/java/org/apache/tika/cli/TikaCLI.java
@@ -1066,7 +1066,7 @@ public class TikaCLI {
                     throw new IOException("unable to create directory \"" + parent + "\"");
                 }
             }
-            System.out.println("Extracting '"+name+"' ("+contentType+") to " + outputFile);
+            System.err.println("Extracting '"+name+"' ("+contentType+") to " + outputFile);
 
             try (FileOutputStream os = new FileOutputStream(outputFile)) {
                 if (inputStream instanceof TikaInputStream) {
