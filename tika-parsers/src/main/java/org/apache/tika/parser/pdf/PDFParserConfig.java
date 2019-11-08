770719a07f57d4804383f262fa8cccc8ec172fc0
tallison <tallison@apache.org>
2019-11-08 13:53:09 -0500

    TIKA-2978 -- setter for maxMainMemoryBytes should be of type long

--- tika-parsers/src/main/java/org/apache/tika/parser/pdf/PDFParserConfig.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/pdf/PDFParserConfig.java
@@ -238,7 +238,7 @@ public class PDFParserConfig implements Serializable {
             accessChecker = new AccessChecker(allowExtractionForAccessibility);
         }
 
-        maxMainMemoryBytes = getIntProp(props.getProperty("maxMainMemoryBytes"), -1);
+        maxMainMemoryBytes = getLongProp(props.getProperty("maxMainMemoryBytes"), -1);
         detectAngles = getBooleanProp(props.getProperty("detectAngles"), false);
     }
 
@@ -701,7 +701,16 @@ public class PDFParserConfig implements Serializable {
         return maxMainMemoryBytes;
     }
 
+    /**
+     * @deprecated use {@link #setMaxMainMemoryBytes(long)}
+     * @param maxMainMemoryBytes
+     */
+    @Deprecated
     public void setMaxMainMemoryBytes(int maxMainMemoryBytes) {
+        this.maxMainMemoryBytes = maxMainMemoryBytes;
+    }
+
+    public void setMaxMainMemoryBytes(long maxMainMemoryBytes) {
         this.maxMainMemoryBytes = maxMainMemoryBytes;
     }
 
