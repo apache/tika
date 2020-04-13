7f03c39553d62225204747d7c75ebd93aaca5c75
Peter van der Weerd <pweerd@bitmanager.nl>
2020-04-13 15:28:54 +0200

    fix for TIKA-3089 contributed by pvanderweerd Wrapping text in pre-tags instead of p-tags will preserving formatting much better

--- tika-parsers/src/main/java/org/apache/tika/parser/txt/TXTParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/txt/TXTParser.java
@@ -97,14 +97,14 @@ public class TXTParser extends AbstractEncodingDetectorParser {
                     new XHTMLContentHandler(handler, metadata);
             xhtml.startDocument();
 
-            xhtml.startElement("p");
+            xhtml.startElement("pre");
             char[] buffer = new char[4096];
             int n = reader.read(buffer);
             while (n != -1) {
                 xhtml.characters(buffer, 0, n);
                 n = reader.read(buffer);
             }
-            xhtml.endElement("p");
+            xhtml.endElement("pre");
 
             xhtml.endDocument();
         }
