38e393e7d1b48a1490a67baf854972f30d1ed7b3
tallison <tallison@apache.org>
2020-06-03 14:02:38 -0400

    TIKA-3104 -- add bplist subtype detector

--- tika-parsers/src/test/java/org/apache/tika/parser/apple/BPListParserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/apple/BPListParserTest.java
@@ -17,7 +17,9 @@
 package org.apache.tika.parser.apple;
 
 import org.apache.tika.TikaTest;
+import org.apache.tika.metadata.HttpHeaders;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.sax.AbstractRecursiveParserWrapperHandler;
 import org.junit.Test;
 
@@ -26,7 +28,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 
-public class PListParserTest extends TikaTest {
+public class BPListParserTest extends TikaTest {
 
     @Test
     public void testBasicBinaryPList() throws Exception {
@@ -35,10 +37,24 @@ public class PListParserTest extends TikaTest {
         List<Metadata> metadataList = getRecursiveMetadata("testBPList.bplist");
         assertEquals(21, metadataList.size());
         Metadata m = metadataList.get(0);
+        assertEquals("application/x-itunes-bplist", m.get(Metadata.CONTENT_TYPE));
         String content = m.get(AbstractRecursiveParserWrapperHandler.TIKA_CONTENT);
         assertContains("<key>Application Version</key><string>9.0", content);
 
         //TODO -- bad encoding right after this...smart quote?
         assertContains("<string>90", content);
     }
+
+    @Test
+    public void testWebArchive() throws Exception {
+        List<Metadata> metadataList = getRecursiveMetadata("testWEBARCHIVE.webarchive");
+        assertEquals(12, metadataList.size());
+        Metadata m0 = metadataList.get(0);
+        assertEquals("application/x-webarchive", m0.get(Metadata.CONTENT_TYPE));
+        Metadata m1 = metadataList.get(1);
+        String content = m1.get(AbstractRecursiveParserWrapperHandler.TIKA_CONTENT);
+        assertContains("December 2008: Apache Tika Release", content);
+    }
+
+    //TODO -- add unit tests for memgraph
 }
