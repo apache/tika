8ba0221bb0b320de424a2e9b5240c59cb7625257
tallison <tallison@apache.org>
2020-08-12 15:46:34 -0400

    TIKA-3159 -- add detection and parser for flat ODF files and handle macros.
    # Conflicts:
    #	tika-parsers/src/main/java/org/apache/tika/parser/odf/OpenDocumentMetaParser.java
    #	tika-parsers/src/test/java/org/apache/tika/parser/odf/ODFParserTest.java

--- tika-core/src/test/java/org/apache/tika/mime/MimeTypesReaderTest.java
+++ tika-core/src/test/java/org/apache/tika/mime/MimeTypesReaderTest.java
@@ -34,6 +34,7 @@ import java.util.concurrent.Executors;
 
 import org.apache.tika.config.TikaConfig;
 import org.apache.tika.metadata.Metadata;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -57,6 +58,8 @@ public class MimeTypesReaderTest {
     private MimeTypes mimeTypes;
     private List<Magic> magics;
 
+    private String customMimeTypes;
+
     @SuppressWarnings("unchecked")
     @Before
     public void setUp() throws NoSuchFieldException, SecurityException,
@@ -66,6 +69,17 @@ public class MimeTypesReaderTest {
         Field magicsField = mimeTypes.getClass().getDeclaredField("magics");
         magicsField.setAccessible(true);
         magics = (List<Magic>)magicsField.get(mimeTypes);
+        //ensure reset of custom mimes path
+        customMimeTypes = System.getProperty(MimeTypesFactory.CUSTOM_MIMES_SYS_PROP);
+    }
+
+    @After
+    public void tearDown() {
+        if (customMimeTypes == null) {
+            System.clearProperty(MimeTypesFactory.CUSTOM_MIMES_SYS_PROP);
+        } else {
+            System.setProperty(MimeTypesFactory.CUSTOM_MIMES_SYS_PROP, customMimeTypes);
+        }
     }
     
     @Test
