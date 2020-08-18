dc1a8af263789d4568dd11fc7244431b3da065f6
PeterAlfredLee <peteralfredlee@gmail.com>
2020-08-18 09:24:03 +0800

    TIKA-2001 -- Add TextAndAttributeContentHandler and TextAndAttributeXMLParser

---
+++ tika-parser-modules/tika-parser-xml-module/src/main/java/org/apache/tika/parser/xml/TextAndAttributeXMLParser.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.parser.xml;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.TextAndAttributeContentHandler;
+import org.xml.sax.ContentHandler;
+
+public class TextAndAttributeXMLParser extends XMLParser {
+
+    private static final long serialVersionUID = 7796914007312429473L;
+
+    @Override
+    protected ContentHandler getContentHandler(ContentHandler handler, Metadata metadata, ParseContext context) {
+        return new TextAndAttributeContentHandler(handler, true);
+    }
+}
