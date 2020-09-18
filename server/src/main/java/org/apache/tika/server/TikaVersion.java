b2b6921017dffad20b802b430d95edf49e8b131e
Aditi Shankar <aditis@jpl.nasa.gov>
2020-09-18 12:43:36 -0700

    changed tika-sever artifact id to tika-server

---
+++ server/src/main/java/org/apache/tika/server/TikaVersion.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.server.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.apache.tika.Tika;
+
+@Path("/version")
+public class TikaVersion {
+    private Tika tika;
+
+    public TikaVersion() {
+        this.tika = new Tika(TikaResource.getConfig());
+    }
+
+    @GET
+    @Produces("text/plain")
+    public String getVersion() {
+        TikaResource.checkIsOperating();
+        return tika.toString();
+    }
+}
