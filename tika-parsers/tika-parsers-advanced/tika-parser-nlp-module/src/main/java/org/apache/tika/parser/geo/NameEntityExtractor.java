ea359c929eb5f37aa109d87cf74c1974838bb5d6
tallison <tallison@apache.org>
2021-03-22 17:53:20 -0400

    TIKA-3324 -- add checkstyle enforcement to tika-parsers and submodules.

--- tika-parsers/tika-parsers-advanced/tika-parser-nlp-module/src/main/java/org/apache/tika/parser/geo/NameEntityExtractor.java
+++ tika-parsers/tika-parsers-advanced/tika-parser-nlp-module/src/main/java/org/apache/tika/parser/geo/NameEntityExtractor.java
@@ -25,21 +25,19 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
-
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.util.Span;
+import org.apache.commons.io.IOUtils;
 
 public class NameEntityExtractor {
+    private final NameFinderME nameFinder;
     ArrayList<String> locationNameEntities;
     String bestNameEntity;
     private HashMap<String, Integer> tf;
-    private final NameFinderME nameFinder;
 
     public NameEntityExtractor(NameFinderME nameFinder) throws IOException {
         this.locationNameEntities = new ArrayList<String>();
@@ -52,14 +50,16 @@ public class NameEntityExtractor {
      * Use OpenNLP to extract location names that's appearing in the steam.
      * OpenNLP's default Name Finder accuracy is not very good, please refer to
      * its documentation.
-     * 
+     *
      * @param stream stream that passed from this.parse()
      */
     public void getAllNameEntitiesfromInput(InputStream stream) throws IOException {
         String[] in = IOUtils.toString(stream, UTF_8).split(" ");
-        Span nameE[];
-        
-        //name finder is not thread safe https://opennlp.apache.org/documentation/1.5.2-incubating/manual/opennlp.html#tools.namefind
+        Span[] nameE;
+
+        //name finder is not thread safe
+        //https://opennlp.apache.org/documentation/1.5.2-incubating/manual/
+        // opennlp.html#tools.namefind
         synchronized (nameFinder) {
             nameE = nameFinder.find(in);
             //the same name finder is reused, so clear adaptive data
@@ -82,13 +82,14 @@ public class NameEntityExtractor {
      * Get the best location entity extracted from the input stream. Simply
      * return the most frequent entity, If there several highest frequent
      * entity, pick one randomly. May not be the optimal solution, but works.
-     * 
+     *
      * @param locationNameEntities OpenNLP name finder's results, stored in
      * ArrayList
      */
     public void getBestNameEntity() {
-        if (this.locationNameEntities.size() == 0)
+        if (this.locationNameEntities.size() == 0) {
             return;
+        }
 
         for (String locationNameEntity : this.locationNameEntities) {
             if (tf.containsKey(locationNameEntity)) {
@@ -103,7 +104,7 @@ public class NameEntityExtractor {
         list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
 
         this.locationNameEntities.clear();// update so that they are in
-                                          // descending order
+        // descending order
         for (Map.Entry<String, Integer> entry : list) {
             this.locationNameEntities.add(entry.getKey());
             if (entry.getValue() > max) {
