de1835b7543bb0abb65774b37e59b927d28ed4bc
tballison <tallison@apache.org>
2022-03-18 10:15:06 -0400

    TIKA-3675 - general upgrades for 2.4.0

--- tika-integration-tests/tika-pipes-opensearch-integration-tests/src/test/java/org/apache/tika/pipes/opensearch/tests/TikaPipesOpenSearchTest.java
+++ tika-integration-tests/tika-pipes-opensearch-integration-tests/src/test/java/org/apache/tika/pipes/opensearch/tests/TikaPipesOpenSearchTest.java
@@ -28,7 +28,7 @@ import org.apache.tika.pipes.xsearch.tests.TikaPipesXSearchBase;
 
 public class TikaPipesOpenSearchTest extends TikaPipesXSearchBase {
 
-    private static final String DOCKER_IMAGE_NAME = "opensearchproject/opensearch:1.2.1";
+    private static final String DOCKER_IMAGE_NAME = "opensearchproject/opensearch:1.3.0";
 
     @ClassRule
     public static GenericContainer<?> OPEN_SEARCH_CONTAINER =
