8ff887fc2603b3d09dc974e7c79eae0e2d258250
tallison <tallison@apache.org>
2022-04-26 11:36:03 -0400

    TIKA-3721 -- add DGN8Parser for metadata via Dan Coldrick -- put unit test in correct package.

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-cad-module/src/test/java/org/apache/tika/parser/dgn/DGN8ParserTest.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-cad-module/src/test/java/org/apache/tika/parser/dgn/DGN8ParserTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.tika.parser.prt;
+package org.apache.tika.parser.dgn;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
