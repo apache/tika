04d00a0aecd0dc4c0f4250fa972d407face06bb5
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2022-07-25 11:52:34 -0500

    TIKA-3820 add tika pipes kafka support (#616)
    * wip - add kafka support
    
    * wip - add kafka support
    
    * finish setting up intiial kafka commit - tests pass.
    
    * fix readme
    
    * remove dead file, rename good test
    
    * remove stuff from earlier test attempts
    
    * deleted wrong file previously.
    
    * bump version after merge.
    
    * add an emit max in case the documents keep coming into the pipe iterator forever.
    
    * clean up javadoc.
    
    * fix a few issues with the test.
    
    * remove commented out line.
    
    * no need to create the topics manually after i fixed issue with conflicting group ids
    
    * remove create topic code.
    
    * add javadoc prefix
    
    * looks like a refactor renamed a couple files
    
    * improve messages
    
    * fix poor message in javadoc.
    
    * apply change suggested in https://stackoverflow.com/a/73024959/1174024
    
    * clarify
    
    * fix message
    
    * fix annoying IDE warnings

---
+++ tika-pipes/tika-emitters/tika-emitter-kafka/src/main/java/org/apache/tika/pipes/emitter/kafka/KafkaEmitter.java
@@ -0,0 +1,284 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.pipes.emitter.kafka;
+
+import static org.apache.tika.config.TikaConfig.mustNotBeEmpty;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.tika.config.Field;
+import org.apache.tika.config.Initializable;
+import org.apache.tika.config.InitializableProblemHandler;
+import org.apache.tika.config.Param;
+import org.apache.tika.exception.TikaConfigException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.pipes.emitter.AbstractEmitter;
+import org.apache.tika.pipes.emitter.TikaEmitterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Emits the now-parsed documents into a specified Apache Kafka topic.
+ */
+public class KafkaEmitter extends AbstractEmitter implements Initializable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaEmitter.class);
+
+    private static final ObjectMapper OM = new ObjectMapper();
+
+    String topic;
+    String bootstrapServers;
+
+    String acks = "all";
+    int lingerMs = 5000;
+    int batchSize = 16384;
+    int bufferMemory = 32 * 1024 * 1024;
+    String compressionType = "none";
+    int connectionsMaxIdleMs = 9 * 60 * 1000;
+    int deliveryTimeoutMs = 120 * 1000;
+    boolean enableIdempotence = false;
+    String interceptorClasses;
+    int maxBlockMs = 60 * 1000;
+    int maxInFlightRequestsPerConnection = 5;
+    int maxRequestSize = 1024 * 1024;
+    int metadataMaxAgeMs = 5 * 60 * 1000;
+    int requestTimeoutMs = 30 * 1000;
+    int retries = Integer.MAX_VALUE;
+    int retryBackoffMs = 100;
+    int transactionTimeoutMs = 60000;
+    String transactionalId;
+    String clientId;
+    String keySerializer;
+    String valueSerializer;
+
+    private Producer<String, String> producer;
+
+    @Field
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    @Field
+    public void setAcks(String acks) {
+        this.acks = acks;
+    }
+
+    @Field
+    public void setLingerMs(int lingerMs) {
+        this.lingerMs = lingerMs;
+    }
+
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    @Field
+    public void setBufferMemory(int bufferMemory) {
+        this.bufferMemory = bufferMemory;
+    }
+
+    @Field
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    @Field
+    public void setCompressionType(String compressionType) {
+        this.compressionType = compressionType;
+    }
+
+    @Field
+    public void setConnectionsMaxIdleMs(int connectionsMaxIdleMs) {
+        this.connectionsMaxIdleMs = connectionsMaxIdleMs;
+    }
+
+    @Field
+    public void setDeliveryTimeoutMs(int deliveryTimeoutMs) {
+        this.deliveryTimeoutMs = deliveryTimeoutMs;
+    }
+
+    @Field
+    public void setEnableIdempotence(boolean enableIdempotence) {
+        this.enableIdempotence = enableIdempotence;
+    }
+
+    @Field
+    public void setInterceptorClasses(String interceptorClasses) {
+        this.interceptorClasses = interceptorClasses;
+    }
+
+    @Field
+    public void setMaxBlockMs(int maxBlockMs) {
+        this.maxBlockMs = maxBlockMs;
+    }
+
+    @Field
+    public void setMaxInFlightRequestsPerConnection(int maxInFlightRequestsPerConnection) {
+        this.maxInFlightRequestsPerConnection = maxInFlightRequestsPerConnection;
+    }
+
+    @Field
+    public void setMaxRequestSize(int maxRequestSize) {
+        this.maxRequestSize = maxRequestSize;
+    }
+
+    @Field
+    public void setMetadataMaxAgeMs(int metadataMaxAgeMs) {
+        this.metadataMaxAgeMs = metadataMaxAgeMs;
+    }
+
+    @Field
+    public void setRequestTimeoutMs(int requestTimeoutMs) {
+        this.requestTimeoutMs = requestTimeoutMs;
+    }
+
+    @Field
+    public void setRetries(int retries) {
+        this.retries = retries;
+    }
+
+    @Field
+    public void setRetryBackoffMs(int retryBackoffMs) {
+        this.retryBackoffMs = retryBackoffMs;
+    }
+
+    @Field
+    public void setTransactionTimeoutMs(int transactionTimeoutMs) {
+        this.transactionTimeoutMs = transactionTimeoutMs;
+    }
+
+    @Field
+    public void setTransactionalId(String transactionalId) {
+        this.transactionalId = transactionalId;
+    }
+
+    @Field
+    public void setKeySerializer(String keySerializer) {
+        this.keySerializer = keySerializer;
+    }
+
+    @Field
+    public void setValueSerializer(String valueSerializer) {
+        this.valueSerializer = valueSerializer;
+    }
+
+    @Field
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public void emit(String emitKey, List<Metadata> metadataList)
+            throws IOException, TikaEmitterException {
+        if (metadataList == null || metadataList.isEmpty()) {
+            throw new TikaEmitterException("metadata list must not be null or of size 0");
+        }
+        for (Metadata metadata : metadataList) {
+            LOGGER.debug("about to emit to target topic: ({}) path:({})", topic, emitKey);
+
+            Map<String, Object> fields = new HashMap<>();
+            for (String n : metadata.names()) {
+                String[] vals = metadata.getValues(n);
+                if (vals.length > 1) {
+                    LOGGER.warn("Can only write the first value for key {}. I see {} values.",
+                            n,
+                            vals.length);
+                }
+                fields.put(n, vals[0]);
+            }
+
+            producer.send(new ProducerRecord<>(topic, emitKey, OM.writeValueAsString(fields)));
+        }
+    }
+
+    private void safePut(Properties props, String key, Object val) {
+        if (val != null) {
+            props.put(key, val);
+        }
+    }
+
+    @Override
+    public void initialize(Map<String, Param> params) throws TikaConfigException {
+
+        // create instance for properties to access producer configs   
+        Properties props = new Properties();
+
+        //Assign localhost id
+        safePut(props, ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        //Set acknowledgements for producer requests.      
+        safePut(props, ProducerConfig.ACKS_CONFIG, acks);
+
+        //If the request fails, the producer can automatically retry,
+        safePut(props, ProducerConfig.RETRIES_CONFIG, retries);
+
+        //Specify buffer size in config
+        safePut(props, ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+
+        //Reduce the no of requests less than 0   
+        safePut(props, ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.   
+        safePut(props, ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
+
+        safePut(props, ProducerConfig.CLIENT_ID_CONFIG, clientId);
+        safePut(props, ProducerConfig.COMPRESSION_TYPE_CONFIG, compressionType);
+        safePut(props, ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeoutMs);
+        safePut(props, ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        safePut(props, ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptorClasses);
+        safePut(props, ProducerConfig.MAX_BLOCK_MS_CONFIG, maxBlockMs);
+        safePut(props, ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequestsPerConnection);
+        safePut(props, ProducerConfig.MAX_REQUEST_SIZE_CONFIG, maxRequestSize);
+        safePut(props, ProducerConfig.METADATA_MAX_AGE_CONFIG, metadataMaxAgeMs);
+        safePut(props, ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);
+        safePut(props, ProducerConfig.RETRY_BACKOFF_MS_CONFIG, retryBackoffMs);
+        safePut(props, ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, transactionTimeoutMs);
+        safePut(props, ProducerConfig.TRANSACTIONAL_ID_CONFIG, transactionalId);
+
+        safePut(props, ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializerClass(keySerializer, StringSerializer.class));
+        safePut(props, ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializerClass(valueSerializer, StringSerializer.class));
+
+        producer = new KafkaProducer<>(props);
+    }
+
+    private Object serializerClass(String className, Class defaultClass) {
+        try {
+            return className == null ? defaultClass : Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Could not find key serializer class: {}", className);
+            return null;
+        }
+    }
+
+    @Override
+    public void checkInitialization(InitializableProblemHandler problemHandler)
+            throws TikaConfigException {
+        mustNotBeEmpty("topic", this.topic);
+        mustNotBeEmpty("server", this.bootstrapServers);
+    }
+
+}
---
+++ tika-pipes/tika-pipes-iterators/tika-pipes-iterator-kafka/src/main/java/org/apache/tika/pipes/pipesiterator/kafka/KafkaPipesIterator.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.pipes.pipesiterator.kafka;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.tika.config.Field;
+import org.apache.tika.config.Initializable;
+import org.apache.tika.config.InitializableProblemHandler;
+import org.apache.tika.config.Param;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.exception.TikaConfigException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.pipes.FetchEmitTuple;
+import org.apache.tika.pipes.HandlerConfig;
+import org.apache.tika.pipes.emitter.EmitKey;
+import org.apache.tika.pipes.fetcher.FetchKey;
+import org.apache.tika.pipes.pipesiterator.PipesIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaPipesIterator extends PipesIterator implements Initializable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaPipesIterator.class);
+    String topic;
+    String bootstrapServers;
+    String keySerializer;
+    String valueSerializer;
+    String groupId;
+    String autoOffsetReset = "earliest";
+    int pollDelayMs = 100;
+    int emitMax = -1;
+    int groupInitialRebalanceDelayMs = 3000;
+
+    private Properties props;
+    private KafkaConsumer<String, String> consumer;
+
+
+    @Field
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Field
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    @Field
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    @Field
+    public void setKeySerializer(String keySerializer) {
+        this.keySerializer = keySerializer;
+    }
+
+    @Field
+    public void setAutoOffsetReset(String autoOffsetReset) {
+        this.autoOffsetReset = autoOffsetReset;
+    }
+
+    @Field
+    public void setValueSerializer(String valueSerializer) {
+        this.valueSerializer = valueSerializer;
+    }
+
+    @Field
+    public void setPollDelayMs(int pollDelayMs) {
+        this.pollDelayMs = pollDelayMs;
+    }
+
+    @Field
+    public void setGroupInitialRebalanceDelayMs(int groupInitialRebalanceDelayMs) {
+        this.groupInitialRebalanceDelayMs = groupInitialRebalanceDelayMs;
+    }
+
+    /**
+     * If the kafka pipe iterator will keep polling for more documents until it returns an empty result.
+     * If you set emitMax is set to > 0, it will stop polling if the number of documents you
+     * have emitted so far > emitMax.
+     */
+    @Field
+    public void setEmitMax(int emitMax) {
+        this.emitMax = emitMax;
+    }
+
+    private void safePut(Properties props, String key, Object val) {
+        if (val != null) {
+            props.put(key, val);
+        }
+    }
+
+    @Override
+    public void initialize(Map<String, Param> params) {
+        props = new Properties();
+        safePut(props, ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        safePut(props, ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, serializerClass(keySerializer, StringDeserializer.class));
+        safePut(props, ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, serializerClass(valueSerializer, StringDeserializer.class));
+        safePut(props, ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        safePut(props, ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        safePut(props, "group.inital.rebalance.delay.ms", groupInitialRebalanceDelayMs);
+        consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+    }
+
+    private Object serializerClass(String className, Class defaultClass) {
+        try {
+            return className == null ? defaultClass : Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Could not find key serializer class: {}", className);
+            return null;
+        }
+    }
+
+    @Override
+    public void checkInitialization(InitializableProblemHandler problemHandler)
+            throws TikaConfigException {
+        super.checkInitialization(problemHandler);
+        TikaConfig.mustNotBeEmpty("bootstrapServers", this.bootstrapServers);
+        TikaConfig.mustNotBeEmpty("topic", this.topic);
+    }
+
+    @Override
+    protected void enqueue() throws InterruptedException, TimeoutException {
+        String fetcherName = getFetcherName();
+        String emitterName = getEmitterName();
+        long start = System.currentTimeMillis();
+        int count = 0;
+        HandlerConfig handlerConfig = getHandlerConfig();
+        ConsumerRecords<String, String> records;
+
+        do {
+            records = consumer.poll(Duration.ofMillis(pollDelayMs));
+            for (ConsumerRecord<String, String> r : records) {
+                long elapsed = System.currentTimeMillis() - start;
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("adding ({}) {} in {} ms", count, r.key(), elapsed);
+                }
+                tryToAdd(new FetchEmitTuple(r.key(), new FetchKey(fetcherName,
+                        r.key()),
+                        new EmitKey(emitterName, r.key()), new Metadata(), handlerConfig,
+                        getOnParseException()));
+                ++count;
+            }
+        } while ((emitMax > 0 || count < emitMax) && !records.isEmpty());
+        long elapsed = System.currentTimeMillis() - start;
+        LOGGER.info("Finished enqueuing {} files in {} ms", count, elapsed);
+    }
+}
