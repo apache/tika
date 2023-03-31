12904468ce6fcefff7bd618a226e63cbd9eb3e1e
tballison <tallison@apache.org>
2023-03-31 16:58:41 -0400

    fix tika-app

--- tika-app/src/main/java/org/apache/tika/cli/TikaCLI.java
+++ tika-app/src/main/java/org/apache/tika/cli/TikaCLI.java
@@ -698,7 +698,7 @@ public class TikaCLI {
 
             parser = new AutoDetectParser(config);
             if (digester != null) {
-                parser = new DigestingParser(parser, digester);
+                parser = new DigestingParser(parser, digester, false);
                 LOG.info("As of Tika 2.5.0, you can set the digester via the AutoDetectParserConfig in " +
                         "tika-config.xml. We plan to remove this commandline option in 2.8.0");
             }
