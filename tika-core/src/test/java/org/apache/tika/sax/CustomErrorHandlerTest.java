a781b558e469bf599664e2778246665238232028
Ravi Jha <ravij@opentext.com>
2023-06-09 12:05:18 +0530

    TIKA-4062: Test cases for custom error handling during XML parsing

---
+++ tika-core/src/test/java/org/apache/tika/sax/CustomErrorHandlerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.sax;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.tika.TikaTest;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.utils.XMLReaderUtils;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+/**
+ * Test that validates a custom {@link ContentHandlerDecorator} can handle errors during XML parsing
+ *
+ * @see <a href="https://issues.apache.org/jira/browse/TIKA-4062">TIKA-4062</a>
+ */
+public class CustomErrorHandlerTest extends TikaTest{
+
+	private void extractXml(InputStream blobStream, OutputStream textStream)
+			throws IOException, SAXException, TikaException, ParserConfigurationException {
+		
+		try {
+			ToXMLContentHandler contentHandler = new ToXMLContentHandler(textStream, StandardCharsets.UTF_8.toString());
+			NonValidatingContentHandler handler = new NonValidatingContentHandler(contentHandler);
+			XMLReaderUtils.parseSAX(blobStream, handler, new ParseContext());
+		} finally {
+			textStream.close();
+		}
+	}
+
+	private String extractTestData(String name)
+			throws IOException, SAXException, TikaException, ParserConfigurationException {
+		try (InputStream is = getResourceAsStream("/test-documents/" + name)) {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			extractXml(is, out);
+			return out.toString(StandardCharsets.UTF_8);
+		}
+	}
+
+	@Test
+	void testUndeclaredEntityXML() throws Exception {
+		try {
+			System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.tika.sax.ErrorResistentSAXParserFactory");
+			String content = extractTestData("undeclared_entity.xml");		
+			assertContains("START", content);
+			//This assertion passes only if custom error handler is called to handle fatal exception
+			assertContains("END", content);
+		}  catch (SAXException e) {
+			 fail("Exception resturned from parser and not handled in error handler " + e);
+		} 
+	}
+}
---
+++ tika-core/src/test/java/org/apache/tika/sax/NonValidatingContentHandler.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.sax;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+
+
+public class NonValidatingContentHandler extends ContentHandlerDecorator {
+    class ClosedInputStream extends InputStream {
+
+        /**
+         * Returns -1 to indicate that the stream is closed.
+         *
+         * @return always -1
+         */
+        @Override
+        public int read() {
+            return -1;
+        }
+
+    }
+
+    public NonValidatingContentHandler(ContentHandler handler) {
+        super(handler);
+    }
+    
+    @Override
+    public void warning(SAXParseException e) throws SAXException {
+    	//NO-OP
+    }
+
+    @Override
+    public void error(SAXParseException e) throws SAXException {
+        //NO-OP
+    }
+
+    @Override
+    public void fatalError(SAXParseException e) throws SAXException {
+    	//NO-OP
+    }
+    
+    @Override
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+        return new InputSource(new ClosedInputStream());
+    }
+    
+}
