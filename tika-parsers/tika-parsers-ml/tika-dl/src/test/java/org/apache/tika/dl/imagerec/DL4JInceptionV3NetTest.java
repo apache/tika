d02b8e822a9a9b7788022ece7d4c1e20505cd9a4
tallison <tallison@apache.org>
2023-11-03 17:59:32 -0400

    Add more robust handling for 403 for model downloading

--- tika-parsers/tika-parsers-ml/tika-dl/src/test/java/org/apache/tika/dl/imagerec/DL4JInceptionV3NetTest.java
+++ tika-parsers/tika-parsers-ml/tika-dl/src/test/java/org/apache/tika/dl/imagerec/DL4JInceptionV3NetTest.java
@@ -39,7 +39,7 @@ public class DL4JInceptionV3NetTest {
             config = new TikaConfig(is);
         } catch (Exception e) {
             if (e.getMessage() != null && (e.getMessage().contains("Connection refused") ||
-                    e.getMessage().contains("connect timed out"))) {
+                    e.getMessage().contains("connect timed out") || e.getMessage().contains("403 for URL"))) {
                 assumeTrue(false, "skipping test because of connection issue");
             }
             throw e;
