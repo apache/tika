ebdb6fc87f5eb390d76ffc9bb7aa747068d531ee
Tim Allison <tallison@apache.org>
2024-03-25 13:27:19 -0400

    TIKA-4220 -- temporary workaround for tar detection regression (#1687) -- main branch

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pkg-module/src/main/java/org/apache/tika/parser/pkg/PackageParser.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pkg-module/src/main/java/org/apache/tika/parser/pkg/PackageParser.java
@@ -61,6 +61,7 @@ import org.xml.sax.helpers.AttributesImpl;
 
 import org.apache.tika.config.Field;
 import org.apache.tika.detect.EncodingDetector;
+import org.apache.tika.detect.zip.TikaArchiveStreamFactory;
 import org.apache.tika.exception.EncryptedDocumentException;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.extractor.EmbeddedDocumentExtractor;
@@ -253,6 +254,16 @@ public class PackageParser extends AbstractEncodingDetectorParser {
         }
 
         TemporaryResources tmp = new TemporaryResources();
+        try {
+            _parse(stream, handler, metadata, context, tmp);
+        } finally {
+            tmp.close();
+        }
+    }
+
+    private void _parse(InputStream stream, ContentHandler handler, Metadata metadata,
+                ParseContext context, TemporaryResources tmp)
+            throws TikaException, IOException, SAXException {
         ArchiveInputStream ais = null;
         String encoding = null;
         try {
@@ -262,9 +273,23 @@ public class PackageParser extends AbstractEncodingDetectorParser {
             // At the end we want to close the archive stream to release
             // any associated resources, but the underlying document stream
             // should not be closed
-
-            ais = factory.createArchiveInputStream(CloseShieldInputStream.wrap(stream));
-
+            //TODO -- fix this when we next upgrade commons-compress
+            //TODO -- we've probably already detected the stream by here. We should
+            //rely on that detection and not re-detect.
+            if (factory != null) {
+                encoding = factory.getEntryEncoding();
+                // At the end we want to close the archive stream to release
+                // any associated resources, but the underlying document stream
+                // should not be closed
+                ais = factory.createArchiveInputStream(new CloseShieldInputStream(stream));
+            } else {
+                TikaArchiveStreamFactory tikaFactory = new TikaArchiveStreamFactory();
+                encoding = tikaFactory.getEntryEncoding();
+                // At the end we want to close the archive stream to release
+                // any associated resources, but the underlying document stream
+                // should not be closed
+                ais = tikaFactory.createArchiveInputStream(new CloseShieldInputStream(stream));
+            }
         } catch (StreamingNotSupportedException sne) {
             // Most archive formats work on streams, but a few need files
             if (sne.getFormat().equals(ArchiveStreamFactory.SEVEN_Z)) {
@@ -294,11 +319,9 @@ public class PackageParser extends AbstractEncodingDetectorParser {
                 // Pending a fix for COMPRESS-269 / TIKA-1525, this bit is a little nasty
                 ais = new SevenZWrapper(sevenz);
             } else {
-                tmp.close();
                 throw new TikaException("Unknown non-streaming format " + sne.getFormat(), sne);
             }
         } catch (ArchiveException e) {
-            tmp.close();
             throw new TikaException("Unable to unpack document stream", e);
         }
 
@@ -330,7 +353,6 @@ public class PackageParser extends AbstractEncodingDetectorParser {
             }
         } finally {
             ais.close();
-            tmp.close();
             xhtml.endDocument();
         }
     }
@@ -518,7 +540,7 @@ public class PackageParser extends AbstractEncodingDetectorParser {
         @Override
         public void close() throws IOException {
             file.close();
-        }   
+        }
     }
 
     /**
