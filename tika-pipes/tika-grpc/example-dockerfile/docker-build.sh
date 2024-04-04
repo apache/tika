a969ae1a53c4478c2e70af3b432a55414029857c
Nicholas DiPiazza <ndipiazza@apache.org>
2024-04-04 10:28:07 -0500

    add quotes

--- tika-pipes/tika-grpc/example-dockerfile/docker-build.sh
+++ tika-pipes/tika-grpc/example-dockerfile/docker-build.sh
@@ -5,30 +5,30 @@ if [ -z "${TAG_NAME}" ]; then
     exit 1
 fi
 
-SCRIPT_DIR=$( cd -- $( dirname -- ${BASH_SOURCE[0]} ) &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TIKA_SRC_PATH=${SCRIPT_DIR}/../../..
 DEST_DIR=${TIKA_SRC_PATH}/tika-pipes/tika-grpc/target/tika-docker
 
-mvn clean install -DskipTests=true -f ${TIKA_SRC_PATH}
-mvn dependency:copy-dependencies -f ${TIKA_SRC_PATH}/tika-pipes/tika-grpc
-rm -rf ${DEST_DIR}
-mkdir -p ${DEST_DIR}
+mvn clean install -DskipTests=true -f "${TIKA_SRC_PATH}"
+mvn dependency:copy-dependencies -f "${TIKA_SRC_PATH}/tika-pipes/tika-grpc"
+rm -rf "${DEST_DIR}"
+mkdir -p "${DEST_DIR}"
 
-cp -r ${TIKA_SRC_PATH}/tika-pipes/tika-grpc/target/dependency ${DEST_DIR}/libs
-cp ${TIKA_SRC_PATH}/tika-pipes/tika-grpc/target/tika-grpc-*.jar ${DEST_DIR}/libs
-cp ${TIKA_SRC_PATH}/tika-pipes/tika-grpc/src/test/resources/log4j2.xml ${DEST_DIR}
-cp ${TIKA_SRC_PATH}/tika-pipes/tika-grpc/src/test/resources/tika-pipes-test-config.xml ${DEST_DIR}/tika-config.xml
-cp ${TIKA_SRC_PATH}/tika-pipes/tika-grpc/example-dockerfile/Dockerfile ${DEST_DIR}/Dockerfile
+cp -r "${TIKA_SRC_PATH}/tika-pipes/tika-grpc/target/dependency" "${DEST_DIR}/libs"
+cp "${TIKA_SRC_PATH}/tika-pipes/tika-grpc/target/tika-grpc-"*".jar" "${DEST_DIR}/libs"
+cp "${TIKA_SRC_PATH}/tika-pipes/tika-grpc/src/test/resources/log4j2.xml" "${DEST_DIR}"
+cp "${TIKA_SRC_PATH}/tika-pipes/tika-grpc/src/test/resources/tika-pipes-test-config.xml" "${DEST_DIR}/tika-config.xml"
+cp "${TIKA_SRC_PATH}/tika-pipes/tika-grpc/example-dockerfile/Dockerfile" "${DEST_DIR}/Dockerfile"
 
-cd ${DEST_DIR}
+cd "${DEST_DIR}" || exit
 
 # build single arch
-#docker build ${DEST_DIR} -t ${TAG_NAME}
+#docker build "${DEST_DIR}" -t "${TAG_NAME}"
 
 # Or we can build multi-arch - https://www.docker.com/blog/multi-arch-images/
 docker buildx create --name tikabuilder
 # see https://askubuntu.com/questions/1339558/cant-build-dockerfile-for-arm64-due-to-libc-bin-segmentation-fault/1398147#1398147
 docker run --rm --privileged tonistiigi/binfmt --install amd64
 docker run --rm --privileged tonistiigi/binfmt --install arm64
-docker buildx build --builder=tikabuilder ${DEST_DIR} -t ${TAG_NAME} --platform linux/amd64,linux/arm64 --push
+docker buildx build --builder=tikabuilder "${DEST_DIR}" -t "${TAG_NAME}" --platform linux/amd64,linux/arm64 --push
 docker buildx stop tikabuilder
