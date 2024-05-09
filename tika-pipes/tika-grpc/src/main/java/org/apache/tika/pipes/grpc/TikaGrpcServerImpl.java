050971e6be6f64fb7581d81b793dafa8bfcbe3a8
Nicholas DiPiazza <ndipiazza@apache.org>
2024-05-09 09:32:06 -0500

    TIKA-4252: fix metadata issue

--- tika-pipes/tika-grpc/src/main/java/org/apache/tika/pipes/grpc/TikaGrpcServerImpl.java
+++ tika-pipes/tika-grpc/src/main/java/org/apache/tika/pipes/grpc/TikaGrpcServerImpl.java
@@ -65,6 +65,7 @@ import org.apache.tika.exception.TikaConfigException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.Property;
 import org.apache.tika.pipes.FetchEmitTuple;
+import org.apache.tika.pipes.HandlerConfig;
 import org.apache.tika.pipes.PipesClient;
 import org.apache.tika.pipes.PipesConfig;
 import org.apache.tika.pipes.PipesResult;
@@ -208,8 +209,7 @@ class TikaGrpcServerImpl extends TikaGrpc.TikaImplBase {
                 }
             }
             PipesResult pipesResult = pipesClient.process(new FetchEmitTuple(request.getFetchKey(),
-                    new FetchKey(fetcher.getName(), request.getFetchKey()), new EmitKey(),
-                    FetchEmitTuple.ON_PARSE_EXCEPTION.SKIP));
+                    new FetchKey(fetcher.getName(), request.getFetchKey()), new EmitKey(), tikaMetadata, HandlerConfig.DEFAULT_HANDLER_CONFIG, FetchEmitTuple.ON_PARSE_EXCEPTION.SKIP));
             FetchAndParseReply.Builder fetchReplyBuilder =
                     FetchAndParseReply.newBuilder().setFetchKey(request.getFetchKey());
             if (pipesResult.getEmitData() != null && pipesResult.getEmitData().getMetadataList() != null) {
