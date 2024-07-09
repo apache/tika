e5a0265ff6737d96f8f1730f8c544a112ce3386f
tallison <tallison@apache.org>
2024-07-09 17:19:54 -0400

    fix rat checks

--- tika-pipes/tika-grpc/example-dockerfile/docker-build.sh
+++ tika-pipes/tika-grpc/example-dockerfile/docker-build.sh
@@ -1,4 +1,15 @@
-TAG_NAME=$1
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# TAG_NAME=$1
 
 if [ -z "${TAG_NAME}" ]; then
     echo "Single command line argument is required which will be used as the -t parameter of the docker build command"
