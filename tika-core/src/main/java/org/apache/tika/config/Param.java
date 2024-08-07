5863923aff4bffcedce84f9636ee87dfb2d6c9fe
Tilman Hausherr <tilman@snafu.de>
2024-08-07 06:51:12 +0200

    remove unused import

--- tika-core/src/main/java/org/apache/tika/config/Param.java
+++ tika-core/src/main/java/org/apache/tika/config/Param.java
@@ -29,7 +29,6 @@ import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
