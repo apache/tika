ac6620352217f391e83fcafbf0f4a369762b2975
Tilman Hausherr <tilman@apache.org>
2024-08-10 19:40:11 +0200

    TIKA-4290: replace deprecated

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pkg-module/src/main/java/org/apache/tika/detect/gzip/GZipSpecializationDetector.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pkg-module/src/main/java/org/apache/tika/detect/gzip/GZipSpecializationDetector.java
@@ -22,7 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
-import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.UnsynchronizedByteArrayInputStream;
 import org.apache.commons.io.output.UnsynchronizedByteArrayOutputStream;
 
@@ -48,7 +48,9 @@ public class GZipSpecializationDetector implements Detector {
         input.mark(2);
         byte[] firstTwo = new byte[2];
         try {
-            IOUtils.readFully(input, firstTwo);
+            // do not change this to commons-io IOUtils.readFully because
+            // org.apache.tika.parser.AutoDetectParserConfigTest tests will fail
+            org.apache.commons.compress.utils.IOUtils.readFully(input, firstTwo);
         } finally {
             input.reset();
         }
@@ -64,7 +66,7 @@ public class GZipSpecializationDetector implements Detector {
         int buffSize = 1024;
         UnsynchronizedByteArrayOutputStream gzippedBytes = UnsynchronizedByteArrayOutputStream.builder().get();
         try {
-            IOUtils.copyRange(input, buffSize, gzippedBytes);
+            IOUtils.copyLarge(input, gzippedBytes, 0, buffSize);
         } catch (IOException e) {
             //swallow
         } finally {
