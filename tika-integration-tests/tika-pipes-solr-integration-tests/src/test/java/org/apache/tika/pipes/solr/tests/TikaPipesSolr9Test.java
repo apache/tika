d9dfe20b806e7e1341fefa6bb84c9361ab37e361
Tim Allison <tallison@apache.org>
2024-10-22 09:01:16 -0400

    TIKA-4321 -- rm solr 6 and 7 unit tests and turn logging to error. (#1994)
    * TIKA-4321 -- rm solr 6 and 7 unit tests and turn logging to error.

--- tika-integration-tests/tika-pipes-solr-integration-tests/src/test/java/org/apache/tika/pipes/solr/tests/TikaPipesSolr9Test.java
+++ tika-integration-tests/tika-pipes-solr-integration-tests/src/test/java/org/apache/tika/pipes/solr/tests/TikaPipesSolr9Test.java
@@ -23,7 +23,7 @@ public class TikaPipesSolr9Test extends TikaPipesSolrTestBase {
 
     @Override
     public String getSolrImageName() {
-        return "solr:9.3";
+        return "solr:9.7.0";
     }
 
     @Override
