80b7f1c9cba38927db67eb0d3a3178c3ed10c868
Tim Allison <tallison@apache.org>
2024-10-30 09:28:29 -0400

    TIKA-4341 -- fix MetadataListFilter's deserialization (#2030)

---
+++ tika-core/src/test/java/org/apache/tika/metadata/listfilter/MetadataListFilterTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.metadata.listfilter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+import org.apache.tika.config.TikaConfig;
+
+public class MetadataListFilterTest {
+
+    @Test
+    public void testBasic() throws Exception {
+        TikaConfig tikaConfig;
+        try (InputStream is = MetadataListFilterTest.class.getResourceAsStream(
+                "metadatalistfilter-config.xml")) {
+            tikaConfig = new TikaConfig(is);
+        }
+        CompositeMetadataListFilter compositeMetadataListFilter = (CompositeMetadataListFilter) tikaConfig.getMetadataListFilter();
+        assertEquals(1, compositeMetadataListFilter.getFilters().size());
+        assertTrue(compositeMetadataListFilter.getFilters().get(0) instanceof AttachmentCountingListFilter);
+    }
+}
