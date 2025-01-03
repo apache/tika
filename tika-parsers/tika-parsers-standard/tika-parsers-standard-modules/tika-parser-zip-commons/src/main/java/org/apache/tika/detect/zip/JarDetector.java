64acd09e7d82b94a286399190ad711153427c52a
subbudvk <115633743+subbudvk@users.noreply.github.com>
2025-01-03 19:43:24 +0530

    TIKA-4365 : feat : Support Detection for AAB Bundle file

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-zip-commons/src/main/java/org/apache/tika/detect/zip/JarDetector.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-zip-commons/src/main/java/org/apache/tika/detect/zip/JarDetector.java
@@ -46,6 +46,10 @@ public class JarDetector implements ZipContainerDetector {
             if (zip.getEntry("META-INF/application.xml") != null) {
                 return MediaType.application("x-tika-java-enterprise-archive");
             }
+            // AAB archive
+             if (zip.getEntry("base/manifest/AndroidManifest.xml") != null) {
+                return MediaType.application("x-authorware-bin");
+            }
 
             // Looks like a regular Jar Archive
             return MediaType.application("java-archive");
@@ -69,7 +73,10 @@ public class JarDetector implements ZipContainerDetector {
         } else if (name.equals("META-INF/MANIFEST.MF")) {
             // It's a Jar file, or something based on Jar
             detectContext.set(SeenManifest.class, SEEN_MANIFEST);
-        }
+        } else if (name.equals("base/manifest/AndroidManifest.xml") != null) {
+                return MediaType.application("x-authorware-bin");
+            }
+        
         SeenManifest seenManifest = detectContext.get(SeenManifest.class);
 
         if (seenManifest != null) {
@@ -79,7 +86,11 @@ public class JarDetector implements ZipContainerDetector {
             } else if (name.equals("WEB-INF/")) {
                 // Check for WAR and EAR
                 return MediaType.application("x-tika-java-web-archive");
+            } else if (name.equals("base/manifest/AndroidManifest.xml") != null) {
+                // Check for AAB bundle
+                return MediaType.application("x-authorware-bin");
             }
+        
             if (name.equals("META-INF/application.xml")) {
                 return MediaType.application("x-tika-java-enterprise-archive");
             }
