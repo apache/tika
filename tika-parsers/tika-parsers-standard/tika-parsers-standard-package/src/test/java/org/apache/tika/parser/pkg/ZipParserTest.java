04b829d083a15105b7fae2b2f166725c529e92fe
tallison <tallison@apache.org>
2025-02-25 11:46:41 -0500

    remove accidental unit test

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-package/src/test/java/org/apache/tika/parser/pkg/ZipParserTest.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-package/src/test/java/org/apache/tika/parser/pkg/ZipParserTest.java
@@ -106,11 +106,6 @@ public class ZipParserTest extends AbstractPkgTest {
     }
 
     @Test
-    public void testZipX() throws Exception {
-        debug(getRecursiveMetadataFromFullPath("/home/tallison/Downloads/demozipxfile.zipx"));
-    }
-
-    @Test
     public void testDataDescriptorWithEmptyEntry() throws Exception {
 
         //test that an empty first entry does not cause problems
