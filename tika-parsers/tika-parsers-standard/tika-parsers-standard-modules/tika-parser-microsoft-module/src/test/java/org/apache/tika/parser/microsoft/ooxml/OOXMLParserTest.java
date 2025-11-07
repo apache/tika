484eddcb1472f5efa40842a6ebe5f2cd3deddff8
Tim Allison <tallison@apache.org>
2025-11-07 15:29:52 -0500

    Revert "Revert "TIKA-4474 -- force spooling on ooxml"" (#2388)
    * Revert "Revert "TIKA-4474 -- force spooling on ooxml (#2386)" (#2387)"
    
    This reverts commit f00dbcee7f50fe14a9ea1b0e13e6895531870c68.
    
    * TIKA-4474 -- force spooling for xml checkstyle
    
    * TIKA-4474 -- remove monster file from multithreaded tests

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-microsoft-module/src/test/java/org/apache/tika/parser/microsoft/ooxml/OOXMLParserTest.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-microsoft-module/src/test/java/org/apache/tika/parser/microsoft/ooxml/OOXMLParserTest.java
@@ -1795,6 +1795,9 @@ public class OOXMLParserTest extends MultiThreadedTikaTest {
         RecursiveParserWrapper wrapper = new RecursiveParserWrapper(AUTO_DETECT_PARSER);
         testMultiThreaded(wrapper, parseContexts, numThreads, numIterations, path -> {
             String pathName = path.getName().toLowerCase(Locale.ENGLISH);
+            if (pathName.equalsIgnoreCase("testRecordSizeExceeded.xlsx")) {
+                return false;
+            }
             int i = pathName.lastIndexOf(".");
             String ext = "";
             if (i > -1) {
@@ -1813,5 +1816,12 @@ public class OOXMLParserTest extends MultiThreadedTikaTest {
         assertEquals("true", m.get(Office.HAS_HIDDEN_TEXT));
         assertEquals("true", m.get(Office.HAS_TRACK_CHANGES));
         assertEquals("true", m.get(Office.HAS_COMMENTS));
+    }
+
+    @Test
+    public void testNoRecordSizeOverflow() throws Exception {
+        //TIKA-4474 -- test: files (passed as stream) no longer have limit on record size as they are spooled
+        String content = getText("testRecordSizeExceeded.xlsx");
+        assertContains("Repetitive content pattern 3 for compression test row 1", content);
     }
 }
