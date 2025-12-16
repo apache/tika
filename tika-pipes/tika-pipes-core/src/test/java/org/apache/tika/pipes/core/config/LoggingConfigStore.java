70dfff7224ff8482c7085030c3930867bf6cb548
Nicholas DiPiazza <ndipiazza@apache.org>
2025-12-16 13:29:32 -0600

    TIKA-4576 Create pluggable storage interface for Fetcher components with in-memory implementation

---
+++ tika-pipes/tika-pipes-core/src/test/java/org/apache/tika/pipes/core/config/LoggingConfigStore.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tika.pipes.core.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tika.plugins.ExtensionConfig;
+
+/**
+ * Example custom ConfigStore implementation for demonstration purposes.
+ * This implementation logs all operations and could be extended to add
+ * persistence, caching, or other custom behavior.
+ */
+public class LoggingConfigStore implements ConfigStore {
+
+    private final Map<String, ExtensionConfig> store = new HashMap<>();
+    
+    @Override
+    public void put(String id, ExtensionConfig config) {
+        System.out.println("ConfigStore: Storing config with id=" + id);
+        synchronized (store) {
+            store.put(id, config);
+        }
+    }
+
+    @Override
+    public ExtensionConfig get(String id) {
+        synchronized (store) {
+            ExtensionConfig config = store.get(id);
+            if (config != null) {
+                System.out.println("ConfigStore: Retrieved config with id=" + id);
+            } else {
+                System.out.println("ConfigStore: Config not found for id=" + id);
+            }
+            return config;
+        }
+    }
+
+    @Override
+    public boolean containsKey(String id) {
+        synchronized (store) {
+            return store.containsKey(id);
+        }
+    }
+
+    @Override
+    public Set<String> keySet() {
+        synchronized (store) {
+            return Set.copyOf(store.keySet());
+        }
+    }
+
+    @Override
+    public int size() {
+        synchronized (store) {
+            return store.size();
+        }
+    }
+}
