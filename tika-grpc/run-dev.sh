7a4498a98c233d191b07eb5e098549a2ed6b3efe
Nicholas DiPiazza <ndipiazza@apache.org>
2025-12-26 10:06:19 -0600

    TIKA-4583 - Add Maven options for module accessibility in development mode

--- tika-grpc/run-dev.sh
+++ tika-grpc/run-dev.sh
@@ -17,4 +17,10 @@ echo "Plugins will be loaded from target/classes directories"
 echo "Press Ctrl+C to stop the server"
 echo ""
 
+export MAVEN_OPTS="--add-opens=java.base/java.nio=ALL-UNNAMED \
+--add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+--add-opens=java.base/java.util=ALL-UNNAMED \
+--add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED"
+
 mvn exec:java -Pdev -Dconfig.file="$CONFIG_FILE"
