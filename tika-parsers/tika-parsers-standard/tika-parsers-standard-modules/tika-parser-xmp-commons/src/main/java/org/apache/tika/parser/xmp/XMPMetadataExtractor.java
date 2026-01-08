c95697127f1ea4d0c82ceea51f7af368d983ccb2
Tilman Hausherr <tilman@apache.org>
2026-01-08 11:11:29 +0100

    TIKA-4614: add Media Management metadata extraction, avoid NPE, add test

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-xmp-commons/src/main/java/org/apache/tika/parser/xmp/XMPMetadataExtractor.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-xmp-commons/src/main/java/org/apache/tika/parser/xmp/XMPMetadataExtractor.java
@@ -18,14 +18,19 @@ package org.apache.tika.parser.xmp;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.commons.io.input.CloseShieldInputStream;
 import org.apache.xmpbox.XMPMetadata;
 import org.apache.xmpbox.schema.DublinCoreSchema;
 import org.apache.xmpbox.schema.XMPBasicSchema;
+import org.apache.xmpbox.schema.XMPMediaManagementSchema;
+import org.apache.xmpbox.type.AbstractField;
+import org.apache.xmpbox.type.ArrayProperty;
 import org.apache.xmpbox.type.BadFieldValueException;
+import org.apache.xmpbox.type.ResourceEventType;
+import org.apache.xmpbox.type.ResourceRefType;
 import org.apache.xmpbox.xml.DomXmpParser;
 
 import org.apache.tika.exception.TikaException;
@@ -33,11 +38,15 @@ import org.apache.tika.metadata.DublinCore;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.Property;
 import org.apache.tika.metadata.XMP;
+import org.apache.tika.metadata.XMPMM;
+import org.apache.tika.utils.DateUtils;
 
 /**
  * XMP Metadata Extractor based on Apache XmpBox.
  */
 public class XMPMetadataExtractor {
+
+    private static volatile int MAX_EVENT_HISTORY_IN_XMPMM = 1024;
 
     /**
      * Parse the XMP Packets.
@@ -59,6 +68,7 @@ public class XMPMetadataExtractor {
         }
         extractDublinCoreSchema(xmp, metadata);
         extractXMPBasicSchema(xmp, metadata);
+        extractXMPMM(xmp, metadata);
     }
 
     /**
@@ -67,18 +77,13 @@ public class XMPMetadataExtractor {
      * Silently swallows exceptions.
      * @param xmp the XMP Metadata object.
      * @param metadata the metadata map
+     * @throws IOException
      */
     public static void extractDublinCoreSchema(XMPMetadata xmp, Metadata metadata) throws IOException {
         if (xmp == null) {
             return;
         }
-        DublinCoreSchema schemaDublinCore;
-        try {
-            schemaDublinCore = xmp.getDublinCoreSchema();
-        } catch (Throwable e) {
-            // Swallow
-            return;
-        }
+        DublinCoreSchema schemaDublinCore = xmp.getDublinCoreSchema();
         if (schemaDublinCore != null) {
             try {
                 addMetadata(metadata, DublinCore.TITLE, schemaDublinCore.getTitle());
@@ -102,24 +107,99 @@ public class XMPMetadataExtractor {
      * Silently swallows exceptions.
      * @param xmp the XMP Metadata object.
      * @param metadata the metadata map
+     * @throws IOException
      */
     public static void extractXMPBasicSchema(XMPMetadata xmp, Metadata metadata) throws IOException {
         if (xmp == null) {
             return;
         }
-        XMPBasicSchema schemaBasic;
-        try {
-            schemaBasic = xmp.getXMPBasicSchema();
-        } catch (Throwable e) {
-            // Swallow
-            return;
-        }
+        XMPBasicSchema schemaBasic = xmp.getXMPBasicSchema();
         if (schemaBasic != null) {
             addMetadata(metadata, XMP.CREATOR_TOOL, schemaBasic.getCreatorTool());
-            addMetadata(metadata, XMP.CREATE_DATE, schemaBasic.getCreateDate().getTime());
-            addMetadata(metadata, XMP.MODIFY_DATE, schemaBasic.getModifyDate().getTime());
-            addMetadata(metadata, XMP.METADATA_DATE, schemaBasic.getModifyDate().getTime());
+            addMetadata(metadata, XMP.CREATE_DATE, schemaBasic.getCreateDate());
+            addMetadata(metadata, XMP.MODIFY_DATE, schemaBasic.getModifyDate());
+            addMetadata(metadata, XMP.METADATA_DATE, schemaBasic.getModifyDate());
             addMetadata(metadata, XMP.RATING, schemaBasic.getRating());
+        }
+    }
+
+    /**
+     * @return maximum number of events to extract from the XMPMM history.
+     */
+    public static int getMaxXMPMMHistory() {
+        return MAX_EVENT_HISTORY_IN_XMPMM;
+    }
+
+    /**
+     * Maximum number of events to extract from the
+     * event history in the XMP Media Management (XMPMM) section.
+     * The extractor will silently stop adding events after it
+     * has reached this threshold.
+     * <p>
+     * The default is 1024.
+     * @param maxEvents
+     */
+    public static void setMaxXMPMMHistory(int maxEvents) {
+        MAX_EVENT_HISTORY_IN_XMPMM = maxEvents;
+    }
+
+    /**
+     * Extracts Media Management metadata from XMP.
+     * <p>
+     * Silently swallows exceptions.
+     *
+     * @param xmp
+     * @param metadata
+     */
+    public static void extractXMPMM(XMPMetadata xmp, Metadata metadata) {
+        if (xmp == null) {
+            return;
+        }
+        XMPMediaManagementSchema mmSchema = xmp.getXMPMediaManagementSchema();
+        if (mmSchema != null) {
+            addMetadata(metadata, XMPMM.DOCUMENTID, mmSchema.getDocumentID());
+            metadata.set(XMPMM.INSTANCEID, mmSchema.getInstanceID());
+            metadata.set(XMPMM.ORIGINAL_DOCUMENTID, mmSchema.getOriginalDocumentID());
+
+            //ResourceRefType derivedFrom = mmSchema.getDerivedFromProperty(); //TODO after XMPBox 3.0.7
+            ResourceRefType derivedFrom = mmSchema.getResourceRefProperty();
+            
+            if (derivedFrom != null) {
+                addMetadata(metadata, XMPMM.DERIVED_FROM_DOCUMENTID, derivedFrom.getDocumentID());
+                addMetadata(metadata, XMPMM.DERIVED_FROM_INSTANCEID, derivedFrom.getInstanceID());
+            }
+            ArrayProperty historyProperty = mmSchema.getHistoryProperty();
+            if (historyProperty != null) {
+                int eventsAdded = 0;
+                for (AbstractField af : historyProperty.getAllProperties()) {
+                    if (eventsAdded >= MAX_EVENT_HISTORY_IN_XMPMM) {
+                        break;
+                    }
+                    if (!(af instanceof ResourceEventType))
+                    {
+                        continue;
+                    }
+                    ResourceEventType stevt = (ResourceEventType) af;
+                    String instanceId = stevt.getInstanceID();
+                    String action = stevt.getAction();
+                    Calendar when = stevt.getWhen();
+                    String softwareAgent = stevt.getSoftwareAgent();
+                    if (instanceId != null && !instanceId.isBlank())
+                    {
+                        // for absent data elements, pass in empty strings so
+                        // that parallel arrays will have matching offsets for absent data
+                        action = action == null ? "" : action;
+                        String dateString = when == null ? "" : DateUtils.formatDate(when);
+                        softwareAgent = softwareAgent == null ? "" : softwareAgent;
+
+                        metadata.add(XMPMM.HISTORY_EVENT_INSTANCEID, instanceId);
+                        metadata.add(XMPMM.HISTORY_ACTION, action);
+                        metadata.add(XMPMM.HISTORY_WHEN, dateString);
+                        metadata.add(XMPMM.HISTORY_SOFTWARE_AGENT, softwareAgent);
+                        eventsAdded++;
+                    }
+                }
+            }
         }
     }
 
@@ -179,7 +259,7 @@ public class XMPMetadataExtractor {
      * @param property the property to add.
      * @param value the value to add.
      */
-    private static void addMetadata(Metadata metadata, Property property, Date value) {
+    private static void addMetadata(Metadata metadata, Property property, Calendar value) {
         if (value != null) {
             metadata.set(property, value);
         }
