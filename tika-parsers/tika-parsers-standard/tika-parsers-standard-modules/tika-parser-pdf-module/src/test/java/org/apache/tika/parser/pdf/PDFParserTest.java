e1f2d5949d6d26a9567826b42602f118bf6b5811
Tilman Hausherr <tilman@snafu.de>
2026-01-14 13:44:07 +0100

    TIKA-4622: Add test for PDF annotations with no content
    Added a test for PDF annotations without content streams.

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pdf-module/src/test/java/org/apache/tika/parser/pdf/PDFParserTest.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pdf-module/src/test/java/org/apache/tika/parser/pdf/PDFParserTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -366,6 +366,13 @@ public class PDFParserTest extends TikaTest {
         XMLResult r = getXML("testPopupAnnotation.pdf");
         assertContains("this is the note", r.xml);
         assertContains("igalsh", r.xml);
+    }
+
+    // TIKA-4622 / PDFBOX-6145 make sure that annotations aren't missed if no page content stream
+    @Test
+    public void testAnnotationNoContents() throws Exception {
+        XMLResult r = getXML("testPDFFileEmbInAnnotation_noContents.pdf");
+        assertContains("Excel.xlsx", r.xml);
     }
 
     @Test
