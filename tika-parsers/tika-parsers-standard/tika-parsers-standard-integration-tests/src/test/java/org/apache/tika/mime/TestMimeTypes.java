5748d4c2b971c400ea8e7524784fba56c43f49d1
Dominik Schmidt <dev@dominik-schmidt.de>
2026-08-19 09:29:49 +0200

    TIKA-4824 - detect little-endian BigTIFF by data

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-integration-tests/src/test/java/org/apache/tika/mime/TestMimeTypes.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-integration-tests/src/test/java/org/apache/tika/mime/TestMimeTypes.java
@@ -902,6 +902,9 @@ public class TestMimeTypes {
         assertTypeByNameAndData("image/x-raw-pentax", "testPEF.pef");
         assertTypeByData("image/tiff", "testDNG.dng");
         assertTypeByNameAndData("image/x-raw-adobe", "testDNG.dng");
+        //little-endian BigTIFF DNG: detectable as tiff by data, x-raw-adobe by name+data
+        assertTypeByData("image/tiff", "testDNG_bigtiff.dng");
+        assertTypeByNameAndData("image/x-raw-adobe", "testDNG_bigtiff.dng");
         assertTypeByData("image/x-canon-cr2", "testCR2.cr2");
         assertTypeByNameAndData("image/x-canon-cr2", "testCR2.cr2");
         assertTypeByName("image/x-raw-olympus", "x.orf");
