73766fcc4a6a1b87673ebfff370e8c0330ea2c8e
Tilman Hausherr <tilman@snafu.de>
2026-08-26 13:38:48 +0200

    Refactor test for reserved key in trusted metadata

--- tika-core/src/test/java/org/apache/tika/metadata/TestMetadata.java
+++ tika-core/src/test/java/org/apache/tika/metadata/TestMetadata.java
@@ -496,8 +496,7 @@ public class TestMetadata extends TikaTest {
     public void testReservedKeyAllowedOnTrustedMetadata() {
         String reservedKey = TikaCoreProperties.TIKA_META_PREFIX + "reserved";
         Metadata m = new Metadata();
-        m.addTrusted(reservedKey, true);
-        m.add(reservedKey, "value");
+        m.addTrusted(reservedKey, "value");
         assertEquals("value", m.get(reservedKey));
     }
 
