599027de9e751ff662db7bc867f9bcaa02e7b7be
Tilman Hausherr <tilman@snafu.de>
2026-08-26 14:35:10 +0200

    Add assertThrows import to TestMetadata.java

--- tika-core/src/test/java/org/apache/tika/metadata/TestMetadata.java
+++ tika-core/src/test/java/org/apache/tika/metadata/TestMetadata.java
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
